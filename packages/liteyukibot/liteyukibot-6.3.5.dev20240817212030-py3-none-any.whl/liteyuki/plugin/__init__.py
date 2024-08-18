72ce7fe9dd32ac72d19008d8626ff501f920fa21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:20 +0000

    Add package liteyukibot/6.3.5.dev20240817212030/liteyukibot-6.3.5.dev20240817212030-py3-none-any.whl

---
+++ packages/liteyukibot/liteyukibot-6.3.5.dev20240817212030-py3-none-any.whl/liteyuki/plugin/__init__.py
@@ -0,0 +1,18 @@
+from liteyuki.plugin.model import Plugin, PluginMetadata
+from liteyuki.plugin.load import load_plugin, load_plugins, _plugins
+
+__all__ = [
+        "PluginMetadata",
+        "Plugin",
+        "load_plugin",
+        "load_plugins",
+]
+
+
+def get_loaded_plugins() -> dict[str, Plugin]:
+    """
+    获取已加载的插件
+    Returns:
+        dict[str, Plugin]: 插件字典
+    """
+    return _plugins
