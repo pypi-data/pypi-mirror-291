b0b7cee0e812c0de9be1760d63dff957bd22b144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:20 +0000

    Add package utils_fastapi/0.0.1/utils_fastapi-0.0.1.tar.gz

---
+++ packages/utils_fastapi/utils_fastapi-0.0.1.tar.gz/utils_fastapi-0.0.1/fastapi_utils/pagination/__init__.py
@@ -0,0 +1,4 @@
+from fastapi_utils.pagination.limit_pagination import LimitOffsetPagination, DictLimitOffsetPagination
+from fastapi_utils.pagination.page_pagination import PageNumberPagination, DictPageNumberPagination
+
+__all__ = ['LimitOffsetPagination', "PageNumberPagination", "DictPageNumberPagination", "DictLimitOffsetPagination"]
