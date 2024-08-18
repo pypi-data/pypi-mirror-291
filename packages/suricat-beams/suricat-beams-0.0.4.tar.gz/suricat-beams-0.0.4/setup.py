403d191a6891ea328f25c9d4a49cba2195c78f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:29 +0000

    Add package suricat-beams/0.0.4/suricat-beams-0.0.4.tar.gz

---
+++ packages/suricat-beams/suricat-beams-0.0.4.tar.gz/suricat-beams-0.0.4/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['suricat']
+
+package_data = \
+{'': ['*'], 'suricat': ['cargo/*', 'cargo/suricat/*']}
+
+install_requires = \
+['astro-tigger-lsm',
+ 'astropy',
+ 'click',
+ 'matplotlib',
+ 'numpy',
+ 'scipy',
+ 'stimela>=2.0,<3.0',
+ 'suricat-beams-cabs==0.0.4',
+ 'xarray-fits']
+
+entry_points = \
+{'console_scripts': ['suricat = suricat.main:cli']}
+
+setup_kwargs = {
+    'name': 'suricat-beams',
+    'version': '0.0.4',
+    'description': 'MeerKAT primary beam model handling utilities',
+    'long_description': '# suricat-beams\nMeerKAT primary beam model handling utilities\n\nSee https://doi.org/10.48479/wdb0-h061 for documentation on beam models. \n\nThe utilities can download the models for you automatically, see suricat.yml Stimela recipe.',
+    'author': 'Oleg Smirnov',
+    'author_email': 'osmirnov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
