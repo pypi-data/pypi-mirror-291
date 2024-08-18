36a5ca9387a0287837ad520d17de5adfa4f46f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:32 +0000

    Add package creao/0.1.37/creao-0.1.37-py3-none-any.whl

---
+++ packages/creao/creao-0.1.37-py3-none-any.whl/creao/component/filters/filter_question_by_intelligent.py
@@ -0,0 +1,29 @@
+from typing import List
+from haystack import component, default_from_dict, default_to_dict
+from creao.core.Generator import *
+import concurrent.futures
+
+@component
+class ItelligentFilter:
+    def __init__(self):
+        self.intelligent_question_filter = Intelligent_Question_Filter()
+
+    @component.output_types(outputs=List[str])
+    def run(self, inputs:List[str], file_name:str, chunk:str):
+        def filter_question(question):
+            filter_flag = self.intelligent_question_filter.execute(question, file_name, chunk)
+            return question if filter_flag['Type_of_question'] == "Type_A" else None
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            filtered_questions = list(executor.map(filter_question, inputs))
+
+        # Filter out any None values from the list of filtered questions
+        questions = [q for q in filtered_questions if q is not None]
+        return {"outputs": questions}
+    
+    def to_dict(self) -> dict:
+        return default_to_dict(self)
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "ItelligentFilter":
+        return default_from_dict(cls, data)
