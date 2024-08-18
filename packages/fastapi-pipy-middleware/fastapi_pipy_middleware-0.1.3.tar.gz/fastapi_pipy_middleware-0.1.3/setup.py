fd68264a71872243a753fb147dea48dd1b652d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:58 +0000

    Add package fastapi-pipy-middleware/0.1.3/fastapi_pipy_middleware-0.1.3.tar.gz

---
+++ packages/fastapi-pipy-middleware/fastapi_pipy_middleware-0.1.3.tar.gz/fastapi_pipy_middleware-0.1.3/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fastapi-pipy-middleware',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'starlette'
+    ],
+    description='A FastAPI middleware example',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://pypi.org/manage/projects/fastapi-pipy-middleware',
+)
