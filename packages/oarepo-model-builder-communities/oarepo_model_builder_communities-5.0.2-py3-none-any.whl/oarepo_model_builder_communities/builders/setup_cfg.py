63ed3dfa6446a33bf122bbc311989e264d4c07b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:46 +0000

    Add package oarepo-model-builder-communities/5.0.2/oarepo_model_builder_communities-5.0.2-py3-none-any.whl

---
+++ packages/oarepo-model-builder-communities/oarepo_model_builder_communities-5.0.2-py3-none-any.whl/oarepo_model_builder_communities/builders/setup_cfg.py
@@ -0,0 +1,13 @@
+from oarepo_model_builder.builders import OutputBuilder
+from oarepo_model_builder.outputs.cfg import CFGOutput
+
+
+class CommunitiesSetupCfgBuilder(OutputBuilder):
+    TYPE = "communities_setup_cfg"
+
+    def finish(self):
+        super().finish()
+
+        output: CFGOutput = self.builder.get_output("cfg", "setup.cfg")
+
+        output.add_dependency("oarepo-communities", ">=5.0.0")
