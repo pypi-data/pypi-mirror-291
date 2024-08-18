32451f2fba53efef8beec2346139113779c1afaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:20 +0000

    Add package blue-plugin/3.131.1/blue_plugin-3.131.1-py3-none-any.whl

---
+++ packages/blue-plugin/blue_plugin-3.131.1-py3-none-any.whl/blue_plugin/README.py
@@ -0,0 +1,25 @@
+from abcli import file
+from abcli.plugins.README import build as build_README
+from blue_plugin import NAME, VERSION, ICON, REPO_NAME
+
+
+items = [
+    "{}[`{}`](#) [![image]({})](#) {}".format(
+        ICON,
+        f"feature {index}",
+        "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
+        f"description of feature {index} ...",
+    )
+    for index in range(1, 4)
+]
+
+
+def build():
+    return build_README(
+        items=items,
+        path=file.path(__file__),
+        ICON=ICON,
+        NAME=NAME,
+        VERSION=VERSION,
+        REPO_NAME=REPO_NAME,
+    )
