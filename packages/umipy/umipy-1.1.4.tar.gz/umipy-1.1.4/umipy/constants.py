69421cfd41ae92038d71c7ccbf9283d60707edb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:18 +0000

    Add package umipy/1.1.4/umipy-1.1.4.tar.gz

---
+++ packages/umipy/umipy-1.1.4.tar.gz/umipy-1.1.4/umipy/constants.py
@@ -0,0 +1,15 @@
+BASE_URL_MAINNET = "https://mainnet.umi.top"
+BASE_URL_TESTNET = "https://testnet.umi.top"
+
+BASE_STATS_URL_MAINNET = "https://stats.umi.top"
+BASE_STATS_URL_TESTNET = "https://stats-testnet.umi.top"
+
+
+class Prefix:
+    UMI = "umi"
+    GLIZE = "glz"
+    GLIZE_STAKING = "gls"
+
+    ROD_STAKING = "rod01"
+    ROD_TRADING = "rod"
+    ROD_TESTNET = "rod99"
