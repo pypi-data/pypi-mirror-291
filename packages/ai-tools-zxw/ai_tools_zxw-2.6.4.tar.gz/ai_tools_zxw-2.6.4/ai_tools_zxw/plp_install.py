bad688d4bee217cfe0c3c551f22fedc9080d637d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:03:05 +0000

    Add package ai-tools-zxw/2.6.4/ai_tools_zxw-2.6.4.tar.gz

---
+++ packages/ai-tools-zxw/ai_tools_zxw-2.6.4.tar.gz/ai_tools_zxw-2.6.4/ai_tools_zxw/plp_install.py
@@ -0,0 +1,32 @@
+"""
+# File       : plp_install.py
+# Time       ：2024/7/29 上午11:05
+# Author     ：xxxx
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+
+
+# yolo 依赖的包
+xlwt
+psutil
+ultralytics
+opencv-python
+matplotlib
+tqdm
+pycocotools
+
+# 讯飞星火大模型依赖的包
+websocket-client
+openpyxl
+openai
+
+# 依赖的其他包，例如：'requests>=2.0.0'
+# nlp预处理
+emoji
+jieba
+
+# Claude api调用依赖包
+anthropic
+
+"""
---
+++ packages/ai-tools-zxw/ai_tools_zxw-2.6.4.tar.gz/ai_tools_zxw-2.6.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_tools_zxw",
+    version="2.6.4",
+    packages=find_packages(),
+    install_requires=[
+        'xlwt',
+        'psutil',
+        'ultralytics',
+        'opencv-python',
+        'matplotlib',
+        'tqdm',
+        'pycocotools',
+        # 讯飞星火大模型依赖的包
+        'websocket-client',
+        'openpyxl',
+        'openai',
+        # 依赖的其他包，例如：'requests>=2.0.0'
+        # nlp预处理
+        'emoji',
+        'jieba',
+        # Claude大模型
+        'anthropic'
+    ],
+    author="xw zhang",
+    author_email="",
+    description="常用的人工智能操作的中文工具包。",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sunshine/",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
