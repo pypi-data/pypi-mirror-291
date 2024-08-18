d17435f9bca4311a5546c72b6bc7ec3d74644fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:56 +0000

    Add package shapeandshare.light/0.5.2/shapeandshare_light-0.5.2.tar.gz

---
+++ packages/shapeandshare.light/shapeandshare_light-0.5.2.tar.gz/shapeandshare_light-0.5.2/src/shapeandshare/light/const.py
@@ -0,0 +1,34 @@
+import pygame
+
+# Predefined some colors
+BLUE: tuple[int, int, int] = (0, 0, 255)
+RED: tuple[int, int, int] = (255, 0, 0)
+GREEN: tuple[int, int, int] = (0, 255, 0)
+BLACK: tuple[int, int, int] = (0, 0, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+
+
+# TILE_X: int = 15
+# TILE_Y: int = 15
+# DIM_X: int = 75
+# DIM_Y: int = 50
+
+
+TILE_X: int = 15
+TILE_Y: int = 15
+
+
+DIM_X: int = 32
+DIM_Y: int = 32
+
+
+# 100x15=1500
+WINDOW_WIDTH: int = TILE_X * DIM_X
+WINDOW_HEIGHT: int = TILE_Y * DIM_Y
+
+
+FPS: int = 60
+FramePerSec = pygame.time.Clock()
+
+
+PRODUCT_NAME: str = "light"
---
+++ packages/shapeandshare.light/shapeandshare_light-0.5.2.tar.gz/shapeandshare_light-0.5.2/src/shapeandshare/light/loop.py
@@ -0,0 +1,57 @@
+import sys
+
+import pygame
+from pygame import QUIT, Surface, SurfaceType
+
+from shapeandshare.darkness import Client, CommandOptions, TileType, Chunk
+
+from .const import DIM_X, DIM_Y, FPS, WHITE, FramePerSec
+from .contracts.sprites.chunk import SpriteChunk
+
+
+async def loop(display_surface: Surface | SurfaceType):
+    options: CommandOptions = CommandOptions(sleep_time=5, retry_count=3, tld="127.0.0.1:8000", timeout=60)
+    client: Client = Client(options=options)
+
+    # create a new world
+    world_id: str = await client.world_create(name="darkness")
+
+    # create a new chunk
+    chunk_id: str = await client.chunk_create(
+        world_id=world_id, name="roshar", dimensions=(DIM_X, DIM_Y), biome=TileType.GRASS
+    )
+    chunk: Chunk = await client.chunk_get(world_id=world_id, chunk_id=chunk_id, full=True)
+
+    sprite_chunk: SpriteChunk = SpriteChunk.model_validate(
+        {**chunk.model_dump(exclude={"contents"}), "offset": (0, 0)}
+    )
+    sprite_chunk.load_tiles(tiles=chunk.contents)
+
+    while True:
+        # Review game events (of interest)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type in (pygame.MOUSEMOTION, pygame.MOUSEBUTTONDOWN):
+                pointer_pos: tuple[int, int] = pygame.mouse.get_pos()
+                # print(pointer_pos)
+
+                # Determine action type
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # print("MOUSEBUTTONDOWN")
+                    sprite_chunk.update_tile_selection(position=pointer_pos)
+
+                elif event.type == pygame.MOUSEMOTION:
+                    # print("MOUSEMOTION")
+                    sprite_chunk.update_tile_hover(position=pointer_pos)
+
+        # Apply game logic updates
+        # Redraw the surface
+        display_surface.fill(WHITE)
+        for _, tile in sprite_chunk.tiles.items():
+            tile.draw(display_surface)
+
+        pygame.display.update()
+        FramePerSec.tick(FPS)
