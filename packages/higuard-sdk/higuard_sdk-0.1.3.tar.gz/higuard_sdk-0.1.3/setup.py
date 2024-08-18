ed73c251ff2face7e7c583e9e50d9b39a53ca4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:28 +0000

    Add package higuard-sdk/0.1.3/higuard_sdk-0.1.3.tar.gz

---
+++ packages/higuard-sdk/higuard_sdk-0.1.3.tar.gz/higuard_sdk-0.1.3/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f if line and not line.startswith("#")]
+
+
+setup(
+    name="higuard_sdk",
+    version="0.1.3",
+    author="Derek Li",
+    author_email="derekli11204@gmail.com",
+    description="Python SDK for Error Dashboard",
+    url="https://github.com/HiQ-Apps/error_dashboard_python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=parse_requirements("requirements.txt"),
+)
