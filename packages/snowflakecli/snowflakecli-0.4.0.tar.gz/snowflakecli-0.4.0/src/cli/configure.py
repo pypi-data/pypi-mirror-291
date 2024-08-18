64913988fe31e67403f4b0aa1ac8b655a95d6059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:48 +0000

    Add package snowflakecli/0.4.0/snowflakecli-0.4.0.tar.gz

---
+++ packages/snowflakecli/snowflakecli-0.4.0.tar.gz/snowflakecli-0.4.0/src/cli/configure.py
@@ -0,0 +1,12 @@
+import typer
+
+from cli.core.config.sfcli import ensure_config_file
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def cli():
+    """Configure SnowflakeCLI"""
+    ensure_config_file()
+    return
