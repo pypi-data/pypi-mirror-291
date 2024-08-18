fb6e99a0a5de4529202169cf16e27800f0b887b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:04:00 +0000

    Add package abantether-python-sdk/0.0.0/abantether-python-sdk-0.0.0.tar.gz

---
+++ packages/abantether-python-sdk/abantether-python-sdk-0.0.0.tar.gz/abantether-python-sdk-0.0.0/README.md
@@ -0,0 +1,16 @@
+# Abantether Python SDK
+A python sdk to trade easily in abantether.com (under development)
+
+## How to use?
+
+To instantiate the client use:
+
+```python
+aban_client = AbantetherClient(access_token=ACCESS_TOKEN)
+```
+
+For using client methods, docstrings and examples are provided for every method. Examples are [in this file](./abantether/examples.py).
+
+## Examples
+
+Examples are [in this file](./abantether/examples.py).
---
+++ packages/abantether-python-sdk/abantether-python-sdk-0.0.0.tar.gz/abantether-python-sdk-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abantether-python-sdk',
+    version='0.0.0',
+    author='Iman Mousaei',
+    author_email='imanmousaei1379@gmail.com',
+    description='A python sdk to trade easily in abantether.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
