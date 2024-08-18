7a80967dc849cc2bd2ddcfbb11a2fcc3fc4613d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:03:00 +0000

    Add package datajunction-server/0.0.1a62.dev1/datajunction_server-0.0.1a62.dev1-py3-none-any.whl

---
+++ packages/datajunction-server/datajunction_server-0.0.1a62.dev1-py3-none-any.whl/datajunction_server/models/user.py
@@ -0,0 +1,31 @@
+"""
+Models for users and auth
+"""
+from typing import Optional
+
+from pydantic import BaseModel
+
+from datajunction_server.database.user import OAuthProvider
+
+
+class UserOutput(BaseModel):
+    """User information to be included in responses"""
+
+    id: int
+    username: str
+    email: Optional[str]
+    name: Optional[str]
+    oauth_provider: OAuthProvider
+    is_admin: bool = False
+
+    class Config:  # pylint: disable=missing-class-docstring, too-few-public-methods
+        orm_mode = True
+
+
+class UserActivity(BaseModel):
+    """
+    User activity info
+    """
+
+    username: str
+    count: int
