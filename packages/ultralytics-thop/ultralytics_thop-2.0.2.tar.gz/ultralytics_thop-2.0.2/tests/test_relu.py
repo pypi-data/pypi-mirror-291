ba7c672f2c182e2d3137c38e2c013969fc50e5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:04:08 +0000

    Add package ultralytics-thop/2.0.2/ultralytics_thop-2.0.2.tar.gz

---
+++ packages/ultralytics-thop/ultralytics_thop-2.0.2.tar.gz/ultralytics_thop-2.0.2/tests/test_relu.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+from thop import profile
+
+
+class TestUtils:
+    def test_relu(self):
+        """Tests ReLU activation ensuring zero FLOPs and displays parameter count using THOP profiling."""
+        n, in_c, _out_c = 1, 100, 200
+        net = nn.ReLU()
+        flops, params = profile(net, inputs=(torch.randn(n, in_c),))
+        print(flops, params)
+        assert flops == 0
