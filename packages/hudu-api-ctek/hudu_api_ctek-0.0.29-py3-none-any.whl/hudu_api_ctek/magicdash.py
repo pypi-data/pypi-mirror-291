2a6402432d00f16f516554883dd377fb42485aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:57 +0000

    Add package hudu-api-ctek/0.0.29/hudu_api_ctek-0.0.29-py3-none-any.whl

---
+++ packages/hudu-api-ctek/hudu_api_ctek-0.0.29-py3-none-any.whl/hudu_api_ctek/magicdash.py
@@ -0,0 +1,22 @@
+import requests
+
+def create_magic_dash(api_baseurl, api_key, dash_name, dash_message, dash_shade, dash_content, company_name):
+    try:
+        url = f"{api_baseurl}/magic_dash"
+        payload = {
+            "title": f"{dash_name}",
+            "message": f"{dash_message}",
+            "shade": f"{dash_shade}",
+            "content": f"{dash_content}",
+            "content_link": "",
+            "image_url": "",
+            "icon": "fas fa-network-wired",
+            "company_name": company_name
+        }
+        app.logger.info(f"Creating magic dash: {payload}")
+        headers = {"x-api-key": api_key}
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        return response.json(), None
+    except requests.exceptions.RequestException as e:
+        return None, f"Error creating magic dash: {e}"
