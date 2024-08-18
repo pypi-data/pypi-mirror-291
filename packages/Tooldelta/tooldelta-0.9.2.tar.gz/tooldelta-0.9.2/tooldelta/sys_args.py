491e2be96069a7b9693c0789969d541be7d0627f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:45 +0000

    Add package Tooldelta/0.9.2/tooldelta-0.9.2.tar.gz

---
+++ packages/Tooldelta/tooldelta-0.9.2.tar.gz/tooldelta-0.9.2/tooldelta/sys_args.py
@@ -0,0 +1,42 @@
+"解析启动参数"
+
+import sys
+
+
+def sys_args_to_dict(argv: list[str] = sys.argv) -> dict[str, str | None]:
+    """将启动参数转换为字典
+
+    Args:
+        argv (list[str], optional): 启动参数
+
+    Returns:
+        dict[str, str | None]: 启动参数字典
+    """
+    # 为什么不用 argsparser? 因为这样可以支持混合型启动参数
+    d: dict[str, str | None] = {}
+    i = 0
+    while i < len(argv):
+        arg = argv[i]
+        if arg.startswith("-"):
+            arg = arg.strip("--").strip("-")
+            try:
+                val = argv[i + 1]
+                if val.startswith("-"):
+                    val = None
+                else:
+                    i += 1
+            except IndexError:
+                val = None
+            d[arg] = val
+        i += 1
+    return d
+
+
+def print_help():
+    print("启动参数帮助：")
+    print("    -help  查看帮助 (当前页)")
+    print("    -no-update-check  禁用自动更新")
+    print("    -no-download-libs  禁用自动更新依赖库")
+    print("    -no-download-neomega 禁用自动更新NeOmega")
+    print("    -l <启动模式>  按照特定的启动模式，跳过启动页面")
+    print("    -user-token <token>  使用传入的而非本地的token来登录验证服务器")
