f698fbe9331233843da44b5cd0c11673cb343a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:19 +0000

    Add package odoolint/1.7/odoolint-1.7.tar.gz

---
+++ packages/odoolint/odoolint-1.7.tar.gz/odoolint-1.7/odoolint/config.py
@@ -0,0 +1,15 @@
+import yaml
+
+DEFAULT_CONFIG = {
+    "flake8_select": "C,E,F,W,B,B9,N801,N803",
+    "flake8_ignore": "E203,E501,W503,C901,W605,E722,E731",
+    "flake8_exclude": ["**unported**", "**__init__.py", "tests"],
+    "check_file_types": [".xml", ".js", ".css", ".scss"]
+}
+
+
+def load_config(config_file=None):
+    if config_file:
+        with open(config_file, 'r') as f:
+            return yaml.safe_load(f)
+    return DEFAULT_CONFIG.copy()
