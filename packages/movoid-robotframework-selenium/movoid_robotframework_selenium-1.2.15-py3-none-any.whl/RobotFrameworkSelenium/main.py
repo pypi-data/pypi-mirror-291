137595b96f3029187f19aa596b722fb1e55cd35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:04:19 +0000

    Add package movoid-robotframework-selenium/1.2.15/movoid_robotframework_selenium-1.2.15-py3-none-any.whl

---
+++ packages/movoid-robotframework-selenium/movoid_robotframework_selenium-1.2.15-py3-none-any.whl/RobotFrameworkSelenium/main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : main
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/2/16 18:47
+# Description   : 
+"""
+from .action import SeleniumAction
+
+
+class RobotSeleniumBasic(SeleniumAction):
+    pass
