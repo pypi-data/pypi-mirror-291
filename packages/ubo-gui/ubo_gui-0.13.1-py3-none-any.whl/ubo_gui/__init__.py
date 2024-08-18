2a16bd492756fd252dacc8cc782a3f55afc63c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:22 +0000

    Add package ubo-gui/0.13.1/ubo_gui-0.13.1-py3-none-any.whl

---
+++ packages/ubo-gui/ubo_gui-0.13.1-py3-none-any.whl/ubo_gui/__init__.py
@@ -0,0 +1,26 @@
+"""Initializes the ubo_gui package and registers various widgets using Kivy Factory.
+
+It also defines the paths for assets and fonts used by the package.
+"""
+
+from pathlib import Path
+
+from kivy.factory import Factory
+
+__import__('ubo_gui.constants')
+__import__('ubo_gui.menu.constants')
+
+ROOT_PATH = Path(__file__).parent
+ASSETS_PATH = ROOT_PATH.joinpath('assets')
+FONTS_PATH = ASSETS_PATH.joinpath('fonts')
+
+
+Factory.register('AnimatedSlider', module='ubo_gui.animated_slider')
+Factory.register('GaugeWidget', module='ubo_gui.gauge')
+Factory.register('ItemWidget', module='ubo_gui.menu.widgets.item_widget')
+Factory.register('MenuWidget', module='ubo_gui.menu')
+Factory.register('NotificationWidget', module='ubo_gui.notification')
+Factory.register('ProgressRingWidget', module='ubo_gui.progress_ring')
+Factory.register('PromptWidget', module='ubo_gui.prompt')
+Factory.register('QRCodeWidget', module='ubo_gui.qrcode')
+Factory.register('VolumeWidget', module='ubo_gui.volume')
