5ec66488eccffafa692817e7ff0a760f1e7ac0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:04:01 +0000

    Add package marine-navigation/0.4.0/marine_navigation-0.4.0.tar.gz

---
+++ packages/marine-navigation/marine_navigation-0.4.0.tar.gz/marine_navigation-0.4.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='marine_navigation',
+  version='0.4.0',
+  description='A package to calculate tidal windows for marine vessels entering ports.',
+  author='Your Name',
+  author_email='your.email@example.com',
+  packages=find_packages(),
+  install_requires=[
+      'pandas',
+      'numpy',
+      'scipy',
+      'matplotlib',
+  ],
+  include_package_data=True,
+  package_data={
+    'marine_navigator': ['data/tide_heights.csv', 'data/ports.csv', 'data/vessels.csv'],
+  },
+)
+
