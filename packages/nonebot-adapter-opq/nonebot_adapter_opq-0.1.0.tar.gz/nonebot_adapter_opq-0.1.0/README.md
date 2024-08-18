b9b95a29a535d6a787173653bdd8e0e5c1aef9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:34 +0000

    Add package nonebot-adapter-opq/0.1.0/nonebot_adapter_opq-0.1.0.tar.gz

---
+++ packages/nonebot-adapter-opq/nonebot_adapter_opq-0.1.0.tar.gz/nonebot_adapter_opq-0.1.0/README.md
@@ -0,0 +1,2 @@
+# adapter-OPQBot
+NoneBot2 OPQBot适配器 / OPQBot adapter for nonebot2
---
+++ packages/nonebot-adapter-opq/nonebot_adapter_opq-0.1.0.tar.gz/nonebot_adapter_opq-0.1.0/nonebot/adapters/opqbot/models/__init__.py
@@ -0,0 +1,2 @@
+from .message import MsgBody,CurrentPacket
+from .response import (Response, UploadImageVoiceResponse, BaseResponse, SendMsgResponse,UploadForwardMsgResponse)
