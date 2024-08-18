06c2c0ff70769a3eba78f8414aed19eced79c484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:38 +0000

    Add package cmw/2.1.9/cmw-2.1.9.tar.gz

---
+++ packages/cmw/cmw-2.1.9.tar.gz/cmw-2.1.9/src/cmw/Replace.js
@@ -0,0 +1,33 @@
+function ReplaceJsSetupFormSubmitListener(addedNodes) {
+    addedNodes.forEach(node => {
+        if (node.tagName === "FORM" && node.hasAttribute("data-replace")) {
+            node.addEventListener("submit", event => {
+                event.preventDefault();
+                (async () => {
+                    const response = await fetch(node.action, {
+                        method: "POST",
+                        body: new FormData(node),
+                    });
+                    node.outerHTML = await response.text();
+                })();
+            });
+        }
+    });
+}
+function ReplaceJsWatchForms() {
+    const forms = document.querySelectorAll("form[data-replace]");
+    ReplaceJsSetupFormSubmitListener(forms);
+    const observer = new MutationObserver(mutationsList => {
+        mutationsList.forEach(mutation => {
+            ReplaceJsSetupFormSubmitListener(mutation.addedNodes);
+        });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+}
+const previousOnload = window.onload;
+window.onload = () => {
+    if (previousOnload) {
+        previousOnload();
+    }
+    ReplaceJsWatchForms();
+};
