53f095c4073038ff6ab3186174d7da8844434ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:04:19 +0000

    Add package kapicorp-reclass/2.0.0/kapicorp_reclass-2.0.0-py3-none-any.whl

---
+++ packages/kapicorp-reclass/kapicorp_reclass-2.0.0-py3-none-any.whl/reclass/version.py
@@ -0,0 +1,26 @@
+#
+# -*- coding: utf-8 -*-
+#
+# This file is part of reclass (http://github.com/madduck/reclass)
+#
+# Copyright © 2007–14 martin f. krafft <madduck@madduck.net>
+# Released under the terms of the Artistic Licence 2.0
+#
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+RECLASS_NAME = 'kapicorp-reclass'
+DESCRIPTION = ('merge data by recursive descent down an ancestry hierarchy '
+               '(forked extended version)')
+VERSION = '2.0.0'
+AUTHOR = 'martin f. krafft / Andrew Pickford / salt-formulas community/ Kapicorp community'
+AUTHOR_EMAIL = 'ademaria@kapicorp.com'
+MAINTAINER = 'Kapicorp community'
+MAINTAINER_EMAIL = 'ademaria@kapicorp.com'
+COPYRIGHT = ('Copyright © 2007–14 martin f. krafft, extensions © 2017 Andrew'
+             'Pickford, extensions © salt-formulas community'
+             'Kapicorp')
+LICENCE = 'Artistic Licence 2.0'
+URL = 'https://github.com/kapicorp/reclass'
