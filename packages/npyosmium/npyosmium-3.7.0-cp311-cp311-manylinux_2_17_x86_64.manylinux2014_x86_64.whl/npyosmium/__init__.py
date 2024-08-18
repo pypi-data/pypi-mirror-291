e8a1b7ca6ff182c2d106e615aa040c84e21e9a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:11 +0000

    Add package npyosmium/3.7.0/npyosmium-3.7.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/npyosmium/npyosmium-3.7.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/npyosmium/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# This file is part of pyosmium. (https://osmcode.org/pyosmium/)
+#
+# Copyright (C) 2024 Sarah Hoffmann <lonvia@denofr.de> and others.
+# For a full list of authors see the git log.
+
+from npyosmium._osmium import *
+from npyosmium.helper import *
+from npyosmium.simple_handler import SimpleHandler
+from npyosmium.file_processor import FileProcessor
+import npyosmium.io
+import npyosmium.osm
+import npyosmium.index
+import npyosmium.geom
+import npyosmium.area
+import npyosmium.filter
