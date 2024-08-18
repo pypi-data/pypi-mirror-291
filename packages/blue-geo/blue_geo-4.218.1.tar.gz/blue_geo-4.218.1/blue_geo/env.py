7bd7133a3f278a5d8946b998e5784d4ec1215af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:58 +0000

    Add package blue-geo/4.218.1/blue_geo-4.218.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.218.1.tar.gz/blue_geo-4.218.1/blue_geo/env.py
@@ -0,0 +1,31 @@
+import os
+from abcli.env import load_env, load_config
+
+load_env(__name__)
+load_config(__name__)
+
+
+BLUE_GEO_UKRAINE_TIMEMAP_QGIS_TEMPLATE = os.getenv(
+    "BLUE_GEO_UKRAINE_TIMEMAP_QGIS_TEMPLATE", ""
+)
+
+BLUE_GEO_FIRMS_AREA_QGIS_TEMPLATE = os.getenv(
+    "BLUE_GEO_FIRMS_AREA_QGIS_TEMPLATE",
+    "",
+)
+
+FIRMS_MAP_KEY = os.getenv("FIRMS_MAP_KEY", "")
+
+QGIS_TEMPLATES = {
+    "firms_area": BLUE_GEO_FIRMS_AREA_QGIS_TEMPLATE,
+}
+
+BLUE_GEO_TEST_DATACUBE_FIRMS_AREA = os.getenv(
+    "BLUE_GEO_TEST_DATACUBE_FIRMS_AREA",
+    "",
+)
+
+BLUE_GEO_TEST_DATACUBE_UKRAINE_TIMEMAP = os.getenv(
+    "BLUE_GEO_TEST_DATACUBE_UKRAINE_TIMEMAP",
+    "",
+)
