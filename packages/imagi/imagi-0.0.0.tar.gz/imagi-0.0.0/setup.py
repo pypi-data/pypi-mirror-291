69353be98e3897e17ba91ba4b8a0fb9ceb1e49b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:59 +0000

    Add package imagi/0.0.0/imagi-0.0.0.tar.gz

---
+++ packages/imagi/imagi-0.0.0.tar.gz/imagi-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imagi",
+    version="0.0.0",
+    packages=find_packages(),
+    author="Ethan Xu",
+    author_email="ethan@waitblock.simplelogin.com",
+    description="Coming soon",
+    long_description="Coming soon",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
+
