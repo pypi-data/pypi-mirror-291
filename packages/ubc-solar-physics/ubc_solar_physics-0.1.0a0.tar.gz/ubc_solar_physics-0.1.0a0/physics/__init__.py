d1f1bf873ae808de0dc12397f80ace17823f3262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:38 +0000

    Add package ubc-solar-physics/0.1.0a0/ubc_solar_physics-0.1.0a0.tar.gz

---
+++ packages/ubc-solar-physics/ubc_solar_physics-0.1.0a0.tar.gz/ubc_solar_physics-0.1.0a0/physics/__init__.py
@@ -0,0 +1,14 @@
+from .environment import (
+    openweather_environment,
+    solcast_environment,
+    weather_forecasts,
+    gis
+)
+
+from .models import (
+    arrays,
+    battery,
+    lvs,
+    motor,
+    regen
+)
