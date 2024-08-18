1f2d52609c30762ae9c55228216500209d8c0b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:23 +0000

    Add package my-browsepy/0.0.2/my_browsepy-0.0.2.tar.gz

---
+++ packages/my-browsepy/my_browsepy-0.0.2.tar.gz/my_browsepy-0.0.2/README.rst
@@ -0,0 +1,4 @@
+browsepy
+========
+
+The simple web file browser and the only change that i made is to get read of the sys.stderr, sys.stdout and redicted them to /dev/null
---
+++ packages/my-browsepy/my_browsepy-0.0.2.tar.gz/my_browsepy-0.0.2/browsepy/__meta__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+app = 'my_browsepy'
+description = 'Simple web file browser'
+version = '0.0.2'
+license = 'MIT'
+author_name = 'Felipe A. Hernandez'
+author_mail = 'ergoithz@gmail.com'
+author = '%s <%s>' % (author_name, author_mail)
+url = 'https://github.com/ergoithz/browsepy'
+tarball = '%s/archive/%s.tar.gz' % (url, version)
