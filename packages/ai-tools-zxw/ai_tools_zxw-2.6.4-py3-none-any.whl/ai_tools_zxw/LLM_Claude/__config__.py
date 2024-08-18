14515da5cab31e252a8465e20aa70e0b55767bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:03:05 +0000

    Add package ai-tools-zxw/2.6.4/ai_tools_zxw-2.6.4-py3-none-any.whl

---
+++ packages/ai-tools-zxw/ai_tools_zxw-2.6.4-py3-none-any.whl/ai_tools_zxw/LLM_Claude/__config__.py
@@ -0,0 +1,9 @@
+"""
+# File       : __config__.py
+# Time       ：2024/8/14 上午10:36
+# Author     ：xuewei zhang
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+"""
+api_key = "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"
