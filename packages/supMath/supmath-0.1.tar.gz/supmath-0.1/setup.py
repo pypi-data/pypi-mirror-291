38fe7de60a459a33d090baf0ada1e0b9a5dfdcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:04:01 +0000

    Add package supMath/0.1/supmath-0.1.tar.gz

---
+++ packages/supMath/supmath-0.1.tar.gz/supmath-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="supMath",
+    version="0.1",
+    description = "Provides vast amount of unique mathematical functions and operators",
+    author = "landee",
+    packages=find_packages(),
+)
