fcdf0e09d5b4383175d66e01a30bc054b6da7c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:26 +0000

    Add package oarepo-communities/5.0.5/oarepo_communities-5.0.5-py3-none-any.whl

---
+++ packages/oarepo-communities/oarepo_communities-5.0.5-py3-none-any.whl/oarepo_communities/ext_config.py
@@ -0,0 +1,7 @@
+from oarepo_communities.services.permissions.policy import (
+    CommunityWorkflowPermissionPolicy,
+)
+
+OAREPO_PERMISSIONS_PRESETS = {
+    "community-workflow": CommunityWorkflowPermissionPolicy,
+}
