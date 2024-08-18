5903e820506a1d50b4f900f68316316ffee1fae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:36 +0000

    Add package yta-multimedia/0.0.25/yta_multimedia-0.0.25-py3-none-any.whl

---
+++ packages/yta-multimedia/yta_multimedia-0.0.25-py3-none-any.whl/yta_multimedia/resources/video/effect/sound/drive_urls.py
@@ -0,0 +1,2 @@
+SAD_MOMENT_GOOGLE_DRIVE_DOWNLOAD_URL = 'https://drive.google.com/file/d/17rw299LIMTEciS7WDgFbIEZ7DckpHsC7/view?usp=sharing'
+PHOTO_GOOGLE_DRIVE_DOWNLOAD_URL = 'https://drive.google.com/file/d/1xr6x_yeU9SF5guB6KA9cLqbmVUwxBMi8/view?usp=sharing'
