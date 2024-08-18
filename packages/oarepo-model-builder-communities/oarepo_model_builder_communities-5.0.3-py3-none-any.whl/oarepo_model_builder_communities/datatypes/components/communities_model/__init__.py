de4244c8fb26009046f81036a5388d24f89d4b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:51 +0000

    Add package oarepo-model-builder-communities/5.0.3/oarepo_model_builder_communities-5.0.3-py3-none-any.whl

---
+++ packages/oarepo-model-builder-communities/oarepo_model_builder_communities-5.0.3-py3-none-any.whl/oarepo_model_builder_communities/datatypes/components/communities_model/__init__.py
@@ -0,0 +1,8 @@
+from .communities_metadata import CommunityMetadataModelComponent
+from .service import RecordCommunitiesServiceModelComponent
+
+
+__all__ = [
+    "RecordCommunitiesServiceModelComponent",
+    "CommunityMetadataModelComponent",
+]
