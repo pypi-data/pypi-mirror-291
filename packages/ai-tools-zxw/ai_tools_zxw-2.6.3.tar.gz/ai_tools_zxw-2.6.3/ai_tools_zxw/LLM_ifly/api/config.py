e5c8b071ffb82a750892c5453b6b466be10d4d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:52 +0000

    Add package ai-tools-zxw/2.6.3/ai_tools_zxw-2.6.3.tar.gz

---
+++ packages/ai-tools-zxw/ai_tools_zxw-2.6.3.tar.gz/ai_tools_zxw-2.6.3/ai_tools_zxw/LLM_ifly/api/config.py
@@ -0,0 +1,7 @@
+appid = "5a921ed1"
+api_secret = "ed4211f7f00494a59679474004ac6029"
+api_key = "b96ff33516c84e865a7cd06c0759691c"  # 位移
+
+
+self_model_serviceId = "xscnllama2"
+self_model_ws_url = "wss://xingcheng-api.cn-huabei-1.xf-yun.com/v1.1/chat"
