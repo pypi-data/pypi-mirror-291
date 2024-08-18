0ac1b544272bafeb1afb5671d92494e35a4f6754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 03:03:16 +0000

    Add package fluidsim/0.8.2/fluidsim-0.8.2-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/fluidsim/fluidsim-0.8.2-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/fluidsim/solvers/ns3d/strat/__numba__/solver.py
@@ -0,0 +1,12 @@
+
+from numba import njit
+
+
+@njit(cache=True, fastmath=True)
+def compute_fb_fft(div_vb_fft, N, vz_fft):
+    fb_fft = div_vb_fft
+    fb_fft[:] = -div_vb_fft - N ** 2 * vz_fft
+    return fb_fft
+
+
+__transonic__ = '0.7.1'
