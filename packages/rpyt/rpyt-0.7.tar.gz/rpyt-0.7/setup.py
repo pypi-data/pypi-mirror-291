0487ff3e3ef7655a4dc123b27e633677b8968e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 14:02:58 +0000

    Add package rpyt/0.7/rpyt-0.7.tar.gz

---
+++ packages/rpyt/rpyt-0.7.tar.gz/rpyt-0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rpyt",  # Name of your package
+    version="0.7",  # Initial version
+    packages=find_packages(),  # Automatically find your packages
+    include_package_data=True,  # Include additional files
+    entry_points={
+        "console_scripts": [
+            "rpyt=rpyt.__main__:main",  # Assuming you have a main function in __main__.py
+        ],
+    },
+    package_data={
+        "": ["*.txt"],  # Include all .txt files in the package
+    },
+)
