0dd60634b7b548ad5b6a23b541ecb1895171ac9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-18 15:03:29 +0000

    Add package easywindcss/1.2/easywindcss-1.2.tar.gz

---
+++ packages/easywindcss/easywindcss-1.2.tar.gz/easywindcss-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easywindcss',
+    version='1.2',  # Update this version as needed
+    description='A Python library to automate Tailwind CSS setup in your python environment.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Sayed Afaq',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts":[
+            "easywindcss = easywindcss:install",
+            "easywindcss-v = easywindcss:check_et"
+        ]
+    }
+)
