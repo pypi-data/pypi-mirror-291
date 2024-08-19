75b3183fad3d796fbc2a3b3341e5717449d337b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:56 +0000

    Add package cubedpandas/0.2.9/cubedpandas-0.2.9-py3-none-any.whl

---
+++ packages/cubedpandas/cubedpandas-0.2.9-py3-none-any.whl/cubedpandas/__init__.py
@@ -0,0 +1,59 @@
+# CubedPandas - Copyright (c)2024 by Thomas Zeutschler, BSD 3-clause license, see LICENSE file.
+
+from cubedpandas.schema import Schema
+from cubedpandas.measure import Measure
+from cubedpandas.measure_collection import MeasureCollection
+from cubedpandas.dimension import Dimension
+from cubedpandas.dimension_collection import DimensionCollection
+from cubedpandas.caching_strategy import CachingStrategy
+from cubedpandas.cube import Cube
+from cubedpandas.cube_aggregation import CubeAggregationFunctionType, CubeAggregationFunction
+from cubedpandas.member import Member, MemberSet
+
+from cubedpandas.slice import Slice
+from cubedpandas.pandas_extension import CubedPandasAccessor, EAGER_CACHING_THRESHOLD
+from cubedpandas.ambiguities import Ambiguities
+from cubedpandas.common import cubed
+
+import cubedpandas.context as context
+from cubedpandas.context.context import Context
+from cubedpandas.context.compound_context import CompoundContext
+from cubedpandas.context.context_context import ContextContext
+from cubedpandas.context.cube_context import CubeContext
+from cubedpandas.context.filter_context import FilterContext
+from cubedpandas.context.measure_context import MeasureContext
+from cubedpandas.context.member_context import MemberContext
+from cubedpandas.context.member_not_found_context import MemberNotFoundContext
+
+
+__version__ = "0.2.9"
+VERSION = __version__
+
+__all__ = [
+    "Ambiguities",
+    "CachingStrategy",
+    "context",
+    "Context",
+    "ContextContext",
+    "CompoundContext",
+    "CubeContext",
+    "Cube",
+    "CubeAggregationFunctionType",
+    "CubeAggregationFunction",
+    "cubed",
+    "CubedPandasAccessor",
+    "Dimension",
+    "DimensionCollection",
+    "EAGER_CACHING_THRESHOLD",
+    "FilterContext",
+    "Measure",
+    "MeasureCollection",
+    "MeasureContext",
+    "Member",
+    "MemberContext",
+    "MemberNotFoundContext",
+    "MemberSet",
+    "Schema",
+    "Slice",
+    "VERSION"
+]
