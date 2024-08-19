88e158df02d6fa2878a49f1bc86e661997959eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 05:03:43 +0000

    Add package packitup/1.0.3/packitup-1.0.3.tar.gz

---
+++ packages/packitup/packitup-1.0.3.tar.gz/packitup-1.0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="packitup",
+    version="1.0.3",
+    author="James Fincher",
+    author_email="james@fincher.dev",
+    description="A tool to generate project structure and contents as Markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamesfincher/packitup",
+    packages=find_packages(),
+    install_requires=[
+        "pyperclip",
+        "colorama",
+        "pyyaml",
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3",
+    entry_points={
+        "console_scripts": [
+            "packitup=packitup.packitup:main",
+        ],
+    },
+)
