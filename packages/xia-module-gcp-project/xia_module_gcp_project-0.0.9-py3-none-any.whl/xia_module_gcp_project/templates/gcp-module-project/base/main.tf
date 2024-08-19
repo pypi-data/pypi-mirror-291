57ed4734aba5895867500f60f11b02c810e290af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:02 +0000

    Add package xia-module-gcp-project/0.0.9/xia_module_gcp_project-0.0.9-py3-none-any.whl

---
+++ packages/xia-module-gcp-project/xia_module_gcp_project-0.0.9-py3-none-any.whl/xia_module_gcp_project/templates/gcp-module-project/base/main.tf
@@ -0,0 +1,14 @@
+module "gcp_module_project" {
+  source = "../../modules/gcp-module-project"
+
+  module_name = "gcp-module-project"
+  config_file = "../../../config/infra/gcp-project.yaml"
+  landscape = local.landscape
+  applications = local.applications
+  modules = local.modules
+  environment_dict = local.environment_dict
+  app_env_config = local.app_env_config
+  module_app_to_activate = local.module_app_to_activate
+
+  depends_on = [module.gh_module_application]
+}
