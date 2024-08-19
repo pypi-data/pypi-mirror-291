5209744e5aee05fe8b7e810ad44c84ab8be2d744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:10 +0000

    Add package pygameGL-devex/0.1.0.post1/pygamegl_devex-0.1.0.post1.tar.gz

---
+++ packages/pygameGL-devex/pygamegl_devex-0.1.0.post1.tar.gz/pygamegl_devex-0.1.0.post1/pygameGL/deps.py
@@ -0,0 +1,25 @@
+import ctypes
+from ctypes import c_void_p
+
+def load_SoGL() -> callable|None:
+    lib = None
+    if __import__('sys').platform == 'win32':
+        lib = ctypes.CDLL('./ext/SoGL/sogl64.dll')
+
+    # error loading SoGL library!
+    if lib == None: return lib
+    else:
+        # configure SoGL functions
+        lib['soglInit'].restype = c_void_p
+
+    return lib
+
+def load_SwarmECS() -> callable|None:
+    try:
+        import swarm as swarmECS
+    except ImportError as err:
+        print(f"Error Importing Swarm-ECS: Please Install it with `pip install -U Swarm-ECS`\n{err}")
+        return None
+    
+    return swarmECS
+
