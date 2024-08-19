af80c9c0ac4541f76cddb93e479bb2ce4460061e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:32 +0000

    Add package gaveta/0.5.0/gaveta-0.5.0-py3-none-any.whl

---
+++ packages/gaveta/gaveta-0.5.0-py3-none-any.whl/gaveta/seq.py
@@ -0,0 +1,17 @@
+from collections.abc import Callable
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def replace_or_append(
+    values: list[T], new_value: T, key: Callable[[T, T], bool]
+) -> list[T]:
+    for index, value in enumerate(values):
+        if key(value, new_value):
+            values[index] = new_value
+            break
+    else:
+        values.append(new_value)
+
+    return values
