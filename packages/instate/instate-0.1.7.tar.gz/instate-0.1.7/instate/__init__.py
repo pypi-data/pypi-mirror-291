d5bf175b81d906df28b2b67279d6f3d5fb4cc136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:32 +0000

    Add package instate/0.1.7/instate-0.1.7.tar.gz

---
+++ packages/instate/instate-0.1.7.tar.gz/instate-0.1.7/instate/__init__.py
@@ -0,0 +1,3 @@
+from .instate import last_state, pred_last_state, state_to_lang, list_states, lookup_lang, predict_lang
+
+__all__ = ["last_state", "pred_last_state", "state_to_lang", "list_states", "lookup_lang", "predict_lang"]
