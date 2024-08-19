112770f6b407e0af4e3cb8b92236f0d5ae4e1a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:49 +0000

    Add package zuu/0.1.8b1/zuu-0.1.8b1.tar.gz

---
+++ packages/zuu/zuu-0.1.8b1.tar.gz/zuu-0.1.8b1/src/zuu/__init__.py
@@ -0,0 +1,7 @@
+from .core.does_nothing import NothingInstance
+from .core.file import has_hidden_attribute
+from .core.timely_property import timely_property, timely_cls_property
+from .core.bit_operation import xor_encrypt
+from .core.string import is_fstring, extract_fstring_keys
+from .core.dicts import flatten_nested_dict, parse_dotted_dict
+from .core.cls_property import classProperty
