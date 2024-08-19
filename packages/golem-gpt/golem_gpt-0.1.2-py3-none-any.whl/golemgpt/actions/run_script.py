0401a91412de633bf0bd24560f180ba7e11b72c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:22 +0000

    Add package golem-gpt/0.1.2/golem_gpt-0.1.2-py3-none-any.whl

---
+++ packages/golem-gpt/golem_gpt-0.1.2-py3-none-any.whl/golemgpt/actions/run_script.py
@@ -0,0 +1,29 @@
+import subprocess
+from golemgpt.utils import workpath
+
+
+def run_script_action(name: str, **kwargs) -> str:
+    """Run a script and return its output."""
+    path = workpath(name)
+
+    if not path.exists():
+        return (
+            f"Script {name} does not exist, please create if first "
+            "and use a proper extension like .sh or .py"
+        )
+
+    if name.endswith('.py'):
+        process = subprocess.Popen(['python', name])
+    elif name.endswith('.sh'):
+        process = subprocess.Popen(['sh', name])
+    else:
+        return f"Rejected: script {name} is not a Bash or Python script."
+
+    process.wait()
+    is_ok = (process.returncode == 0)
+
+    return (
+        f"Script {name} executed successfully."
+        if is_ok else
+        f"Script {name} failed."
+    )
