0dd5f6f5937154b3a7d5bebdae33174979d3328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:19 +0000

    Add package metocean-stats/1.0.0/metocean_stats-1.0.0.tar.gz

---
+++ packages/metocean-stats/metocean_stats-1.0.0.tar.gz/metocean_stats-1.0.0/README.md
@@ -0,0 +1,14 @@
+# metocean-stats
+
+[![Tests (python)](https://github.com/MET-OM/metocean-stats/actions/workflows/tests.yml/badge.svg)](https://github.com/MET-OM/metocean-stats/actions/workflows/tests.yml)
+[![Documentation Status](https://readthedocs.org/projects/metocean-stats/badge/?version=latest)](https://metocean-stats.readthedocs.io/en/latest/?badge=latest)	[![Documentation Status](https://readthedocs.org/projects/metocean-stats/badge/?version=latest)](https://metocean-stats.readthedocs.io/en/latest/?badge=latest)
+
+## Purpose
+Metocen stastistics
+
+## Quick Installation
+
+$ pip install metocean-stats 
+
+## Docs
+Information about the package can be found at the [documentation](https://metocean-stats.readthedocs.io/en/latest/index.html).
---
+++ packages/metocean-stats/metocean_stats-1.0.0.tar.gz/metocean_stats-1.0.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(here, 'version.py')).read())
+
+setuptools.setup(
+    name        = 'metocean-stats',
+    description = 'metocean-stats - Tool for generation of metocean statistics',
+    author      = 'Konstantinos Christakos MET Norway & NTNU',
+    url         = 'https://github.com/MET-OM/metocean-stats',
+    download_url = 'https://github.com/MET-OM/metocean-stats',
+    version = __version__,
+    license = 'GPLv3',
+    install_requires = [
+        'numpy>=1.17',
+        'matplotlib>=3.1',
+        'pandas',
+        'pip',
+        'scipy',
+        'pip',
+        'windrose',
+        'seaborn>=0.12.2',
+        'metocean-api',
+        'xarray',
+        'metocean-api',
+        'pyextremes',
+        'scikit-learn',
+        'keras',
+        'tensorflow',
+        'cartopy',
+    ],
+    packages = setuptools.find_packages(),
+    include_package_data = True,
+    setup_requires = ['setuptools_scm'],
+    tests_require = ['pytest'],
+    scripts = []
+)
