7fee86bc109e95574a82b1337c0e6f4a453dd8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:43 +0000

    Add package logger-by-format/0.1/logger_by_format-0.1.tar.gz

---
+++ packages/logger-by-format/logger_by_format-0.1.tar.gz/logger_by_format-0.1/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="logger_by_format",
+    version="0.1",
+    author="",
+    author_email="",
+    description="A simple logging utility for saving logs as JSON files.",
+    long_description_content_type="text/markdown",
+    url="",  
+    packages=find_packages(),
+  
+    python_requires=">=3.6",
+)
