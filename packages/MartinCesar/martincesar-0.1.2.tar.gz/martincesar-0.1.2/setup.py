7237c2ad05980a92b76d96075518e19eee7b050a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 05:03:42 +0000

    Add package MartinCesar/0.1.2/martincesar-0.1.2.tar.gz

---
+++ packages/MartinCesar/martincesar-0.1.2.tar.gz/martincesar-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MartinCesar',
+    version='0.1.2',    
+    packages=find_packages(), 
+    author='Martin',
+    author_email='tu.email@example.com',
+    description='Descripción de lo que hace tu librería',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+)
