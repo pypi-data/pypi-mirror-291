fc0ca59b40e29394674340e369a529ebb5cc41e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:38 +0000

    Add package lmo/0.14.1/lmo-0.14.1.tar.gz

---
+++ packages/lmo/lmo-0.14.1.tar.gz/lmo-0.14.1/lmo/typing/__init__.py
@@ -0,0 +1,21 @@
+"""Typing utilities, mostly meant for internal usage."""
+from ._core import (
+    AnyAWeights,
+    AnyFWeights,
+    AnyOrder,
+    AnyOrderND,
+    AnyTrim,
+    LComomentOptions,
+    LMomentOptions,
+)
+
+
+__all__ = (
+    'AnyAWeights',
+    'AnyFWeights',
+    'AnyOrder',
+    'AnyOrderND',
+    'AnyTrim',
+    'LComomentOptions',
+    'LMomentOptions',
+)
