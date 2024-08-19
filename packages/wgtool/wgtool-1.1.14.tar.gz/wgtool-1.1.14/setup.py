dc4124edcf8d5bfb394664f866a2098646eb3cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:34 +0000

    Add package wgtool/1.1.14/wgtool-1.1.14.tar.gz

---
+++ packages/wgtool/wgtool-1.1.14.tar.gz/wgtool-1.1.14/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="wgtool",
+    version="1.1.14",
+    author="rpcsp",
+    author_email="pcunha@hotmail.com",
+    description="WireGuard Configuration Tool",
+    license="https://github.com/rpcsp/wgtool/blob/main/LICENSE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rpcsp/wgtool",
+    project_urls={
+        "Project page": "https://github.com/rpcsp/wgtool",
+    },
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=["pydantic", "qrcode"],
+    entry_points="""\
+    [console_scripts]
+    wgtool = wgtool.cli:main
+    """,
+)
