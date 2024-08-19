df798363309296f48721005002c67acabec439b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:51 +0000

    Add package yhttp-sqlalchemy/1.0.1/yhttp_sqlalchemy-1.0.1-py3-none-any.whl

---
+++ packages/yhttp-sqlalchemy/yhttp_sqlalchemy-1.0.1-py3-none-any.whl/yhttp/ext/sqlalchemy/cli.py
@@ -0,0 +1,35 @@
+from sqlalchemy import text
+from easycli import SubCommand
+
+
+class CreateObjectsCommand(SubCommand):
+    __command__ = 'create'
+    __aliases__ = ['c']
+
+    def __call__(self, args):
+        app = args.application
+        app.db.initialize()
+        app.db.create_objects()
+        self.report_objects(args)
+        app.db.deinitialize()
+
+    def report_objects(self, args):
+        app = args.application
+        with app.db.engine.connect() as conn:
+            result = conn.execute(text('''
+                SELECT relname, relkind
+                FROM pg_class
+                WHERE relname !~ '^(pg|sql)_' AND relkind != 'v';
+            '''))
+
+            print('Following objects has been created successfully:')
+            for name, kind in result.fetchall():
+                print(kind, name)
+
+
+class DatabaseObjectsCommand(SubCommand):
+    __command__ = 'objects'
+    __aliases__ = ['obj', 'o']
+    __arguments__ = [
+        CreateObjectsCommand,
+    ]
