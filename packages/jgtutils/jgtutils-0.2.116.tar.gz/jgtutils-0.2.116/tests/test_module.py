8acafb22a51902e45e06bec26beeddd3fd9e3582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:45 +0000

    Add package jgtutils/0.2.116/jgtutils-0.2.116.tar.gz

---
+++ packages/jgtutils/jgtutils-0.2.116.tar.gz/jgtutils-0.2.116/tests/test_module.py
@@ -0,0 +1,7 @@
+import pytest
+from jgtutils import module
+
+def test_function():
+    result = module.function()  # replace 'function' with your actual function
+    expected_result =  # add your expected result here
+    assert result == expected_result
