0f27245a64aceea9022cb8c453f1bf3e9834537a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:42 +0000

    Add package MLStructFP/0.6.6/MLStructFP-0.6.6-py3-none-any.whl

---
+++ packages/MLStructFP/MLStructFP-0.6.6-py3-none-any.whl/MLStructFP/db/image/__init__.py
@@ -0,0 +1,8 @@
+"""
+MLSTRUCT-FP - DB - IMAGE
+
+Dataset image operations.
+"""
+
+from MLStructFP.db.image._rect_binary import RectBinaryImage
+from MLStructFP.db.image._rect_photo import RectFloorPhoto
