ff55a39aedfbc0b948e8dce3549a2e1e1c0e97ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:47 +0000

    Add package powerview/2024.6.6/powerview-2024.6.6.tar.gz

---
+++ packages/powerview/powerview-2024.6.6.tar.gz/powerview-2024.6.6/powerview/_version.py
@@ -0,0 +1,8 @@
+__year__ = "2024"
+__version__ = f"{__year__}.6.6"
+__author__ = [
+    "Aniq Fakhrul",
+    "Ali Radzali"
+]
+
+BANNER = "Powerview.py v{} ({}) - by {}\n".format(__version__, __year__, ", ".join(__author__))
---
+++ packages/powerview/powerview-2024.6.6.tar.gz/powerview-2024.6.6/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+setup(
+	name='powerview',
+	version='2024.6.6',
+	description='Python based PowerView script',
+	author='Aniq Fakhrul',
+	author_email='aniqfakhrull@gmail.com',
+	maintainer='Aniq Fakhrul',
+	maintainer_email='aniqfakhrull@gmail.com',
+	url='https://github.com/aniqfakhrul/powerview.py',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	packages=[
+		'powerview',
+        'powerview.utils',
+        'powerview.modules',
+        'powerview.lib'
+    ],
+	license='MIT',
+	install_requires=[
+		'impacket',
+		'ldap3-custom-requirements[kerberos]',
+		'dnspython',
+		'future',
+		'gnureadline',
+		'validators',
+		'dsinternals',
+		'chardet',
+		'tabulate',
+		'argparse',
+	],
+	classifiers=[
+		'Intended Audience :: Information Technology',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
+		'Programming Language :: Python :: 3.11',
+	],
+	entry_points= {
+		'console_scripts': ['powerview=powerview:main']
+	}
+)
