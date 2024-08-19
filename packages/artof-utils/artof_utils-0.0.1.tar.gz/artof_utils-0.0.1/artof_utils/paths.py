1edeb205f575001edd0c273667b92075fcea4aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:32 +0000

    Add package artof-utils/0.0.1/artof_utils-0.0.1.tar.gz

---
+++ packages/artof-utils/artof_utils-0.0.1.tar.gz/artof_utils-0.0.1/artof_utils/paths.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from os import path, getenv
+
+load_dotenv()
+
+ilvo_path = getenv('ILVO_PATH')
+if ilvo_path and path.exists(ilvo_path):
+    base = path.join(ilvo_path)
+    platform_settings = path.join(base, 'settings.json')
+    implements = path.join(base, 'implement')
+    fields = path.join(base, 'field')
