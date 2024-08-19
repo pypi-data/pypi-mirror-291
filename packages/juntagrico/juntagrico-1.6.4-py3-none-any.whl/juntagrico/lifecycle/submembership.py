b404d3bb18ecf168c3553e623e71d5db5c44ad33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:32 +0000

    Add package juntagrico/1.6.4/juntagrico-1.6.4-py3-none-any.whl

---
+++ packages/juntagrico/juntagrico-1.6.4-py3-none-any.whl/juntagrico/lifecycle/submembership.py
@@ -0,0 +1,66 @@
+import datetime
+
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from django.utils.translation import gettext as _
+
+from juntagrico.config import Config
+
+
+def sub_membership_pre_save(sender, instance, **kwargs):
+    if not kwargs.get('raw', False):
+        check_sub_membership_consistency(instance)
+
+
+def check_submembership_dates(instance):
+    has_joined = instance.join_date is not None
+    has_left = instance.leave_date is not None
+    join_date = instance.join_date or datetime.date.today()
+    leave_date = instance.leave_date or join_date  # allow future join dates
+    if has_left and not has_joined:
+        raise ValidationError(_('Bitte "Beitrittsdatum" ausfüllen'), code='invalid')
+    if not (join_date <= leave_date):
+        raise ValidationError(_('Datenreihenfolge stimmt nicht.'), code='invalid')
+
+
+def check_submembership_parent_dates(instance):
+    subscription = instance.subscription
+    s_activated = subscription.activation_date is not None
+    m_joined = instance.join_date is not None
+    s_deactivated = subscription.deactivation_date is not None
+    m_left = instance.leave_date is not None
+    wrong_start = (m_joined and s_activated and subscription.activation_date > instance.join_date) or (not s_activated and m_joined)
+    wrong_end = (m_left and s_deactivated and subscription.deactivation_date < instance.leave_date) or (s_deactivated and not m_left)
+    if wrong_start:
+        raise ValidationError(_('Beitrittsdatum des Bestandteils passt nicht zum übergeordneten Aktivierungsdatum'),
+                              code='part_activation_date_mismatch')
+    if wrong_end:
+        raise ValidationError(_('Austrittsdatum des Bestandteils passt nicht zum übergeordneten Deaktivierungsdatum'),
+                              code='part_deactivation_date_mismatch')
+
+
+def check_sub_membership_consistency(instance):
+    check_submembership_dates(instance)
+    if hasattr(instance, 'subscription'):
+        check_submembership_parent_dates(instance)
+        subscription = instance.subscription
+        if hasattr(instance, 'member'):
+            check_sub_membership_consistency_ms(instance.member, subscription, instance.join_date, instance.leave_date)
+
+
+def check_sub_membership_consistency_ms(member, subscription, join_date, leave_date):
+    from juntagrico.entity.member import SubscriptionMembership
+    # check for subscription membership overlaps
+    memberships = SubscriptionMembership.objects.exclude(subscription=subscription).filter(member=member)
+    if join_date is None:
+        check = Q(join_date__isnull=True)
+    elif leave_date is None:
+        check = Q(leave_date__isnull=True) | Q(leave_date__gte=join_date)
+    else:
+        check = Q(join_date__lte=leave_date, leave_date__gte=leave_date) | \
+            Q(join_date__lte=join_date, leave_date__gte=join_date)
+    if memberships.filter(check).exists():
+        raise ValidationError(
+            _('{} kann nur 1 {} gleichzeitig haben.').format(Config.vocabulary('member'),
+                                                             Config.vocabulary('subscription')),
+            code='invalid')
