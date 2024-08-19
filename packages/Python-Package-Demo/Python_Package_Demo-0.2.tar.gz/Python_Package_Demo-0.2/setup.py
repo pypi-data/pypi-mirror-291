3b345b2a056cbdae9ffaaff6691d17b1c1632e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:04:12 +0000

    Add package Python-Package-Demo/0.2/Python_Package_Demo-0.2.tar.gz

---
+++ packages/Python-Package-Demo/Python_Package_Demo-0.2.tar.gz/Python_Package_Demo-0.2/setup.py
@@ -0,0 +1,29 @@
+# This file contains instruction on how to bundle and publish the package.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Python_Package_Demo",  # This needs to match folder name.
+    version="0.2",  # Needs to be updated if new versions are releases by you.
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": ["Python-Package-Demo = Python_Package_Demo:hello"]
+    },
+)
+
+# After the above things are done type the following command in the terminal:
+# { python setup.py sdist bdist_wheel }
+
+# The command creates 2 distributions:
+# ** A source distribution which is generally the python scripts.
+# ** And a wheel distribution which contains binaries and are platform specific.
+
+# These archieves will have all the necessary for installing and running the package.
+
+# Rum the command below to test the installation of the package.
+
+# [ pip install dist/Python_Package_Demo-0.1-py3-none-any.whl ]
