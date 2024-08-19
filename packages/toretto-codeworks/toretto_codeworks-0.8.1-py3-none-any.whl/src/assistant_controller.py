f5434384aacef9b80a5eb8a6aebf9608aa63e2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:26 +0000

    Add package toretto-codeworks/0.8.1/toretto_codeworks-0.8.1-py3-none-any.whl

---
+++ packages/toretto-codeworks/toretto_codeworks-0.8.1-py3-none-any.whl/src/assistant_controller.py
@@ -0,0 +1,150 @@
+"""
+assistant controller
+"""
+
+from difflib import get_close_matches
+from prompt_toolkit.completion import Completer, Completion
+from src.models.address_book import AddressBook
+from src.book_controller import (
+    add_contact,
+    remove_contact,
+    remove_phone,
+    change_contact,
+    show_phone,
+    show_all,
+    add_birthday,
+    show_birthday,
+    birthdays,
+    add_note_to_contact,
+    edit_note_in_contact,
+    remove_note_from_contact,
+    show_notes_for_contact,
+    find_notes_by_keyword,
+    add_email,
+    add_address,
+    add_tag_to_contact,
+    show_tags_for_contact,
+    remove_tag_from_contact,
+    find_notes_by_tag,
+    show_all_sorted_tags,
+    show_all_notes_sorted_by_tags,
+)
+
+COMMANDS = {
+    "add": add_contact,
+    "remove-contact": remove_contact,
+    "remove-phone": remove_phone,
+    "change": change_contact,
+    "phone": show_phone,
+    "all": show_all,
+    "add-birthday": add_birthday,
+    "show-birthday": show_birthday,
+    "birthdays": birthdays,
+    "add-note": add_note_to_contact,
+    "edit-note": edit_note_in_contact,
+    "remove-note": remove_note_from_contact,
+    "show-notes": show_notes_for_contact,
+    "find-notes": find_notes_by_keyword,
+    "exit": "",
+    "close": "",
+    "hello": "",
+    "cls": "",
+    "clear": "",
+    "add-email": add_email,
+    "add-address": add_address,
+    "add-tag": add_tag_to_contact,
+    "show-tags": show_tags_for_contact,
+    "remove-tag": remove_tag_from_contact,
+    "find-notes-by-tag": find_notes_by_tag,
+    "show-all-tags": show_all_sorted_tags,
+    "show-all-notes-by-tags": show_all_notes_sorted_by_tags,
+}
+
+
+def suggest_command(command, available_commands):
+    """
+    Suggest the closest matching command based on user input.
+
+    Args:
+        command (str): The command to match.
+        available_commands (list): List of available command strings.
+
+    Returns:
+        str: The closest matching command.
+    """
+    matches = get_close_matches(command, available_commands, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+
+class CommandCompleter(Completer):
+    """
+    Command completer for providing dynamic suggestions based on input.
+
+    This class is designed to work with `prompt_toolkit` to offer command
+    suggestions as the user types. It suggests the closest matching commands
+    based on the text entered before the cursor.
+
+    Methods
+    -------
+    get_completions(document, complete_event)
+        Generates command completions based on the current input.
+
+    Parameters
+    ----------
+    document : Document
+        The `prompt_toolkit` document object representing the current state
+        of the text input, including the text before the cursor.
+
+    complete_event : CompleteEvent
+        The `prompt_toolkit` event object representing the current state of the
+        completion event, which includes flags and settings about the
+        completion process.
+
+    Yields
+    ------
+    Completion
+        A `Completion` object for each matching command, providing the
+        completion text and the position where it should be inserted.
+
+    """
+
+    def get_completions(self, document, complete_event):
+        # Split input to get the command part only
+        text_before_cursor = document.text_before_cursor
+        words = text_before_cursor.strip().split()
+
+        # Only suggest completions for the first word (command)
+        if len(words) == 1 and " " not in text_before_cursor:
+            matches = get_close_matches(
+                words[0].lower(), COMMANDS.keys(), n=5, cutoff=0.1
+            )
+            for match in matches:
+                yield Completion(match, start_position=-len(words[0]))
+
+
+def execute_command(command: str, args: list, book: AddressBook) -> str:
+    """
+    Execute the given command with the provided arguments and address book.
+
+    Args:
+        command (str): The command to execute.
+        args (list): The arguments for the command.
+        book (AddressBook): The address book data.
+
+    Returns:
+        str: The result of the command execution.
+    """
+
+    match command:
+        case "hello":
+            return "How can I help you?"
+
+        case command if command in COMMANDS:
+            return COMMANDS[command](args, book)
+
+        case _:
+            suggested_command = suggest_command(command, list(COMMANDS.keys()))
+            if suggested_command:
+                return f"Invalid command. Did you mean '{suggested_command}'?"
+
+            return "Invalid command."
---
+++ packages/toretto-codeworks/toretto_codeworks-0.8.1-py3-none-any.whl/src/models/record.py
@@ -0,0 +1,191 @@
+"""
+class Record
+"""
+
+import re
+from src.models.fields import (
+    Name,
+    Birthday,
+    Phone,
+    Note,
+    Email,
+    Address,
+    PhoneNumberValueError,
+    NoteValueError,
+)
+
+
+class Record:
+    """
+    Class for storing contact information, including name, phone numbers, and birthday.
+
+    Attributes:
+        name (Name): The contact's name.
+        phones (list of Phone): The contact's phone numbers.
+        birthday (Birthday): The contact's birthday.
+    """
+
+    def __init__(self, name: str):
+        """
+        Initializes the record with the contact's name.
+
+        Args:
+            name (str): The contact's name.
+        """
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+        self.notes = []
+        self.email = None
+        self.address = None
+
+    def add_birthday(self, birthday: str) -> None:
+        """
+        Adds a birthday to the record.
+
+        Args:
+            birthday (str): The birthday to add.
+        """
+        self.birthday = Birthday(birthday)
+
+    def add_phone(self, phone_number: str) -> None:
+        """
+        Adds a phone number to the record.
+
+        Args:
+            phone_number (str): The phone number to add.
+        """
+        self.phones.append(Phone(phone_number))
+
+    def remove_phone(self, phone_number: str) -> None:
+        """
+        Removes a phone number from the record.
+
+        Args:
+            phone_number (str): The phone number to remove.
+        """
+        phone_to_remove = self.find_phone(phone_number)
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+        else:
+            raise PhoneNumberValueError("Phone number not found")
+
+    def edit_phone(self, old_number: str, new_number: str) -> None:
+        """
+        Edits a phone number in the record.
+
+        Args:
+            old_number (str): The old phone number.
+            new_number (str): The new phone number.
+        """
+        phone_to_edit = self.find_phone(old_number)
+        if phone_to_edit:
+            if not re.fullmatch(r"\d{10}", new_number):
+                raise PhoneNumberValueError("Phone number must be 10 digits")
+            phone_to_edit.value = new_number
+        else:
+            raise PhoneNumberValueError("Phone number not found")
+
+    def find_phone(self, phone_number: str) -> Phone | None:
+        """
+        Finds a phone number in the record.
+
+        Args:
+            phone_number (str): The phone number to find.
+
+        Returns:
+            Phone: The phone number if found, or None.
+        """
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def add_note(self, note: str, name: str = None) -> None:
+        """
+        Adds a new note to the record with an optional name.
+
+        Args:
+            note (str): The content of the note to add.
+            name (str, optional): The name associated with the note.
+        """
+        self.notes.append(Note(note, name))
+
+    def edit_note(self, old_name: str, new_note: str) -> None:
+        """
+        Edits an existing note in the record by note name.
+
+        Args:
+            old_name (str): The name of the note to be replaced.
+            new_note (str): The new content to replace the old note.
+        Raises:
+            NoteValueError: If the new note is empty or the old note is not found.
+        """
+        for i, note in enumerate(self.notes):
+            if note.name == old_name:
+                if not new_note:
+                    raise NoteValueError("Note cannot be empty")
+                self.notes[i] = Note(new_note, note.name)
+                return
+        raise NoteValueError("Note with the given name not found")
+
+    def remove_note_by_name(self, name: str) -> None:
+        """
+        Removes a note from the record by name.
+
+        Args:
+            name (str): The name associated with the note to remove.
+
+        Raises:
+            NoteValueError: If no note with the given name is found.
+        """
+        for i, existing_note in enumerate(self.notes):
+            if existing_note.name == name:
+                del self.notes[i]
+                return
+
+        raise NoteValueError(f"Note with the name '{name}' not found")
+
+    def find_note_by_keyword(self, keyword: str) -> list:
+        """
+        Finds notes containing a specific keyword.
+
+        Args:
+            keyword (str): The keyword to search for in the notes.
+
+        Returns:
+            list: A list of notes that contain the keyword.
+        """
+        return [note for note in self.notes if keyword in note.value]
+
+    def add_email(self, email: str) -> None:
+        """
+        Adds a Email to the record.
+
+        Args:
+            email (str): The email to add.
+        """
+        self.email = Email(email)
+
+    def add_address(self, address: str) -> None:
+        """
+        Adds an address to the record.
+
+        Args:
+            address (str): The address to add.
+        """
+        self.address = Address(" ".join(address))
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the record.
+
+        Returns:
+            str: The string representation of the record.
+        """
+        phone_list = "; ".join(p.value for p in self.phones)
+        birthday_str = f" {self.birthday}" if self.birthday else ""
+        email_str = f" {self.email}" if self.email else "-"
+        address_str = f" {self.address}" if self.address else "-"
+        return f"Contact name: {self.name.value:<10}| phones: {phone_list:<10}|\
+              birthday: {birthday_str:<12}| email: {email_str:<10}| address: {address_str:<10}"
