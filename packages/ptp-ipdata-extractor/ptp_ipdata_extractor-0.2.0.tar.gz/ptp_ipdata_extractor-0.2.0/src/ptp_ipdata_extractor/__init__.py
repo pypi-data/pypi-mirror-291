64be5a5c299d0578d1c6204ce5cde5f5c4cb69e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:04:00 +0000

    Add package ptp-ipdata-extractor/0.2.0/ptp_ipdata_extractor-0.2.0.tar.gz

---
+++ packages/ptp-ipdata-extractor/ptp_ipdata_extractor-0.2.0.tar.gz/ptp_ipdata_extractor-0.2.0/src/ptp_ipdata_extractor/__init__.py
@@ -0,0 +1,11 @@
+from .ip_extractor import (
+    get_available_ips_releases,
+    get_ip_release_data,
+    get_latest_data_per_ip
+)
+
+__all__ = [
+    "get_available_ips_releases",
+    "get_ip_release_data",
+    "get_latest_data_per_ip"
+]
