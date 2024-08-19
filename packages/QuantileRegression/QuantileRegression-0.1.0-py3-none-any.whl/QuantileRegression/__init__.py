e7b74c67c63a96062fd4310a7e352f0e53ab40a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:23 +0000

    Add package QuantileRegression/0.1.0/QuantileRegression-0.1.0-py3-none-any.whl

---
+++ packages/QuantileRegression/QuantileRegression-0.1.0-py3-none-any.whl/QuantileRegression/__init__.py
@@ -0,0 +1,3 @@
+"""QuantileRegression class : for given data and probabilities fits a basis of functions."""
+from QuantileRegression.QuantileRegression import QuantileRegressionFit
+from QuantileRegression.QuantileRegression import quantile_regression_fit
