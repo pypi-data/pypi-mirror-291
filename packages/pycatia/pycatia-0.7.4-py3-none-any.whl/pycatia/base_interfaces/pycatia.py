836f2a0e652ca0945e320e749a720392f084ddd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:29 +0000

    Add package pycatia/0.7.4/pycatia-0.7.4-py3-none-any.whl

---
+++ packages/pycatia/pycatia-0.7.4-py3-none-any.whl/pycatia/base_interfaces/pycatia.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3.9
+
+import logging
+
+from pycatia.cat_logger import create_logger
+
+
+class PyCATIA:
+    """
+    Base class for the catia application.
+    """
+
+    def __init__(self):
+        pass
+
+    @property
+    def logger(self) -> logging.Logger:
+        """
+        :rtype: logging.Logger
+        """
+        return create_logger()
+
+    def __repr__(self):
+        return 'PyCATIA()'
