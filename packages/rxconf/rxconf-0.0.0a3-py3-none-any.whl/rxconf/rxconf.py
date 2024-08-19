8371415800c060ff8c195d8938c2b88cf7f22ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:36 +0000

    Add package rxconf/0.0.0a3/rxconf-0.0.0a3-py3-none-any.whl

---
+++ packages/rxconf/rxconf-0.0.0a3-py3-none-any.whl/rxconf/rxconf.py
@@ -0,0 +1,31 @@
+import pathlib
+import typing as tp
+
+from rxconf import config_resolver, config_types
+
+
+class RxConf:
+
+    def __init__(
+        self: "RxConf",
+        config: config_types.ConfigType,
+    ) -> None:
+        self._config = config
+
+    @classmethod
+    def from_file(
+        cls: tp.Type["RxConf"],
+        config_path: tp.Union[str, pathlib.PurePath],
+        file_config_resolver: config_resolver.FileConfigResolver = config_resolver.DefaultFileConfigResolver,
+    ) -> "RxConf":
+        return cls(
+            config=file_config_resolver.resolve(
+                path=config_path,
+            )
+        )
+
+    def __getattr__(self, item: str) -> tp.Any:
+        return getattr(self._config, item)
+
+    def __repr__(self) -> str:
+        return repr(self._config)
