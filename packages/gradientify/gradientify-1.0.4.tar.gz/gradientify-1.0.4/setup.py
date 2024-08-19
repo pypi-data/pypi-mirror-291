bc5a891c1b61419a3c176029b774436157bbf7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:27 +0000

    Add package gradientify/1.0.4/gradientify-1.0.4.tar.gz

---
+++ packages/gradientify/gradientify-1.0.4.tar.gz/gradientify-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='gradientify',
+    version='1.0.4',
+    author='knelly999',
+    description='A Python package for gradient prints or inputs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/knelly999/gradientify',
+    packages=find_packages(),
+    python_requires='>=3.11',
+)
