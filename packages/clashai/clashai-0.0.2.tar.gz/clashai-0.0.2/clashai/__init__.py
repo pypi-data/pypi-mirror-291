3532829964c4809c9c462ac1b096009b98d6f8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:52 +0000

    Add package clashai/0.0.2/clashai-0.0.2.tar.gz

---
+++ packages/clashai/clashai-0.0.2.tar.gz/clashai-0.0.2/clashai/__init__.py
@@ -0,0 +1,28 @@
+__version__ = "0.0.2"
+
+import requests
+
+class Client:
+    def __init__(self, api_key: str, model: str = None, base_url: str = None):
+        self.api_key = api_key
+        self.model = model
+        self.base_url =  base_url if base_url else "http://clashai.3utilities.com:25621"
+
+    def make_request(self, messages: list):
+        sys_prompt = {"role": "system", "content": self.system_prompt if self.system_prompt else "You are a helpful assistant."}
+        model = self.model if self.model else "chatgpt-4o-latest"
+        api_key = self.api_key
+        endpoint = "v1/chat/completions"
+        url = f"{self.base_url}/{endpoint}"
+        payload = {
+                "model": model,
+                "messages": messages
+                                                                ,
+            }
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
+        }
+        
+        response = requests.post(url, headers=headers, json=payload)
+        return response.json()
