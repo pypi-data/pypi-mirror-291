66d20f78001463bfca272c10f1a1b61c81ff7c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:58 +0000

    Add package qtest-swagger-client/0.0.3/qtest_swagger_client-0.0.3-py3-none-any.whl

---
+++ packages/qtest-swagger-client/qtest_swagger_client-0.0.3-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.attachment_api import AttachmentApi
+from swagger_client.api.auth_systems_api import AuthSystemsApi
+from swagger_client.api.automation_job_api import AutomationJobApi
+from swagger_client.api.build_api import BuildApi
+from swagger_client.api.common_api import CommonApi
+from swagger_client.api.defect_api import DefectApi
+from swagger_client.api.field_api import FieldApi
+from swagger_client.api.groups_api import GroupsApi
+from swagger_client.api.integration_settings_api import IntegrationSettingsApi
+from swagger_client.api.login_api import LoginApi
+from swagger_client.api.module_api import ModuleApi
+from swagger_client.api.object_link_api import ObjectLinkApi
+from swagger_client.api.project_api import ProjectApi
+from swagger_client.api.release_api import ReleaseApi
+from swagger_client.api.requirement_api import RequirementApi
+from swagger_client.api.search_api import SearchApi
+from swagger_client.api.site_templates_api import SiteTemplatesApi
+from swagger_client.api.test_case_api import TestCaseApi
+from swagger_client.api.test_cycle_api import TestCycleApi
+from swagger_client.api.test_log_api import TestLogApi
+from swagger_client.api.test_run_api import TestRunApi
+from swagger_client.api.test_suite_api import TestSuiteApi
+from swagger_client.api.tosca_api import ToscaApi
+from swagger_client.api.user_api import UserApi
+from swagger_client.api.user_profile_api import UserProfileApi
+from swagger_client.api.webhook_api import WebhookApi
