5aff575d6db302e1e953b5d4a0cdf55300113b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:49 +0000

    Add package json-log-utils/0.2/json_log_utils-0.2.tar.gz

---
+++ packages/json-log-utils/json_log_utils-0.2.tar.gz/json_log_utils-0.2/json_log_utils/__init__.py
@@ -0,0 +1,3 @@
+# json_file_logger/__init__.py
+
+from .logger import Logger
---
+++ packages/json-log-utils/json_log_utils-0.2.tar.gz/json_log_utils-0.2/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="json_log_utils",
+    version="0.2",
+    author="",
+    author_email="",
+    description="A simple logging utility for saving logs as JSON files.",
+    long_description_content_type="text/markdown",
+    url="",  
+    packages=find_packages(),
+  
+    python_requires=">=3.6",
+)
