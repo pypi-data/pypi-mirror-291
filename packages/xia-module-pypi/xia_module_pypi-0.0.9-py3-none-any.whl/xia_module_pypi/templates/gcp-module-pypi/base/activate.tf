d6a6308664b98643670b86503bdd10786504947c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:00 +0000

    Add package xia-module-pypi/0.0.9/xia_module_pypi-0.0.9-py3-none-any.whl

---
+++ packages/xia-module-pypi/xia_module_pypi-0.0.9-py3-none-any.whl/xia_module_pypi/templates/gcp-module-pypi/base/activate.tf
@@ -0,0 +1,15 @@
+module "activate_gcp_module_pypi" {
+  source = "../../modules/activate-gcp-module-pypi"
+
+  module_name = "gcp-module-pypi"
+  landscape = local.landscape
+  applications = local.applications
+  modules = local.modules
+  environment_dict = local.environment_dict
+  app_env_config = local.app_env_config
+  module_app_to_activate = local.module_app_to_activate
+
+  gcp_projects = module.gcp_module_project.gcp_projects
+  depends_on = [module.gcp_module_project, module.gh_module_application]
+}
+
