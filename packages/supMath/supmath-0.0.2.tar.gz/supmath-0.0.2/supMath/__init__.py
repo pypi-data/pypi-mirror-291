d9a41a76f4b84846d53b46dfb7ccfde8b1bd5a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:34 +0000

    Add package supMath/0.0.2/supmath-0.0.2.tar.gz

---
+++ packages/supMath/supmath-0.0.2.tar.gz/supmath-0.0.2/supMath/__init__.py
@@ -0,0 +1,18 @@
+"""
+Provides vast amount of unique mathematical functions and operators:
+ 
+    counting
+    --------
+            funcs and operators used in scientific and math formulas
+
+    converting
+    ----------
+            notation scale and measure-to-measure converters 
+
+    geometry
+    --------
+        formulas used in various geometrical tasks
+"""
+from .advConverters import *
+from .advGeometry import *
+from .advOperators import *
