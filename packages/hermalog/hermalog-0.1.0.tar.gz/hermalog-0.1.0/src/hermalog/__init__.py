982c6dd7d44c7e32c8214dfdb6a2ff78aa402d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 05:03:26 +0000

    Add package hermalog/0.1.0/hermalog-0.1.0.tar.gz

---
+++ packages/hermalog/hermalog-0.1.0.tar.gz/hermalog-0.1.0/src/hermalog/__init__.py
@@ -0,0 +1,10 @@
+from .customLogging import CustomLogging
+from .simpleLogging import SimpleLogging
+from .configurations import BaseLoggingConfiguration, CustomLoggingConfiguration
+
+__all__ = [
+    "CustomLogging",
+    "SimpleLogging",
+    "BaseLoggingConfiguration",
+    "CustomLoggingConfiguration",
+]
