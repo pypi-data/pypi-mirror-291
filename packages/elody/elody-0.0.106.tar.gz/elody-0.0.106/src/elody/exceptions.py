2f565eef7ec8bab92fe8c395c1bb0e011a6b693b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:49 +0000

    Add package elody/0.0.106/elody-0.0.106.tar.gz

---
+++ packages/elody/elody-0.0.106.tar.gz/elody-0.0.106/src/elody/exceptions.py
@@ -0,0 +1,46 @@
+class DuplicateFileException(Exception):
+    def __init__(self, message, filename=None, md5sum=None):
+        super().__init__(message)
+        self.message = message
+        self.filename = filename
+        self.md5sum = md5sum
+
+
+class ColumnNotFoundException(Exception):
+    pass
+
+
+class FileNotFoundException(Exception):
+    pass
+
+
+class IncorrectTypeException(Exception):
+    pass
+
+
+class InvalidExtensionException(Exception):
+    pass
+
+
+class InvalidObjectException(Exception):
+    pass
+
+
+class NoMediafilesException(Exception):
+    pass
+
+
+class NonUniqueException(Exception):
+    pass
+
+
+class NotFoundException(Exception):
+    pass
+
+
+class NoTenantException(Exception):
+    pass
+
+
+class UnsupportedVersionException(Exception):
+    pass
