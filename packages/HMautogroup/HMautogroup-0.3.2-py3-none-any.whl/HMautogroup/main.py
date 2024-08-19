194c0b506cead510340de7ad48117b6a2750155a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:29 +0000

    Add package HMautogroup/0.3.2/HMautogroup-0.3.2-py3-none-any.whl

---
+++ packages/HMautogroup/HMautogroup-0.3.2-py3-none-any.whl/HMautogroup/main.py
@@ -0,0 +1,8 @@
+from text_processing import create_dataframe_advanced
+from similarity import group_products, update_grouped_titles
+
+def main(df, title, product_type_dict, stopwords, color_list, attributes):
+    Filtered_DF = create_dataframe_advanced(df, title, product_type_dict, stopwords, color_list, attributes)
+    grouped_df = group_products(Filtered_DF)
+    updated_df = update_grouped_titles(grouped_df)
+    return updated_df
