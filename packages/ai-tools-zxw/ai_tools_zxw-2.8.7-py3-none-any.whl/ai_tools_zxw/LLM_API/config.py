6fc302bdcc5dc9b58a4a65cedb8a844c27256c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:18 +0000

    Add package ai-tools-zxw/2.8.7/ai_tools_zxw-2.8.7-py3-none-any.whl

---
+++ packages/ai-tools-zxw/ai_tools_zxw-2.8.7-py3-none-any.whl/ai_tools_zxw/LLM_API/config.py
@@ -0,0 +1,24 @@
+"""
+# File       : config.py
+# Time       ：2024/8/18 下午4:45
+# Author     ：xuewei zhang
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+"""
+#
+baidu_appid = "107737802"
+baidu_access_key = "d4547e7b0e34463b9715b1ffdfb152a1"
+baidu_secret_key = "b0ab5886ec8942a3bf30ecf153a18f2f"
+
+#
+ifly_appid = "5a921ed2"
+ifly_api_key = "ed4211f7f00494a59679474004ac6020"
+ifly_secret_key = "b96ff33516c84e865a7cd06c0759691d"
+
+ifly_self_model_serviceId = "xscnllama2"
+ifly_self_model_ws_url = "wss://xingcheng-api.cn-huabei-1.xf-yun.com/v1.1/chat"
+
+#
+claude_api_key = "sk-ant-api03-DNkgDiAup6lpzsULwVOp9hKw6w-LCa4hPc3m1CNkbltZrGsZTUnpfIxiUalP0EBU8m1KcHYaqGMnYI-lL558Dg-55HsnAAB"
+
