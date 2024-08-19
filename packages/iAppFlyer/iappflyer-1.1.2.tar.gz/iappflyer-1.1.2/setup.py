21f9632de2d69441d0b398649a56e7ec76067de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:33 +0000

    Add package iAppFlyer/1.1.2/iappflyer-1.1.2.tar.gz

---
+++ packages/iAppFlyer/iappflyer-1.1.2.tar.gz/iappflyer-1.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='iAppFlyer',
+    version='1.1.2',
+    packages=['iAppFlyer'],
+    entry_points={
+        'console_scripts': [
+            'iAppFlyer=iAppFlyer.main:main',
+        ],
+    },
+    install_requires=[
+        'Pillow',
+        'requests',
+    ],
+    include_package_data=True,
+    package_data={
+        '': ['*.png'],
+    },
+    zip_safe=False,
+)
