14545ca3359554ff116598610c2ddcf877dff52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:41 +0000

    Add package cdmtb/0.2.4/cdmtb-0.2.4-py3-none-any.whl

---
+++ packages/cdmtb/cdmtb-0.2.4-py3-none-any.whl/cdmtb/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present Rui Hirokawa
+#
+# SPDX-License-Identifier: MIT
+from .cdmtb import cdia, g2c, c2g, g2t, aref
+
+__all__ = ['cdia', 'g2c', 'c2g', 'g2t', 'aref']
