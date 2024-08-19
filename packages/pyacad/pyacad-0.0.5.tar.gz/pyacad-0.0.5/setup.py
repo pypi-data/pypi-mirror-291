3ab585529dd461dec0e71c8fba373b8d3286c7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:04:13 +0000

    Add package pyacad/0.0.5/pyacad-0.0.5.tar.gz

---
+++ packages/pyacad/pyacad-0.0.5.tar.gz/pyacad-0.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+readme = open("./README.md", "r")
+
+setup(name="pyacad",
+      packages=["pyacad"],
+      version="0.0.5",
+      description="A package aimed to simplfy coding of Activex Automation Module of AutoCAD, based on pywin32 library.",
+      long_description = readme.read(),
+      long_description_content_type="text/markdown",
+      author="Kevin Axel Tagliaferri",
+      author_email='kevinaxeltagliaferri@hotmail.com',
+      url="https://github.com/AxelTAG/pyacad.git",
+      install_requires=['pywin32'])
