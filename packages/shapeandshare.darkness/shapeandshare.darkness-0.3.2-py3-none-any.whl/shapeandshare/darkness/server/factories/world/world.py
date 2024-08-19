2afc42f7fda2ba598332ccd09c5b675242970ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:03 +0000

    Add package shapeandshare.darkness/0.3.2/shapeandshare.darkness-0.3.2-py3-none-any.whl

---
+++ packages/shapeandshare.darkness/shapeandshare.darkness-0.3.2-py3-none-any.whl/shapeandshare/darkness/server/factories/world/world.py
@@ -0,0 +1,17 @@
+import uuid
+
+from pydantic import BaseModel
+
+from ....sdk.contracts.dtos.tiles.world import World
+from ...dao.world import WorldDao
+
+
+class WorldFactory(BaseModel):
+    worlddao: WorldDao
+
+    async def create(self, name: str | None = None) -> str:
+        if name is None:
+            name = "darkness"
+        world: World = World(id=str(uuid.uuid4()), name=name)
+        await self.worlddao.post(tokens={"world_id": world.id}, document=world)
+        return world.id
