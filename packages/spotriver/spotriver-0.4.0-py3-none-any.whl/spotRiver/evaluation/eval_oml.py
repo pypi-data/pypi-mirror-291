bdc0ba011f5009549bc2c624765d1e370774a758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 15:03:40 +0000

    Add package spotriver/0.4.0/spotriver-0.4.0-py3-none-any.whl

---
+++ packages/spotriver/spotriver-0.4.0-py3-none-any.whl/spotRiver/evaluation/eval_oml.py
@@ -0,0 +1,218 @@
+import matplotlib.pyplot as plt
+from river.evaluate import iter_progressive_val_score
+from spotpython.utils.progress import progress_bar
+from numpy import mean
+from numpy import zeros
+from numpy import array
+
+
+def eval_oml_iter_progressive(dataset, metric, models, step=100, weight_coeff=0.0, log_level=50):
+    """Evaluate OML Models on Streaming Data
+
+    This function evaluates one or more OML models on a streaming dataset. The evaluation
+    is done iteratively, and the models are tested every `step` iterations. The results
+    are returned as a dictionary of metrics and their values.
+
+    Args:
+        dataset (list or river.Stream): A list of river.Stream objects containing the
+            streaming data to be evaluated. If a single river.Stream object is provided,
+            it is automatically converted to a list.
+        metric (river.metrics.base.MultiClassMetric or river.metrics.base.RegressionMetric):
+            The metric to be used for evaluation.
+        models (dict): A dictionary of OML models to be evaluated. The keys are the names
+            of the models, and the values are the model objects.
+        step (int): Iteration number at which to yield results. This only takes into account
+            the predictions, and not the training steps. Defaults to 100.
+        weight_coeff (float): Results are multiplied by (step/n_steps)**weight_coeff,
+            where n_steps is the total number of iterations. Results from the beginning have
+            a lower weight than results from the end if weight_coeff > 1. If weight_coeff == 0,
+            then results are multiplied by 1 and every result has an equal weight. Defaults to 0.0.
+        log_level (int): The level of logging to use. 0 = no logging, 50 = print only important
+            information. Defaults to 50.
+
+    Returns:
+        (dict): A dictionary containing the evaluation results. The keys are the names of the
+            models, and the values are dictionaries with the following keys:
+            - "step": A list of iteration numbers at which the model was evaluated.
+            - "error": A list of the weighted errors for each iteration.
+            - "r_time": A list of the weighted running times for each iteration.
+            - "memory": A list of the weighted memory usages for each iteration.
+            - "metric_name": The name of the metric used for evaluation.
+
+    Reference:
+        https://riverml.xyz/0.15.0/recipes/on-hoeffding-trees/
+
+    Examples:
+        >>> from river import compose
+            from river import linear_model
+            from river import preprocessing, datasets, utils, metrics
+            import matplotlib.pyplot as plt
+            from spotriver.utils.features import get_ordinal_date
+            from spotriver.evaluation.eval_nowcast import eval_nowcast_model, plot_nowcast_model
+            model = compose.Pipeline(
+                ('ordinal_date', compose.FuncTransformer(get_ordinal_date)),
+                ('scale', preprocessing.StandardScaler()),
+                ('lin_reg', linear_model.LinearRegression())
+            )
+            dataset = datasets.AirlinePassengers()
+            dates, metric, y_trues, y_preds = eval_nowcast_model(model, dataset=dataset)
+            plot_nowcast_model(dates, metric, y_trues, y_preds)
+
+    """
+    metric_name = metric.__class__.__name__
+    # Convert dataset to a list if needed
+    if dataset.__class__ != list:
+        dataset = [dataset]
+    n_steps = len(dataset)
+    result = {}
+    for model_name, model in models.items():
+        result_i = {"step": [], "error": [], "r_time": [], "memory": []}
+        for checkpoint in iter_progressive_val_score(
+            dataset, model, metric, measure_time=True, measure_memory=True, step=step
+        ):
+            if log_level <= 20:
+                progress_bar(checkpoint["Step"] / n_steps, message="Eval iter_prog_val_score:")
+            w = (checkpoint["Step"] / n_steps) ** weight_coeff
+            result_i["step"].append(checkpoint["Step"])
+            result_i["error"].append(w * checkpoint[metric_name].get())
+            # Convert timedelta object into seconds
+            result_i["r_time"].append(w * checkpoint["Time"].total_seconds())
+            # Make sure the memory measurements are in MB
+            raw_memory = checkpoint["Memory"]
+            result_i["memory"].append(w * raw_memory * 2**-20)
+        result_i["metric_name"] = metric_name
+        result[model_name] = result_i
+    return result
+
+
+def plot_oml_iter_progressive(result, log_x=False, log_y=False, figsize=None, filename=None):
+    """Plot evaluation of OML models.
+
+    Args:
+        result (dict): A dictionary of evaluation results, as returned by eval_oml_iter_progressive.
+        log_x (bool, optional): If True, the x-axis is set to log scale. Defaults to False.
+        log_y (bool, optional): If True, the y-axis is set to log scale. Defaults to False.
+        figsize (tuple, optional): The size of the figure. Defaults to None, in which case
+            the default figure size `(10, 5)` is used.
+        filename (str, optional): The name of the file to save the plot to. If None, the plot
+            is not saved. Defaults to None.
+
+    Returns:
+        (matplotlib.figure.Figure): The figure object.
+
+    Reference:
+        https://riverml.xyz/0.15.0/recipes/on-hoeffding-trees/
+
+    Examples:
+        >>> from spotriver.evaluation.eval_oml import plot_oml_iter_progressive
+
+        >>> result = {
+        ...     "model1": {
+        ...         "step": [1, 2, 3],
+        ...         "error": [0.1, 0.2, 0.3],
+        ...         "r_time": [0.1, 0.2, 0.3],
+        ...         "memory": [0.1, 0.2, 0.3],
+        ...         "metric_name": "MAE"
+        ...     },
+        ...     "model2": {
+        ...         "step": [1, 2, 3],
+        ...         "error": [0.2, 0.3, 0.4],
+        ...         "r_time": [0.2, 0.3, 0.4],
+        ...         "memory": [0.2, 0.3, 0.4],
+        ...         "metric_name": "MAE"
+        ...     }
+        ... }
+
+        >>> plot_oml_iter_progressive(result)
+        <Figure size 1000x500 with 3 Axes>
+
+    """
+    if figsize is None:
+        figsize = (10, 5)
+    fig, ax = plt.subplots(figsize=figsize, nrows=3, dpi=300)
+    for model_name, model in result.items():
+        ax[0].plot(model["step"], model["error"], label=model_name)
+        ax[1].plot(model["step"], model["r_time"], label=model_name)
+        ax[2].plot(model["step"], model["memory"], label=model_name)
+
+    ax[0].set_ylabel(model["metric_name"])
+    ax[1].set_ylabel("Time (seconds)")
+    ax[2].set_ylabel("Memory (MB)")
+    ax[2].set_xlabel("Instances")
+
+    ax[0].grid(True)
+    ax[1].grid(True)
+    ax[2].grid(True)
+    if log_y:
+        ax[0].set_yscale("log")
+        ax[1].set_yscale("log")
+        ax[2].set_yscale("log")
+
+    ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.25), ncol=3, fancybox=True, shadow=True)
+    plt.tight_layout()
+    plt.close()
+    if filename is not None:
+        fig.savefig(filename, dpi=300)
+    return fig
+
+
+def fun_eval_oml_iter_progressive(result, metric=None, weights=None):
+    """Wrapper function for eval_oml_iter_progressive, returning a single function value.
+
+    Args:
+        result (dict): A dictionary of evaluation results, as returned by eval_oml_iter_progressive.
+        metric (function, optional): The metric function to use for computing the function value.
+            Defaults to None, in which case the mean function is used.
+        weights (numpy.array, optional): An array of weights for error, r_time, and memory.
+            If None, the weights are set to [1, 0, 0], which considers only the error.
+            Defaults to None.
+
+    Returns:
+        (numpy.array): An array of function values, one for each model in the evaluation results.
+
+    Raises:
+        ValueError: If the weights array is not of length 3.
+
+    Reference:
+        https://riverml.xyz/0.15.0/recipes/on-hoeffding-trees/
+
+    Examples:
+        >>> from spotriver.evaluation.eval_oml import fun_eval_oml_iter_progressive
+
+        >>> result = {
+        ...     "model1": {
+        ...         "step": [1, 2, 3],
+        ...         "error": [0.1, 0.2, 0.3],
+        ...         "r_time": [0.1, 0.2, 0.3],
+        ...         "memory": [0.1, 0.2, 0.3],
+        ...         "metric_name": "MAE"
+        ...     },
+        ...     "model2": {
+        ...         "step": [1, 2, 3],
+        ...         "error": [0.2, 0.3, 0.4],
+        ...         "r_time": [0.2, 0.3, 0.4],
+        ...         "memory": [0.2, 0.3, 0.4],
+        ...         "metric_name": "MAE"
+        ...     }
+        ... }
+
+        >>> fun_eval_oml_iter_progressive(result)
+        array([0.1, 0.2])
+
+    """
+    if metric is None:
+        metric = mean
+    if weights is None:
+        weights = array([1, 0, 0])
+    if len(weights) != 3:
+        raise ValueError("The weights array must be of length 3.")
+    model_names = list(result.keys())
+    n = len(model_names)
+    y = zeros([n])
+    for i in range(n):
+        y[i] = (
+            weights[0] * metric(result[model_names[i]]["error"])
+            + weights[1] * metric(result[model_names[i]]["r_time"])
+            + weights[2] * metric(result[model_names[i]]["memory"])
+        )
+    return y
---
+++ packages/spotriver/spotriver-0.4.0-py3-none-any.whl/spotRiver/utils/data_conversion.py
@@ -0,0 +1,171 @@
+from river import datasets
+import pandas as pd
+from tabulate import tabulate
+from sklearn.model_selection import train_test_split
+from numpy.typing import ArrayLike
+from math import inf
+
+
+def convert_to_df(dataset: datasets.base.Dataset, target_column: str = "y", n_total: int = None) -> pd.DataFrame:
+    """Converts a river dataset into a pandas DataFrame.
+
+    Args:
+        dataset (datasets.base.Dataset):
+            The river dataset to be converted.
+        target_column (str):
+            The name of the target column in the resulting DataFrame.
+            Defaults to "y".
+        n_total (int, optional):
+            The number of samples to be converted.
+            If set to None or inf, the full dataset is converted.
+            Defaults to None, i.e, the full dataset is converted.
+
+    Returns:
+        (pd.DataFrame): A pandas DataFrame representation of the given dataset.
+
+    Examples:
+        >>> from river import datasets
+            from spotriver.utils.data_conversion import convert_to_df
+            dataset = datasets.TrumpApproval()
+            target_column = "Approval"
+            df = convert_to_df(dataset, target_column)
+            df.rename(columns={
+                'date': 'ordinal_date',
+                'Gallup': 'gallup',
+                'Ipsos': 'ipsos',
+                'Morning Consult': 'morning_consult',
+                'Rasmussen': 'rasmussen',
+                'YouGov': 'you_gov'},
+                inplace=True)
+            # Split the data into train and test sets
+            train = df[:500]
+            test = df[500:]
+    """
+    data_dict = {key: [] for key in list(dataset.take(1))[0][0].keys()}
+    data_dict[target_column] = []
+    if n_total is None or n_total == inf:
+        for x in dataset:
+            for key, value in x[0].items():
+                data_dict[key].append(value)
+            data_dict[target_column].append(x[1])
+    else:
+        for x in dataset.take(n_total):
+            for key, value in x[0].items():
+                data_dict[key].append(value)
+            data_dict[target_column].append(x[1])
+    df = pd.DataFrame(data_dict)
+    return df
+
+
+def compare_two_tree_models(model1, model2, headers=["Parameter", "Default", "Spot"]):
+    """Compares two tree models and returns a table of the differences.
+    Args:
+        model1 (Pipeline): A river model pipeline.
+        model2 (Pipeline): A river model pipeline.
+    Returns:
+        (str): A table of the differences between the two models.
+    """
+    keys = model1[1].summary.keys()
+    values1 = model1[1].summary.values()
+    values2 = model2[1].summary.values()
+    tbl = []
+    for key, value1, value2 in zip(keys, values1, values2):
+        tbl.append([key, value1, value2])
+    return tabulate(tbl, headers=headers, numalign="right", tablefmt="github")
+
+
+def rename_df_to_xy(df, target_column="y"):
+    """Renames the columns of a DataFrame to x1, x2, ..., xn, y.
+
+    Args:
+        df (pd.DataFrame):
+            The DataFrame to be renamed.
+        target_column (str, optional):
+            The name of the target column. Defaults to "y".
+
+    Returns:
+        (pd.DataFrame): The renamed DataFrame.
+
+    Examples:
+        >>> from spotriver.utils.data_conversion import rename_df_to_xy
+            df = pd.DataFrame({
+            "feature1": [1, 2, 3],
+            "feature2": [4, 5, 6],
+            "target": [7, 8, 9]
+        })
+        >>> df = rename_df_to_xy(df, "target")
+        >>> print(df)
+           x1  x2  y
+        0   1   4  7
+        1   2   5  8
+        2   3   6  9
+    """
+    n_features = len(df.columns) - 1
+    df.columns = [f"x{i}" for i in range(1, n_features + 1)] + [target_column]
+    return df
+
+
+def split_df(
+    dataset: pd.DataFrame, test_size: float, seed: int, stratify: ArrayLike, shuffle=True, target_type: str = None
+) -> tuple:
+    """
+    Split a pandas DataFrame into a training and a test set.
+
+    Args:
+        dataset (pd.DataFrame):
+            The input data set.
+        test_size (float):
+            The percentage of the data set to be used as test set.
+            If float, should be between 0.0 and 1.0 and represent the proportion
+            of the dataset to include in the test split.
+            If int, represents the absolute number of test samples.
+            If None, the value is set to the complement of the train size.
+            If train_size is also None, it will be set to 0.25.
+        target_type (str):
+            The type of the target column. Can be "int", "float" or None.
+            If None, the type of the target column is not changed.
+            Otherwise, the target column is converted to the specified type.
+        seed (int):
+            The seed for the random number generator.
+        stratify (ArrayLike):
+            The array of target values.
+        shuffle (bool, optional):
+            Whether or not to shuffle the data before splitting. Defaults to True.
+
+    Returns:
+        tuple: The tuple (train, test, n_samples).
+
+    Examples:
+        >>> from spotriver.utils.data_conversion import split_df
+            df = pd.DataFrame({
+            "feature1": [1, 2, 3],
+            "feature2": [4, 5, 6],
+            "target": [7, 8, 9]})
+            train, test, n_samples = split_df(df, 0.2, "int", 42)
+
+    """
+    # Rename the columns of a DataFrame to x1, x2, ..., xn, y.
+    # From now on we assume that the target column is called "y":
+    df = rename_df_to_xy(df=dataset, target_column="y")
+    if target_type == "float":
+        df["y"] = df["y"].astype(float)
+    elif target_type == "int":
+        df["y"] = df["y"].astype(int)
+    else:
+        pass
+    target_column = "y"
+    # split the data set into a training and a test set,
+    # where the test set is a percentage of the data set given as test_size:
+    X = df.drop(columns=[target_column])
+    Y = df[target_column]
+    # Split the data into training and test sets
+    # test_size is the percentage of the data that should be held over for testing
+    # random_state is a seed for the random number generator to make your train and test splits reproducible
+    train_features, test_features, train_target, test_target = train_test_split(
+        X, Y, test_size=test_size, random_state=seed, shuffle=shuffle, stratify=stratify
+    )
+    # combine the training features and the training target into a training DataFrame
+    train = pd.concat([train_features, train_target], axis=1)
+    test = pd.concat([test_features, test_target], axis=1)
+    n_samples = train.shape[0] + test.shape[0]
+    return train, test, n_samples
