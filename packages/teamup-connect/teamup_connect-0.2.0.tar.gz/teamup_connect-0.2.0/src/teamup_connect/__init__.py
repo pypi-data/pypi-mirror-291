b1013d344c6f0044c014f6279d6f92d1edd2c645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:18 +0000

    Add package teamup-connect/0.2.0/teamup_connect-0.2.0.tar.gz

---
+++ packages/teamup-connect/teamup_connect-0.2.0.tar.gz/teamup_connect-0.2.0/src/teamup_connect/__init__.py
@@ -0,0 +1,6 @@
+"""This module serves as the entry point for the package. It initializes the necessary components and provides a convenient interface for accessing the functionality of the package.
+
+For more details on how to use this package, please refer to the `teamup_api.py` file.
+"""
+
+from .teamup_api import DEFAULT_CALENDAR_ID, TeamUP
