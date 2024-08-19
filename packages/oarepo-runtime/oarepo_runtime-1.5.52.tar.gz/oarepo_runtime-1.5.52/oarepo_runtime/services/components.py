62e9e0eea5f0998f4b3fc9a7bdd9b0c016f2c5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:33 +0000

    Add package oarepo-runtime/1.5.52/oarepo_runtime-1.5.52.tar.gz

---
+++ packages/oarepo-runtime/oarepo_runtime-1.5.52.tar.gz/oarepo_runtime-1.5.52/oarepo_runtime/services/components.py
@@ -0,0 +1,53 @@
+from invenio_accounts.models import User
+
+from oarepo_runtime.services.generators import RecordOwners
+
+try:
+    from invenio_drafts_resources.services.records.uow import ParentRecordCommitOp
+except ImportError:
+    from invenio_records_resources.services.uow import (
+        RecordCommitOp as ParentRecordCommitOp,
+    )
+
+from invenio_records_resources.services.records.components import ServiceComponent
+
+
+class OwnersComponent(ServiceComponent):
+    def create(self, identity, *, record, **kwargs):
+        """Create handler."""
+        self.add_owner(identity, record)
+
+    def add_owner(self, identity, record, commit=False):
+        if not hasattr(identity, "id") or not isinstance(identity.id, int):
+            return
+
+        owners = getattr(record.parent, "owners", None)
+        if owners is not None:
+            user = User.query.filter_by(id=identity.id).first()
+            record.parent.owners.add(user)
+            if commit:
+                self.uow.register(ParentRecordCommitOp(record.parent))
+
+    def update(self, identity, *, record, **kwargs):
+        """Update handler."""
+        self.add_owner(identity, record, commit=True)
+
+    def update_draft(self, identity, *, record, **kwargs):
+        """Update handler."""
+        self.add_owner(identity, record, commit=True)
+
+    def search_drafts(self, identity, search, params, **kwargs):
+        new_term = RecordOwners().query_filter(identity)
+        if new_term:
+            return search.filter(new_term)
+        return search
+
+
+from datetime import datetime
+
+
+class DateIssuedComponent(ServiceComponent):
+    def publish(self, identity, data=None, record=None, errors=None, **kwargs):
+        """Create a new record."""
+        if "dateIssued" not in record["metadata"]:
+            record["metadata"]["dateIssued"] = datetime.today().strftime("%Y-%m-%d")
