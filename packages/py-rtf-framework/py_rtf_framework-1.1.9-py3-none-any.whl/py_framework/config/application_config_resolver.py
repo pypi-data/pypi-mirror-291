385797a0fa34b564ed246fd4ec86d9ed04ca0c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:43 +0000

    Add package py-rtf-framework/1.1.9/py_rtf_framework-1.1.9-py3-none-any.whl

---
+++ packages/py-rtf-framework/py_rtf_framework-1.1.9-py3-none-any.whl/py_framework/config/application_config_resolver.py
@@ -0,0 +1,19 @@
+from .base_config_resolver import BaseConfigResolver
+from py_framework.py_constants import AppProfile
+
+
+class ApplicationConfigResolver(BaseConfigResolver):
+    """启动配置解析器"""
+    base_dir: str
+
+    profile: AppProfile
+
+    def __init__(self, profile: AppProfile, base_dir: str = './'):
+        super().__init__('application')
+        self.base_dir = base_dir if base_dir.endswith('/') else base_dir + '/'
+        self.profile = profile
+        # 开始装载配置
+        self.load_config()
+
+    def load_config(self):
+        self.load_config_from_yml(self.base_dir + 'application-' + self.profile + '.yml')
