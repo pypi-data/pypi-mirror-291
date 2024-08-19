a3162f4bc69d23eb078c4cf4701003b8240ea8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:21 +0000

    Add package trunco/0.3.1/trunco-0.3.1.tar.gz

---
+++ packages/trunco/trunco-0.3.1.tar.gz/trunco-0.3.1/trunco/__init__.py
@@ -0,0 +1,12 @@
+from .base import Component
+from .enums import Attribute, Directive, Method, Trigger, HxMethod, Swap
+
+__all__ = [
+    "Component",
+    "Attribute",
+    "Directive",
+    "Method",
+    "Trigger",
+    "HxMethod",
+    "Swap",
+]
