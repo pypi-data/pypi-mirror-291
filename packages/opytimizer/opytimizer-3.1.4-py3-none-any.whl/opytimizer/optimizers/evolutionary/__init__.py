3d0f7df1bac002b65f5b36124d3822b70d4a62ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:05 +0000

    Add package opytimizer/3.1.4/opytimizer-3.1.4-py3-none-any.whl

---
+++ packages/opytimizer/opytimizer-3.1.4-py3-none-any.whl/opytimizer/optimizers/evolutionary/__init__.py
@@ -0,0 +1,15 @@
+"""An evolutionary package for all common opytimizer modules.
+It contains implementations of evolutionary-based optimizers.
+"""
+
+from opytimizer.optimizers.evolutionary.bsa import BSA
+from opytimizer.optimizers.evolutionary.de import DE
+from opytimizer.optimizers.evolutionary.ep import EP
+from opytimizer.optimizers.evolutionary.es import ES
+from opytimizer.optimizers.evolutionary.foa import FOA
+from opytimizer.optimizers.evolutionary.ga import GA
+from opytimizer.optimizers.evolutionary.gp import GP
+from opytimizer.optimizers.evolutionary.gsgp import GSGP
+from opytimizer.optimizers.evolutionary.hs import GHS, GOGHS, HS, IHS, NGHS, SGHS
+from opytimizer.optimizers.evolutionary.iwo import IWO
+from opytimizer.optimizers.evolutionary.rra import RRA
