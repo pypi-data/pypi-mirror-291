61837852f472b0a3c1f3e95e8b8a03237d1b2a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:10 +0000

    Add package torch-onnx/0.1.13/torch_onnx-0.1.13-py3-none-any.whl

---
+++ packages/torch-onnx/torch_onnx-0.1.13-py3-none-any.whl/torch_onnx/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    "ONNXRegistry",
+    "ONNXProgram",
+    "analyze",
+    "export",
+    "exported_program_to_ir",
+    "patch_torch",
+    "unpatch_torch",
+    "verify_onnx_program",
+    "testing",
+]
+
+from . import _testing as testing
+from ._analysis import analyze
+from ._core import export, exported_program_to_ir
+from ._onnx_program import ONNXProgram
+from ._patch import patch_torch, unpatch_torch
+from ._registration import ONNXRegistry
+from ._verification import verify_onnx_program
