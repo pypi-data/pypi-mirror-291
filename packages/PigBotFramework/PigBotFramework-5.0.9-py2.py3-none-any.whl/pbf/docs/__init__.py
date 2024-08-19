ad750882de8ca74e842a4172ca941d84967d8e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:27 +0000

    Add package PigBotFramework/5.0.9/PigBotFramework-5.0.9-py2.py3-none-any.whl

---
+++ packages/PigBotFramework/PigBotFramework-5.0.9-py2.py3-none-any.whl/pbf/docs/__init__.py
@@ -0,0 +1,18 @@
+"""
+# PigBotFramework文档
+**目录**
+- `pbf`模块结构
+- `pbf`使用详解
+- `pbf.controller`使用详解
+- `pbf.driver`使用详解
+- `pbf.model`使用详解
+- `pbf.statement`使用详解
+- `pbf.utils`使用详解
+- **注意事项**
+
+
+# `pbf`模块结构
+
+
+# `pbf`使用详解
+"""
