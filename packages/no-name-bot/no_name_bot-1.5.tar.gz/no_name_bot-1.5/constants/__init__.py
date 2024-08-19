8c9997a828f9cdb3e5a27a100735aa80e910bf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:55 +0000

    Add package no-name-bot/1.5/no_name_bot-1.5.tar.gz

---
+++ packages/no-name-bot/no_name_bot-1.5.tar.gz/no_name_bot-1.5/constants/__init__.py
@@ -0,0 +1,19 @@
+"""This module provides constants."""
+
+from constants.assistant_info import (
+    ADDRESSBOOK_INFO_TABLE_DATA,
+    ADDRESSBOOK_INFO_TABLE_HEADERS,
+    NOTEBOOK_INFO_TABLE_DATA,
+    NOTEBOOK_INFO_TABLE_HEADERS,
+)
+
+from constants.auto_complete_cl import COMPLETER, COMMAND_CORRECTIONS
+
+__all__ = [
+    "ADDRESSBOOK_INFO_TABLE_DATA",
+    "ADDRESSBOOK_INFO_TABLE_HEADERS",
+    "NOTEBOOK_INFO_TABLE_DATA",
+    "NOTEBOOK_INFO_TABLE_HEADERS",
+    "COMPLETER",
+    "COMMAND_CORRECTIONS",
+]
