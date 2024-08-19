65995fa63fefef0faaba31936dc09285656f5106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 05:03:43 +0000

    Add package tink-http-python/1.2.4/tink_http_python-1.2.4-py3-none-any.whl

---
+++ packages/tink-http-python/tink_http_python-1.2.4-py3-none-any.whl/tink_http_python/authentication/storage.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Storage:
+    @abstractmethod
+    def store_new_refresh_token_refresh_token(self, new_refresh_token) -> str:
+        pass
+
+    @abstractmethod
+    def retrieve_refresh_token(self) -> str:
+        pass
+
+    @abstractmethod
+    def retrieve_authorization_code(self) -> str:
+        pass
+
+    @abstractmethod
+    def store_new_authorization_code(self, new_authorization_code: str) -> str:
+        pass
