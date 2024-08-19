4d55e12f19cb4a0e50c6da8d5162aabaf5ae29fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:44 +0000

    Add package pf-2023b-neural-decoding/0.0.11/pf_2023b_neural_decoding-0.0.11.tar.gz

---
+++ packages/pf-2023b-neural-decoding/pf_2023b_neural_decoding-0.0.11.tar.gz/pf_2023b_neural_decoding-0.0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pf_2023b_neural_decoding',
+    version='0.0.11',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.26.4'
+    ],
+    author='Gonzalo Beade, Mila Langone, Santiago Hadad',
+    author_email='gbeade@itba.edu.ar, mlangone@itba.edu.ar, shadad@itba.edu.ar',
+    description="""
+        A package containing all exported artifacts from the pf-2023b-neural-decoding final graduate project.
+    """,
+    url='https://bitbucket.org/itba/pf-2023b-neural-decoding/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
