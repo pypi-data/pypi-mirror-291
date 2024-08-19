2f194a97c61076b65861986ef8926a0c7e14c6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:28 +0000

    Add package nice-go/0.2.0/nice_go-0.2.0-py3-none-any.whl

---
+++ packages/nice-go/nice_go-0.2.0-py3-none-any.whl/nice_go/_exceptions.py
@@ -0,0 +1,21 @@
+"""Exceptions for Nice G.O. API."""
+
+
+class NiceGOError(Exception):
+    """Base exception for Nice G.O. API."""
+
+
+class NoAuthError(NiceGOError):
+    """Not authenticated exception."""
+
+
+class ApiError(NiceGOError):
+    """API error."""
+
+
+class AuthFailedError(NiceGOError):
+    """Authentication failed. Check your credentials."""
+
+
+class WebSocketError(NiceGOError):
+    """WebSocket error."""
