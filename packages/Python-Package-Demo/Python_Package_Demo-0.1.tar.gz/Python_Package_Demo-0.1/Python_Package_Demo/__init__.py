668d121d5bc312e955b1fddc6b941c5c94e2991d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:04:12 +0000

    Add package Python-Package-Demo/0.1/Python_Package_Demo-0.1.tar.gz

---
+++ packages/Python-Package-Demo/Python_Package_Demo-0.1.tar.gz/Python_Package_Demo-0.1/Python_Package_Demo/__init__.py
@@ -0,0 +1,5 @@
+from .main import hello
+
+# This file does 2 things:
+# ** This tells python that the whole directory here is a package
+# ** It controls what is available when the package is imported.
