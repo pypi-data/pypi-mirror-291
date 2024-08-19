dfbfeae3fd163cc64f53a0d5071b4b3f11cf36d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:02:58 +0000

    Add package rxconf/0.0.0a4/rxconf-0.0.0a4-py3-none-any.whl

---
+++ packages/rxconf/rxconf-0.0.0a4-py3-none-any.whl/rxconf/types.py
@@ -0,0 +1,27 @@
+import sys
+import typing as tp
+from datetime import date, datetime
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+
+YAML_ATTRIBUTE_TYPE: TypeAlias = tp.Union[
+    tp.Union[bool, int, str, float, None],
+    tp.List[tp.Union[bool, int, str, float, None]],
+    tp.Set[tp.Union[bool, int, str, float, None]],
+    tp.Union[date, datetime],
+]
+
+JSON_ATTRIBUTE_TYPE: TypeAlias = tp.Union[
+    tp.Union[bool, int, str, float, None],
+    tp.List[tp.Union[bool, int, str, float, None]],
+]
+
+TOML_ATTRIBUTE_TYPE: TypeAlias = tp.Union[
+    tp.Union[bool, int, str, float],
+    tp.List[tp.Union[bool, int, str, float]],
+    tp.Union[date, datetime],
+]
