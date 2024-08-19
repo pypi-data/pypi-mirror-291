3666e36274d8a2ee45c5c2a0e86a8563bc72c973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 14:03:25 +0000

    Add package xcodex/0.0.7/xcodex-0.0.7.tar.gz

---
+++ packages/xcodex/xcodex-0.0.7.tar.gz/xcodex-0.0.7/xcodex/Util/check.py
@@ -0,0 +1,21 @@
+from netCDF4 import Dataset
+
+
+def check_date(calendar_list, xco2_netCDF4:Dataset) -> bool:
+    """
+    This routine will check if the calendar year matches with the current file
+    """
+
+    if str(calendar_list.year[0]) > str(xco2_netCDF4['time'].begin_date)[0:4]:
+        flag = True
+
+    else:
+        if str(calendar_list.month[0]) > str(xco2_netCDF4['time'].begin_date)[4:6].lstrip("0"):
+            flag = True
+        else:
+            if str(calendar_list.day[0]) > str(xco2_netCDF4['time'].begin_date)[6:].lstrip("0"):
+                flag = True
+            else:
+                flag = False
+
+    return flag
