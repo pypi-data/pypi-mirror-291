5c4a9cad4df10851a1edc2dc1bb3acfe851a948e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 03:03:52 +0000

    Add package AlekSIS-App-Chronos/4.0.0.dev5/aleksis_app_chronos-4.0.0.dev5.tar.gz

---
+++ packages/AlekSIS-App-Chronos/aleksis_app_chronos-4.0.0.dev5.tar.gz/aleksis_app_chronos-4.0.0.dev5/aleksis/apps/chronos/frontend/components/Timetable.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TimetableWrapper from "./TimetableWrapper.vue";
+</script>
+<script>
+export default {
+  name: "Timetable",
+};
+</script>
+
+<template>
+  <timetable-wrapper>
+    <template #default="{ selected }">
+      <calendar-with-controls
+        :calendar-feeds="[
+          { name: 'lesson' },
+          { name: 'supervision' },
+          { name: 'holidays' },
+        ]"
+        :params="{ type: selected.type, id: selected.objId }"
+      />
+    </template>
+  </timetable-wrapper>
+</template>
---
+++ packages/AlekSIS-App-Chronos/aleksis_app_chronos-4.0.0.dev5.tar.gz/aleksis_app_chronos-4.0.0.dev5/aleksis/apps/chronos/frontend/index.js
@@ -0,0 +1,52 @@
+import { hasPersonValidator } from "aleksis.core/routeValidators";
+import Timetable from "./components/Timetable.vue";
+
+export default {
+  meta: {
+    inMenu: true,
+    titleKey: "chronos.menu_title",
+    icon: "mdi-school-outline",
+    iconActive: "mdi-school",
+    validators: [hasPersonValidator],
+  },
+  children: [
+    {
+      path: "timetable/",
+      component: Timetable,
+      name: "chronos.timetable",
+      meta: {
+        inMenu: true,
+        titleKey: "chronos.timetable.menu_title",
+        toolbarTitle: "chronos.timetable.menu_title",
+        icon: "mdi-grid",
+        permission: "chronos.view_timetable_overview_rule",
+        fullWidth: true,
+      },
+    },
+    {
+      path: "timetable/:type/:id/",
+      component: Timetable,
+      name: "chronos.timetableWithId",
+      meta: {
+        permission: "chronos.view_timetable_overview_rule",
+        fullWidth: true,
+      },
+    },
+    {
+      path: "substitutions/print/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "chronos.substitutions",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+    {
+      path: "substitutions/print/:date/",
+      component: () => import("aleksis.core/components/LegacyBaseTemplate.vue"),
+      name: "chronos.substitutionsByDate",
+      props: {
+        byTheGreatnessOfTheAlmightyAleksolotlISwearIAmWorthyOfUsingTheLegacyBaseTemplate: true,
+      },
+    },
+  ],
+};
