fdcbd815dd89b0b7f441c8d707369bab396de21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:05 +0000

    Add package opytimizer/3.1.4/opytimizer-3.1.4.tar.gz

---
+++ packages/opytimizer/opytimizer-3.1.4.tar.gz/opytimizer-3.1.4/opytimizer/optimizers/misc/__init__.py
@@ -0,0 +1,10 @@
+"""An evolutionary package for all common opytimizer modules.
+It contains implementations of miscellaneous-based optimizers.
+"""
+
+from opytimizer.optimizers.misc.aoa import AOA
+from opytimizer.optimizers.misc.cem import CEM
+from opytimizer.optimizers.misc.doa import DOA
+from opytimizer.optimizers.misc.gs import GS
+from opytimizer.optimizers.misc.hc import HC
+from opytimizer.optimizers.misc.nds import NDS
