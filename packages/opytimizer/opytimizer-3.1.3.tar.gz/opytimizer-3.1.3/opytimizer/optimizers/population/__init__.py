83b095276bc7c7cda92c029ec53ef0134b7deaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:05 +0000

    Add package opytimizer/3.1.3/opytimizer-3.1.3.tar.gz

---
+++ packages/opytimizer/opytimizer-3.1.3.tar.gz/opytimizer-3.1.3/opytimizer/optimizers/population/__init__.py
@@ -0,0 +1,16 @@
+"""An evolutionary package for all common opytimizer modules.
+It contains implementations of population-based optimizers.
+"""
+
+from opytimizer.optimizers.population.aeo import AEO
+from opytimizer.optimizers.population.ao import AO
+from opytimizer.optimizers.population.coa import COA
+from opytimizer.optimizers.population.epo import EPO
+from opytimizer.optimizers.population.gco import GCO
+from opytimizer.optimizers.population.gwo import GWO
+from opytimizer.optimizers.population.hho import HHO
+from opytimizer.optimizers.population.loa import LOA
+from opytimizer.optimizers.population.osa import OSA
+from opytimizer.optimizers.population.ppa import PPA
+from opytimizer.optimizers.population.pvs import PVS
+from opytimizer.optimizers.population.rfo import RFO
---
+++ packages/opytimizer/opytimizer-3.1.3.tar.gz/opytimizer-3.1.3/opytimizer/spaces/__init__.py
@@ -0,0 +1,11 @@
+"""Customizable space module that provides different search spaces
+implementations.
+"""
+
+from opytimizer.spaces.boolean import BooleanSpace
+from opytimizer.spaces.graph import GraphSpace
+from opytimizer.spaces.grid import GridSpace
+from opytimizer.spaces.hyper_complex import HyperComplexSpace
+from opytimizer.spaces.pareto import ParetoSpace
+from opytimizer.spaces.search import SearchSpace
+from opytimizer.spaces.tree import TreeSpace
