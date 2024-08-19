0816ac806951311097fef10e3f4585560408c91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-19 04:03:15 +0000

    Add package MartinCesar/0.1/martincesar-0.1.tar.gz

---
+++ packages/MartinCesar/martincesar-0.1.tar.gz/martincesar-0.1/martincesar/operaciones.py
@@ -0,0 +1,3 @@
+def sumanumeros(n1,n2):
+    sumaT=n1+n2
+    print(sumaT)
---
+++ packages/MartinCesar/martincesar-0.1.tar.gz/martincesar-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="MartinCesar",
+    version='0.1',
+    packages=['martincesar'],
+    description="",
+    long_description=open('Readme.md',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='martin',
+)
