18f998cb1934547560acc1cf2fed861b045cf309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 04:03:40 +0000

    Add package geopi/0.1.3/geopi-0.1.3.tar.gz

---
+++ packages/geopi/geopi-0.1.3.tar.gz/geopi-0.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="geopi",
+    version="0.1.3",
+    author="author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        requirement for requirement in open('requirements.txt').readlines()
+        if requirement.strip() and not requirement.startswith('-')
+    ],
+    package_data={
+        'geopi': ['data/*.json', 'data/city_boundary/*.json', 'data/city_shp/*.*'],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
