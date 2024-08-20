58ae4c177784e15dba4c5290a59dbdedb0e38442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 04:03:53 +0000

    Add package ssm-client/0.5.1/ssm_client-0.5.1-py3-none-any.whl

---
+++ packages/ssm-client/ssm_client-0.5.1-py3-none-any.whl/ssm_client/containers/collection_container.py
@@ -0,0 +1,62 @@
+import json
+
+
+class CollectionContainer:
+    def __init__(self, **kwargs):
+        self.__title = kwargs.get("title", None)
+        self.__uri = kwargs.get("uri", None)
+
+    def __eq__(self, other):
+        """
+        Support "==" comparison between CollectionContainers
+
+        Args:
+            other (CollectionContainer): Collection to compare for equality.
+
+        Return:
+            areCollectionsEqual (bool)
+        """
+        titleEqual = self.title == other.title
+        uriEqual = self.uri == other.uri
+        return titleEqual and uriEqual
+
+    def __ne__(self, other):
+        """
+        Support "!=" comparison between CollectionContainers
+
+        Args:
+            other (CollectionContainer):
+                Collection to compare for non-equality.
+
+        Return:
+            areCollectionsNotEqual (bool)
+        """
+
+        titleNotEqual = self.title != other.title
+        uriNotEqual = self.uri != other.uri
+        return titleNotEqual or uriNotEqual
+
+    def __repr__(self):
+        collection_dict = {
+            "title": self.title,
+            "uri": self.uri,
+        }
+        return json.dumps(collection_dict)
+
+    def __str__(self):
+        fmt = "CollectionContainer(title={title},uri={uri})"
+        return fmt.format(title=self.title, uri=self.uri)
+
+    @property
+    def title(self):
+        """
+        Title for CollectionContainer
+        """
+        return self.__title
+
+    @property
+    def uri(self):
+        """
+        Fuseki server URI / URL where CollectionContainer is stored
+        """
+        return self.__uri
---
+++ packages/ssm-client/ssm_client-0.5.1-py3-none-any.whl/ssm_client/services/__init__.py
@@ -0,0 +1,10 @@
+"""Services for ssm-client."""
+
+from .collection_service import CollectionService
+from .dataset_service import DatasetService
+
+
+__all__ = [
+    "CollectionService",
+    "DatasetService",
+]
