a355aafda05e2109d85a0185cc30e4ad363fbd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:47 +0000

    Add package pyimager/3.4.0/pyimager-3.4.0-py3-none-any.whl

---
+++ packages/pyimager/pyimager-3.4.0-py3-none-any.whl/pyimager/__init__.py
@@ -0,0 +1,30 @@
+"""
+PYIMAGER
+Module for displaying images in console
+
+submodules:
+pyimager.designer -- submodule of Designer mode
+pyimager.display -- submodule for displaying images
+pyimager.compress -- submodule for compressing images
+pyimager.utils -- pyimagers utils
+pyimager.data -- pyimagers data
+
+Start pyimager via cmd to execute __main__.py,
+which will make you enter the Designer Mode to create your own lkims.
+"""
+def about():
+    """
+    Returns information about your release and other projects by LK.
+    """
+    return {"version": (3, 4, 0), "author": "Leander Kafemann", "date": "19.08.2024", "recommend": ("Büro by LK"), "feedback to": "leander@kafemann.berlin"}
+
+from . import utils
+from . import compress
+from . import display
+from . import designer
+from . import data
+
+from .display import *
+from .designer import *
+from .compress import *
+from .data import *
