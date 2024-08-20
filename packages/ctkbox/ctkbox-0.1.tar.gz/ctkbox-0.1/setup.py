d9f13f3eb3ca911923619b813b50d388d3e6e259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:06 +0000

    Add package ctkbox/0.1/ctkbox-0.1.tar.gz

---
+++ packages/ctkbox/ctkbox-0.1.tar.gz/ctkbox-0.1/setup.py
@@ -0,0 +1,24 @@
+#python 3.10.10
+import setuptools
+setuptools.setup(
+    name = 'ctkbox',
+    version = '0.1',
+    author= 'RedShadow',
+    description= 'simple message box you can use it in CTK > ctkbox(msg)',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
+
+
+
+
+
+
+
+
+
+
