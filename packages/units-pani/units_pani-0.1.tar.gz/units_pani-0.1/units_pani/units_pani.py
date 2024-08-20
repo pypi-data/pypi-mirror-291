7781a68d1e5411632e9980d0f0082b68f131640c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 20:03:59 +0000

    Add package units-pani/0.1/units_pani-0.1.tar.gz

---
+++ packages/units-pani/units_pani-0.1.tar.gz/units_pani-0.1/units_pani/units_pani.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# sec = base unit
+sec = 1
+min = 60 * sec
+h = 60 * min
+ms = sec / 1000
+us = ms / 1000
+ns = us / 1000
+ps = ns / 1000
+
+# deg = base unit
+deg = 1
+rad = 180 / np.pi
+
+# Hz is base unit
+Hz = 1/sec
+kHz = 1000 * Hz
+MHz = 1000 * kHz
+GHz = 1000 * MHz
