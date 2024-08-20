cdb86e97f8d39f4c27f335074e95dcf13047035c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:23 +0000

    Add package python-ta/2.8.1/python_ta-2.8.1.tar.gz

---
+++ packages/python-ta/python_ta-2.8.1.tar.gz/python_ta-2.8.1/python_ta/checkers/one_iteration_checker.py
@@ -0,0 +1,67 @@
+"""Checker for a loop that can only ever run for one iteration.
+"""
+
+from typing import Union
+
+from astroid import nodes
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import only_required_for_messages
+from pylint.lint import PyLinter
+
+
+class OneIterationChecker(BaseChecker):
+    name = "one_iteration"
+    msgs = {
+        "E9996": (
+            "This loop will only ever run for one iteration",
+            "one-iteration",
+            "Reported when the loop body always ends the loop in its first iteration "
+            '(e.g., by returning or using the "break" keyword).',
+        )
+    }
+
+    @only_required_for_messages("one-iteration")
+    def visit_for(self, node: nodes.For) -> None:
+        """Visits for node"""
+        if self._check_one_iteration(node):
+            self.add_message("one-iteration", node=node)
+
+    @only_required_for_messages("one-iteration")
+    def visit_while(self, node: nodes.While) -> None:
+        """Visits while node"""
+        if self._check_one_iteration(node):
+            self.add_message("one-iteration", node=node)
+
+    def _check_one_iteration(self, node: Union[nodes.For, nodes.While]) -> bool:
+        """Return whether the given loop is guaranteed to stop after one iteration.
+
+        More precisely, Returns False if there exists a direct predecessor
+        block `p` to the start of the loop block `s` such that the
+        first statement in `p` is a child node of <node> and that there exists a
+        path from `s` to `p.
+
+        Note: For `while` loops, 'start of the loop block' refers to the block with
+        the test condition (or the first of the blocks that make up test condition).
+        For `for` loops, it refers to the block with the assignment target.
+        """
+        start = node.target if isinstance(node, nodes.For) else node
+        if not hasattr(start, "cfg_block"):
+            return False
+
+        preds = start.cfg_block.predecessors
+
+        if preds == []:
+            return False
+
+        for pred in preds:
+            stmt = pred.source.statements[0]
+            if node.parent_of(stmt) and pred.source.reachable:
+                if isinstance(node, nodes.For) and stmt is node.iter:
+                    continue
+                return False
+        return True
+
+
+def register(linter: PyLinter) -> None:
+    """Required method to auto-register this checker to the linter"""
+    linter.register_checker(OneIterationChecker(linter))
---
+++ packages/python-ta/python_ta-2.8.1.tar.gz/python_ta-2.8.1/python_ta/patches/transforms.py
@@ -0,0 +1,21 @@
+"""Patch to add transforms for setting type constraints and creating control flow graphs.
+"""
+
+from pylint.lint import PyLinter
+
+from ..cfg.visitor import CFGVisitor
+
+
+def patch_ast_transforms():
+    old_get_ast = PyLinter.get_ast
+
+    def new_get_ast(self, filepath, modname, data):
+        ast = old_get_ast(self, filepath, modname, data)
+        if ast is not None:
+            try:
+                ast.accept(CFGVisitor())
+            except:
+                pass
+        return ast
+
+    PyLinter.get_ast = new_get_ast
