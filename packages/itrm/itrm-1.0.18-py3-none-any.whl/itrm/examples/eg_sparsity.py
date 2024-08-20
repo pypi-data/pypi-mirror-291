91fd12796f96499784b260a0d46a002589a1bc83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 05:04:31 +0000

    Add package itrm/1.0.18/itrm-1.0.18-py3-none-any.whl

---
+++ packages/itrm/itrm-1.0.18-py3-none-any.whl/itrm/examples/eg_sparsity.py
@@ -0,0 +1,15 @@
+import numpy as np
+import itrm
+
+N = 100
+M = np.random.randn(N, N)
+M = M.T @ M
+M = (M > 3)
+itrm.spy(M)
+
+N = 25
+M = np.random.randn(N, N)
+M = M.T @ M
+M = (M > 3)
+itrm.CONFIG.uni = False
+itrm.spy(M)
