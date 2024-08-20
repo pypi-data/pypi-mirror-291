204611fce7a9224dbb71b85b8383de4bc8459a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:06 +0000

    Add package tse-derivatives/0.1.0/tse_derivatives-0.1.0.tar.gz

---
+++ packages/tse-derivatives/tse_derivatives-0.1.0.tar.gz/tse_derivatives-0.1.0/README.md
@@ -0,0 +1,18 @@
+# Live Options Data Fetcher
+
+## Description
+
+This Python package retrieves live data for options trade of Tehran Stock Exchange and Iran Farabourse Exchange based on the underlying asset's ticker and option type. It supports tickers such as "خودرو" and option types including "call" and "put."
+
+## Features
+
+- Fetch live option data for specified tickers.
+- Support for multiple option types: "call" and "put."
+- Easy integration with your existing applications.
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install tse_derivatives
---
+++ packages/tse-derivatives/tse_derivatives-0.1.0.tar.gz/tse_derivatives-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='tse_derivatives',
+
+    version='0.1.0',
+
+    packages=find_packages(),
+
+    install_requires=[],  # List dependencies here
+
+    author='Jalal Seifoddini',
+
+    author_email='jalal.seifoddini@gmail.com',
+
+    description='Gets the underlying asset ticker (e.g., "خودرو") and the option type (either "call" or "put") to fetch live option data.',
+
+    long_description=open('README.md').read(),
+
+    long_description_content_type='text/markdown',
+
+    classifiers=[
+
+        'Programming Language :: Python :: 3',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+    ],
+
+    python_requires='>=3.6',
+
+)
