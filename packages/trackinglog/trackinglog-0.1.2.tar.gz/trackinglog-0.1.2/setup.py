deffc6f2a33a945f65d0b4b883b210be5ac8706f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:52 +0000

    Add package trackinglog/0.1.2/trackinglog-0.1.2.tar.gz

---
+++ packages/trackinglog/trackinglog-0.1.2.tar.gz/trackinglog-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trackinglog',
+    version='0.1.2',
+    description='A logging package for tracing function calls with error handling and email notification',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yinghao Li',
+    author_email='shiyi.yinghao@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.11',
+)
+
