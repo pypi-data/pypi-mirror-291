b267d60ccc05899c89a327b63618cc5ba349f143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:58 +0000

    Add package hlaoui/0.1/hlaoui-0.1.tar.gz

---
+++ packages/hlaoui/hlaoui-0.1.tar.gz/hlaoui-0.1/README.md
@@ -0,0 +1,9 @@
+# My Library
+
+A simple library for basic math operations.
+
+## Installation
+
+```bash
+pip install my_library
+```
---
+++ packages/hlaoui/hlaoui-0.1.tar.gz/hlaoui-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hlaoui',
+    version='0.1',
+    description='A simple math operations library',
+    author='mohamedtaher.hlaoui',
+    author_email='mohamedtaher.hlaoui@allence-tunisie.com',
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
