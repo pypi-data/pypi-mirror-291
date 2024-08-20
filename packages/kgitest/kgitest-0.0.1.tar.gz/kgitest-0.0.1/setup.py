a211432d673a4c24fb977c455fedeebc817e1041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:04:02 +0000

    Add package kgitest/0.0.1/kgitest-0.0.1.tar.gz

---
+++ packages/kgitest/kgitest-0.0.1.tar.gz/kgitest-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='UTF-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kgitest', #pip download project name
+    version='0.0.1',
+    description='Test project',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='KGI',
+    author_email='Teste@kgi.com',
+    packages=find_packages(),
+    package_data={
+    },
+    install_requires=[
+    ]
+)
