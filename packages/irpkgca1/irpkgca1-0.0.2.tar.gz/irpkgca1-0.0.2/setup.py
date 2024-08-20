fae373f829c27a49e4fa111f3e27e081ce201615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:57 +0000

    Add package irpkgca1/0.0.2/irpkgca1-0.0.2.tar.gz

---
+++ packages/irpkgca1/irpkgca1-0.0.2.tar.gz/irpkgca1-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+# exclude server directory
+# exclude test files
+setup(
+    name='irpkgca1',
+    version='0.0.2',
+    url='https://github.com/vsaravind01/ir_pkg',
+    maintainer='Senapathy Veerasekaran',
+    classifiers=[],
+    description='description',
+    keywords=[],
+    project_urls={},
+    python_requires='>=3.7')
