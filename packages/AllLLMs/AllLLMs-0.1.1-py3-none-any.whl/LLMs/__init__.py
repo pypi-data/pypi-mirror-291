5d8d4cec11d1e29197aa9742165bb9c4738ac599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:16 +0000

    Add package AllLLMs/0.1.1/AllLLMs-0.1.1-py3-none-any.whl

---
+++ packages/AllLLMs/AllLLMs-0.1.1-py3-none-any.whl/LLMs/__init__.py
@@ -0,0 +1,8 @@
+from .LLMInitialization import Initialize
+from .LLMOutput import Output
+from .LLMModels import get_llm_info
+
+__all__ = ['Initialize', 'Output', 'get_llm_info']
+
+# You can add a version number if desired
+__version__ = '0.1.0'
