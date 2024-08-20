a267b34b2b34262d3c63c4ff9429e6f4385c7585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:36 +0000

    Add package WrappedLLM/0.1.3/WrappedLLM-0.1.3-py3-none-any.whl

---
+++ packages/WrappedLLM/WrappedLLM-0.1.3-py3-none-any.whl/WrappedLLM/__init__.py
@@ -0,0 +1,8 @@
+from .LLMInitialization import Initialize
+from .LLMOutput import Output
+from .LLMModels import get_info
+
+__all__ = ['Initialize', 'Output', 'get_info']
+
+# You can add a version number if desired
+__version__ = '0.1.2'
