efa8df36f2072de4d16887fdd905e158146f6d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:57 +0000

    Add package logging-commons/0.1.12/logging_commons-0.1.12.tar.gz

---
+++ packages/logging-commons/logging_commons-0.1.12.tar.gz/logging_commons-0.1.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logging-commons",
+    version="0.1.12",
+    url="https://github.com/KaribuLab/python-logging-commons",
+    license="Apache Software License",
+    author="Patricio Ascencio",
+    author_email="paascencio@gmail.com",
+    description="Python Logging Tools",
+    python_requires=">=3.12",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms=["MacOS X", "Linux", "Windows"],
+)
