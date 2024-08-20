268873be0b17ac3880c7c59a3cdf99b2a5e223db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:55 +0000

    Add package fitsImageProcess/0.3.0/fitsImageProcess-0.3.0.tar.gz

---
+++ packages/fitsImageProcess/fitsImageProcess-0.3.0.tar.gz/fitsImageProcess-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fitsImageProcess',  # This should be a unique name for your package
+    version='0.3.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # List your package dependencies here
+        # e.g., 'numpy', 'pandas', etc.
+        'numpy','astropy','matplotlib','photutils','sep','astroquery','numba','reproject','scipy','setuptools','opencv-python','cmastro','scikit-image'
+    ],
+    entry_points={
+        'console_scripts': [
+            # Define any command-line scripts here
+        ],
+    },
+    # url='https://github.com/yourusername/fitsImageProcess',  # Update this with your repository URL
+    license='MIT',
+    author='Matthieu Musy',
+    author_email='matthieu.musy@ensta-bretagne.org',
+    description='A package for processing FITS images',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
