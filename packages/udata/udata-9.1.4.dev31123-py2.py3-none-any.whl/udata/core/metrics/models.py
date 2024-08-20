c27f71772876f053f238fd4560b835ae8ece3ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 20:04:36 +0000

    Add package udata/9.1.4.dev31123/udata-9.1.4.dev31123-py2.py3-none-any.whl

---
+++ packages/udata/udata-9.1.4.dev31123-py2.py3-none-any.whl/udata/core/metrics/models.py
@@ -0,0 +1,13 @@
+from udata.api_fields import field
+from udata.mongo import db
+
+__all__ = ("WithMetrics",)
+
+
+class WithMetrics(object):
+    metrics = field(db.DictField())
+
+    __metrics_keys__ = []
+
+    def get_metrics(self):
+        return {key: self.metrics.get(key, 0) for key in self.__metrics_keys__}
---
+++ packages/udata/udata-9.1.4.dev31123-py2.py3-none-any.whl/udata/mongo/taglist_field.py
@@ -0,0 +1,17 @@
+from mongoengine.fields import ListField, StringField
+from slugify import slugify
+
+
+class TagListField(ListField):
+    def __init__(self, **kwargs):
+        self.tags = []
+        super(TagListField, self).__init__(StringField(), **kwargs)
+
+    def clean(self, value):
+        return sorted(list(set([slugify(v, to_lower=True) for v in value])))
+
+    def to_python(self, value):
+        return super(TagListField, self).to_python(self.clean(value))
+
+    def to_mongo(self, value):
+        return super(TagListField, self).to_mongo(self.clean(value))
