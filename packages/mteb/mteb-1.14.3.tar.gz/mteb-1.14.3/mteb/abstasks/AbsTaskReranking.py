9180ffe209c6fe23ea74d3d62b000c2cf9dc8387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:59 +0000

    Add package mteb/1.14.3/mteb-1.14.3.tar.gz

---
+++ packages/mteb/mteb-1.14.3.tar.gz/mteb-1.14.3/mteb/abstasks/AbsTaskReranking.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import Any
+
+from datasets import Dataset
+
+from mteb.encoder_interface import Encoder, EncoderWithQueryCorpusEncode
+from mteb.load_results.mteb_results import ScoresDict
+
+from ..evaluation.evaluators import RerankingEvaluator
+from .AbsTask import AbsTask
+
+
+class AbsTaskReranking(AbsTask):
+    """Abstract class for re-ranking experiments.
+
+    self.load_data() must generate a huggingface dataset with a split matching self.metadata_dict["eval_splits"], and assign it to self.dataset. It must contain the following columns:
+        query: str
+        positive: list[str]
+        negative: list[str]
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def _evaluate_subset(
+        self,
+        model: Encoder | EncoderWithQueryCorpusEncode,
+        data_split: Dataset,
+        *,
+        encode_kwargs: dict[str, Any] = {},
+        **kwargs: Any,
+    ) -> ScoresDict:
+        evaluator = RerankingEvaluator(
+            data_split,
+            task_name=self.metadata.name,
+            encode_kwargs=encode_kwargs,
+            **kwargs,
+        )
+        scores = evaluator(model)
+
+        self._add_main_score(scores)
+        return scores
+
+    def _add_main_score(self, scores: ScoresDict) -> None:
+        scores["main_score"] = scores[self.metadata.main_score]
