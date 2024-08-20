30f98f5c88b6898a8afff7ffc9f06e178f882fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 19:03:49 +0000

    Add package excel2db/1.0.1/excel2db-1.0.1-py3-none-any.whl

---
+++ packages/excel2db/excel2db-1.0.1-py3-none-any.whl/excel2db/excel2db.py
@@ -0,0 +1,29 @@
+# -*- coding:utf8 -*-
+"""
+用于excel转sqlite数据库
+注意：
+依据变量修改配置文件，应在cheakConf中进行
+依据配置文件修改变量，应在配置文件完全生成后进行
+"""
+from . import value, cheakConf, excel
+
+class excel2db:
+
+    def __init__(self, *configFileUrl):
+        """
+        :param *configFileUrl: 支持直接传入python字典或者配置json或者配置文件路径
+        """
+        self.value = value.value() ##生成变量文件
+        self.value.rawConf = cheakConf.combinExcelConf(*configFileUrl) ##生成配置文件
+
+    def excel2db(self, excelUrl):
+        ##执行excel级别转换
+        excelManager = excel.excel(self.value, self.value.rawConf)
+        excelManager.excel(excelUrl)
+
+    def setDateFormatFunc(self, *dateFormat):
+        """
+        设置日期格式化方法
+        """
+        for func in dateFormat:
+            self.value.dateFormatFunc[func.__name__] = func
