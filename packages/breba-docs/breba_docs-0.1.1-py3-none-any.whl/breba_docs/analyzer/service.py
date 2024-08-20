1860b636ee31ab25530e8aea2a1fb34fe63fc2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:46 +0000

    Add package breba-docs/0.1.1/breba_docs-0.1.1-py3-none-any.whl

---
+++ packages/breba-docs/breba_docs-0.1.1-py3-none-any.whl/breba_docs/analyzer/service.py
@@ -0,0 +1,33 @@
+from breba_docs.services.openai_agent import OpenAIAgent
+from docker.models.containers import Container
+
+
+def analyze(agent: OpenAIAgent, container: Container, doc: str):
+
+    commands = agent.fetch_commands(doc)
+
+    chained_commands = ' && '.join(commands)
+
+    # Execute a command in the container with real-time output
+    exit_code, output = container.exec_run(
+        f'/bin/bash -c "{chained_commands}"',
+        stdout=True,
+        stderr=True,
+        tty=True,
+        stream=True,
+    )
+
+    output_text = ""
+
+    for line in output:
+        line_text = line.decode("utf-8")
+        print(line_text.strip())
+        output_text += line_text
+
+    print(agent.analyze_output(output_text))
+
+    result = container.exec_run('ls -la')
+    print(result.output.decode('utf-8'))
+
+    container.stop()
+    container.remove()
