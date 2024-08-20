f5f417c229304408c069d8bc18c0ff0cff3ba3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:01 +0000

    Add package composabl-core-dev/0.10.0.dev4/composabl_core_dev-0.10.0.dev4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/composabl-core-dev/composabl_core_dev-0.10.0.dev4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/composabl_core/examples/demo/agent/agent_skill_single.py
@@ -0,0 +1,143 @@
+# Copyright (C) Composabl, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+from composabl_core import Skill
+from composabl_core.agent.agent import Agent
+from composabl_core.agent import SkillGroup
+from composabl_core.examples.demo.agent import (
+    target_skill_box,
+    target_skill_dictionary,
+    target_skill_discrete,
+    target_skill_multi_binary,
+    target_skill_multi_discrete,
+    target_skill_nested_scenario,
+    target_skill_tuple,
+    target_skill_text,
+    target_skill_custom_action_space,
+    expert_skill_controller_box,
+    pass_through_skill_controller,
+    sensors_dict,
+    mapped_sensors_dict
+)
+from composabl_core.examples.demo.agent.teacher import TeacherSpaceDictionary
+
+agent_dictionary = Agent()
+agent_dictionary.add_sensors(mapped_sensors_dict["dictionary"])
+agent_dictionary.add_skill(target_skill_dictionary)
+
+agent_box = Agent()
+agent_box.add_sensors(mapped_sensors_dict["box"])
+agent_box.add_skill(target_skill_box)
+
+agent_discrete = Agent()
+agent_discrete.add_sensors(mapped_sensors_dict["discrete"])
+agent_discrete.add_skill(target_skill_discrete)
+
+agent_multidiscrete = Agent()
+agent_multidiscrete.add_sensors(mapped_sensors_dict["multidiscrete"])
+agent_multidiscrete.add_skill(target_skill_multi_discrete)
+
+agent_multibinary = Agent()
+agent_multibinary.add_sensors(mapped_sensors_dict["multibinary"])
+agent_multibinary.add_skill(target_skill_multi_binary)
+
+agent_tuple = Agent()
+agent_tuple.add_sensors(mapped_sensors_dict["tuple"])
+agent_tuple.add_skill(target_skill_tuple)
+
+agent_nested_scenario = Agent()
+agent_nested_scenario.add_sensors(mapped_sensors_dict["dictionary"])
+agent_nested_scenario.add_skill(target_skill_nested_scenario)
+
+agent_setpoint = Agent()
+agent_setpoint.add_sensors(mapped_sensors_dict["discrete"])
+agent_setpoint.add_skill(target_skill_custom_action_space)
+agent_setpoint.add_skill(pass_through_skill_controller)
+skill_group = SkillGroup(target_skill_custom_action_space, pass_through_skill_controller)
+agent_setpoint.add_skill_group(skill_group=skill_group)
+
+agent_controller = Agent()
+agent_controller.add_skill(expert_skill_controller_box)
+agent_controller.add_sensors(mapped_sensors_dict["box"])
+
+agent_text = Agent()
+agent_text.add_skill(target_skill_text)
+agent_text.add_sensors(mapped_sensors_dict["text"])
+
+mapped_agents_for_space = {
+    "box": agent_box,
+    "dictionary": agent_dictionary,
+    "discrete": agent_discrete,
+    "multidiscrete": agent_multidiscrete,
+    "multibinary": agent_multibinary,
+    "tuple": agent_tuple,
+    "set_point": agent_setpoint,
+    "controller_box": agent_controller,
+    "text": agent_text
+}
+
+agent_dictionary = Agent()
+agent_dictionary.add_sensors(sensors_dict["dictionary"])
+agent_dictionary.add_skill(target_skill_dictionary)
+
+agent_box = Agent()
+agent_box.add_sensors(sensors_dict["box"])
+agent_box.add_skill(target_skill_box)
+
+agent_discrete = Agent()
+agent_discrete.add_sensors(sensors_dict["discrete"])
+agent_discrete.add_skill(target_skill_discrete)
+
+agent_multidiscrete = Agent()
+agent_multidiscrete.add_sensors(sensors_dict["multidiscrete"])
+agent_multidiscrete.add_skill(target_skill_multi_discrete)
+
+agent_multibinary = Agent()
+agent_multibinary.add_sensors(sensors_dict["multibinary"])
+agent_multibinary.add_skill(target_skill_multi_binary)
+
+agent_tuple = Agent()
+agent_tuple.add_sensors(sensors_dict["tuple"])
+agent_tuple.add_skill(target_skill_tuple)
+
+agent_nested_scenario = Agent()
+agent_nested_scenario.add_sensors(sensors_dict["dictionary"])
+agent_nested_scenario.add_skill(target_skill_nested_scenario)
+
+agent_setpoint = Agent()
+agent_setpoint.add_sensors(sensors_dict["discrete"])
+agent_setpoint.add_skill(target_skill_custom_action_space)
+agent_setpoint.add_skill(pass_through_skill_controller)
+skill_group = SkillGroup(target_skill_custom_action_space, pass_through_skill_controller)
+agent_setpoint.add_skill_group(skill_group=skill_group)
+
+agent_controller = Agent()
+agent_controller.add_skill(expert_skill_controller_box)
+agent_controller.add_sensors(sensors_dict["box"])
+
+agent_text = Agent()
+agent_text.add_skill(target_skill_text)
+agent_text.add_sensors(sensors_dict["text"])
+
+skill1 = Skill("teacher-skill-dictionary", TeacherSpaceDictionary)
+skill2 = Skill("teacher-skill-dictionary2", TeacherSpaceDictionary)
+skill_group = SkillGroup(skill1, skill2)
+
+agent_skill_group = Agent()
+agent_skill_group.add_sensors(sensors_dict["dictionary"])
+agent_skill_group.add_skill(skill1)
+agent_skill_group.add_skill_group(skill_group)
+
+agents_for_space = {
+    "box": agent_box,
+    "dictionary": agent_dictionary,
+    "discrete": agent_discrete,
+    "multidiscrete": agent_multidiscrete,
+    "multibinary": agent_multibinary,
+    "tuple": agent_tuple,
+    "set_point": agent_setpoint,
+    "controller_box": agent_controller,
+    "text": agent_text,
+    "skill_group": agent_skill_group
+}
+
