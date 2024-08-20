0aba2ec61c5a0fff195d5d2ec7e23415be7b6f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:56 +0000

    Add package Eridium/1.5/eridium-1.5.tar.gz

---
+++ packages/Eridium/eridium-1.5.tar.gz/eridium-1.5/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='Eridium',
+	version='1.5',
+	author='Darkester',
+	author_email='chernaev.alex@mail.ru',
+	description='Simple Math',
+	packages=['Eridium'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
