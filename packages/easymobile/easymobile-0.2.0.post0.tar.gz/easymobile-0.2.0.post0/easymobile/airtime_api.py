3675f9faea7cd17fff6a3c402b95fd86132f5c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 04:06:14 +0000

    Add package easymobile/0.2.0.post0/easymobile-0.2.0.post0.tar.gz

---
+++ packages/easymobile/easymobile-0.2.0.post0.tar.gz/easymobile-0.2.0.post0/easymobile/airtime_api.py
@@ -0,0 +1,129 @@
+from . import exceptions
+from .base import EasyBase
+
+
+class EasyAirtime(EasyBase):
+    """
+    A class to interact with the Easymobile API for airtime-related operations.
+    ref: https://b2b.eazymobile.ng/api/developer/v1/documentation/start#item-2-4
+    """
+    
+    def __init__(self, api_key: str = None, timeout=None) -> None:
+        """
+        Initialize the EasyAirtime object.
+        
+        Args:
+            api_key (str, optional): The API key for authentication. If not provided, will use the environment variable `EASYB2B_API_KEY`. Set key as "Demo" to use the mocked API.
+            timeout (float, optional): Timeout for the requests. Default is 45.1 seconds.
+        """
+        super().__init__(api_key, timeout)
+
+    def get_networks(self, networks:str='all', **kwargs):
+        """
+        Query all the networks for airtime top-up.
+
+        Args:
+            networks (str, optional): Specify the network to retrieve. Default is 'all'. Do not change unless you know what you are doing!
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: JSON response from the API.
+        """
+        url = f'{self.base_url}/v1/topup/load/networks'
+        data = {'networks': networks}
+        data.update(**kwargs)
+        response = self.sess.post(url, json=data, timeout=self.timeout)
+        try:
+            response = self.sess.post(url, json=data, timeout=self.timeout)
+            response.raise_for_status()
+            return response.json()
+        except exceptions.RequestException as e:
+            return {'error': str(e)}
+
+    def get_airtime_types(self, network:int, **kwargs):
+        """
+        Retrieve available airtime types for a specific network.
+
+        Args:
+            network (int): Network ID. Use the `.get_networks` method to fetch available networks and their IDs.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: JSON response from the API.
+        """
+        url = f'{self.base_url}/v1/topup/load/airtime-types'
+        data = {'network': str(network)}
+        data.update(**kwargs)
+        try:
+            response = self.sess.post(url, json=data, timeout=self.timeout)
+            response.raise_for_status()
+            return response.json()
+        except exceptions.RequestException as e:
+            return {'error': str(e)}
+
+    def get_airtime_rates(self, network:int, airtime_type:str, **kwargs):
+        """
+        Retrieve airtime rates for a specific network and airtime type.
+
+        Args:
+            network (int): Network ID. Use the `.get_networks` method to fetch available networks and their IDs.
+            airtime_type (str): Type of airtime. Use the `.get_airtime_types` method to fetch available network types and their names.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: JSON response from the API.
+        """
+        url = f'{self.base_url}/v1/topup/load/airtime-rate'
+        data = {'network': str(network), 'airtimeType': airtime_type}
+        data.update(**kwargs)
+        try:
+            response = self.sess.post(url, json=data, timeout=self.timeout)
+            response.raise_for_status()
+            return response.json()
+        except exceptions.RequestException as e:
+            return {'error': str(e)}
+
+    def purchase_airtime(self, reference: str, network:int, airtime_type:str, amount:int, phone:str):
+        """
+        Purchase airtime for a given phone number.
+
+        Args:
+            reference (str): Unique reference for the transaction. (see https://b2b.eazymobile.ng/api/developer/v1/documentation/start#item-2-14 ).
+            network (int): Network ID. Use the `.get_networks` method to fetch available networks and their IDs.
+            airtime_type (str): Type of airtime. Use the `.get_airtime_types` method to fetch available network types and their names.
+            amount (int|str): Amount of airtime to top-up.
+            phone (str): Phone number to receive the airtime.
+
+        Returns:
+            dict: JSON response from the API.
+        """
+        url = f'{self.base_url}/v1/topup/airtime'
+        data = {
+            "network": str(network),
+            "airtimeType": airtime_type,
+            "amount": str(amount),
+            "phone": phone,
+            "reference": reference 
+        }
+        try:
+            response = self.sess.post(url, json=data, timeout=self.timeout)
+            response.raise_for_status()
+            return response.json()
+        except exceptions.RequestException as e:
+            return {'error': str(e)}
+
+    def get_transaction_status(self, ref: str):
+        """
+        Get the status of a specific airtime transaction.
+
+        Args:
+            ref (str): Reference of the transaction you generated and supplied.
+
+        Returns:
+            dict: JSON response from the API.
+        """
+        return super().get_transaction_status('airtime', ref)
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/easymobile/easymobile-0.2.0.post0.tar.gz/easymobile-0.2.0.post0/easymobile/base.py
@@ -0,0 +1,95 @@
+import requests
+import os
+from . import exceptions
+from .mocked import MockedRequests
+
+API_KEY = os.getenv('EASYMOBILE_API_KEY')
+TIMEOUT = os.getenv('EASYMOBILE_TIMEOUT')
+
+
+class EasyBase:
+    """
+    A class to interact with the EasyB2B API.
+    
+    Attributes:
+        api_key (str): The API key for authentication.
+        timeout (float): The timeout for API requests.
+    """
+    _mocked = False
+    sess = None
+    base_url = "https://b2b.eazymobile.ng/api/live"
+    timeout = 45.1
+
+    def __init__(self, api_key: str = None, timeout: float = None) -> None:
+        """
+        Initialize the EasyB2B object.
+        """
+        if timeout or TIMEOUT:
+            self.timeout = timeout or float(TIMEOUT)
+
+        self.req_session = requests.session()
+        self.req_session.headers['Content-Type'] = 'application/json'
+        self.req_session.headers['Accept'] = 'application/json'
+
+        if api_key:
+            self.req_session.headers['Authorization'] = f'Bearer {api_key}'
+            self.mocked = True if api_key.lower() == 'demo' else False
+        elif API_KEY:
+            self.req_session.headers['Authorization'] =  f'Bearer {API_KEY}'
+            self.mocked = True if API_KEY.lower() == 'demo' else False
+        else:
+            raise ValueError("Invalid or No API key provided!")
+
+    @property
+    def mocked(self):
+        return self._mocked
+
+    @mocked.setter
+    def mocked(self, val):
+        if not isinstance(val, bool):
+            raise ValueError("property mocked must be a boolean value!")
+        if val:
+            self.sess = MockedRequests(session=self.req_session)
+        else:
+            self.sess = self.req_session
+        self._mocked = val
+
+    def get_wallet_balance(self, email, **kwargs):
+        """
+        Get the wallet balance for a given email.
+
+        Args:
+            email (str): The email to get the wallet balance for.
+
+        Returns:
+            dict: The response from the API as a JSON dictionary.
+        """
+        url = f'{self.base_url}/v1/load/wallet-balance'
+        data = {'email': email}
+        data.update(**kwargs)
+        try:
+            response = self.sess.post(url, json=data, timeout=self.timeout)
+            response.raise_for_status()
+            return response.json()
+        except exceptions.RequestException as e:
+            return {'error': str(e)}
+
+    def get_transaction_status(self, name: str, ref: str):
+        """
+        Get the transaction status for a given transaction reference.
+
+        Args:
+            name (str): The name/type associated with the transaction.
+            ref (str): The reference of the transaction.
+
+        Returns:
+            dict: The response from the API as a JSON dictionary.
+        """
+        url = f'{self.base_url}/v1/transaction/status/{name}'
+        data = {"reference": ref}
+        try:
+            response = self.sess.post(url, json=data, timeout=self.timeout)
+            response.raise_for_status()
+            return response.json()
+        except exceptions.RequestException as e:
+            return {'error': str(e)}
