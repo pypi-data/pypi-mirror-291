40133a8c8f227be011b6d7384e3c1f8e813ea9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:52 +0000

    Add package beat/2.0.2/beat-2.0.2-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/beat/beat-2.0.2-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/beat/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+project_root = '/project'
+git_sha1 = 'c70b2761d05a24fb53295eff5beab6cbe5997e2a'
+local_modifications = False
+version = '2.0.2'
+long_version = 'beat-2.0.2-c70b2761d05a24fb53295eff5beab6cbe5997e2a-2024-08-19_12:10:07'  # noqa
+installed_date = '2024-08-19_12:10:07'
