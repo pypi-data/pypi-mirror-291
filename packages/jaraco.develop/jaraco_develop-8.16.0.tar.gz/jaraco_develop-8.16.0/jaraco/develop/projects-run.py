8ac073040515951233059e9e9f1a566a806c41f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:11 +0000

    Add package jaraco.develop/8.16.0/jaraco_develop-8.16.0.tar.gz

---
+++ packages/jaraco.develop/jaraco_develop-8.16.0.tar.gz/jaraco_develop-8.16.0/jaraco/develop/projects-run.py
@@ -0,0 +1,39 @@
+"""
+Routine to run a command across all projects.
+"""
+
+import functools
+import subprocess
+from typing import List, cast
+
+import typer
+from jaraco.ui.main import main
+from typing_extensions import Annotated
+
+from . import filters, git
+
+
+@functools.partial(
+    main,
+    app=typer.Typer(
+        context_settings=dict(allow_extra_args=True, ignore_unknown_options=True)
+    ),
+)
+def run(
+    tag: Annotated[
+        List[filters.Tag], typer.Option('--tag', '-t', parser=filters.Tag)
+    ] = [],
+    keyword: Annotated[
+        List[filters.Keyword],
+        typer.Option('--keyword', '-k', parser=filters.Keyword),
+    ] = [],
+    *,
+    ctx: typer.Context,
+):
+    cmd = cast(List[str], ctx.args)
+    selectors = filters.Selectors(tag + keyword)
+    for project in filter(selectors, git.projects()):
+        print(project, flush=True)
+        with git.temp_checkout(project, quiet=True):
+            subprocess.Popen(cmd).wait()
+        print(flush=True)
