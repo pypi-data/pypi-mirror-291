400d41aecaa3e164a6dbb2b52647b67545543d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:45 +0000

    Add package unit-simple/1.0.26/unit_simple-1.0.26-py3-none-any.whl

---
+++ packages/unit-simple/unit_simple-1.0.26-py3-none-any.whl/unit_simple/__init__.py
@@ -0,0 +1,8 @@
+"""
+unit_simple module initializer in order to import unit_simple instead of unit_simple.unit_simple
+"""
+
+from unit_simple.unit_simple import Factor, Unit, FundamentalUnit, DerivedUnit, TransformedUnit, UnitConverter, \
+                                     UnitConverters, Metric, Time, Length, Surface, Temperature, Intensity, Current, \
+                                     Mass, Angle, SolidAngle, Speed, Volume, Frequency, Substance, Force, Energy, \
+                                     Charge, Voltage, Si
