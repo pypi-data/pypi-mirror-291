254eba23b1595bb3e0a8e0bd3df5255738009d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:32 +0000

    Add package llmio/0.5.3/llmio-0.5.3.tar.gz

---
+++ packages/llmio/llmio-0.5.3.tar.gz/llmio-0.5.3/llmio/__init__.py
@@ -0,0 +1,18 @@
+from .assistant import (
+    Assistant,
+    Message,
+    UserMessage,
+    AssistantMessage,
+    ToolCall,
+    ToolMessage,
+)
+
+
+__all__ = [
+    "Assistant",
+    "Message",
+    "UserMessage",
+    "AssistantMessage",
+    "ToolCall",
+    "ToolMessage",
+]
