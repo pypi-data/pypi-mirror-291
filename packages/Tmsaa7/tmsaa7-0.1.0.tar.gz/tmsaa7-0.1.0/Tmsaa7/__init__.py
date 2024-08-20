b9295d356aa512018e372fd3702afab07f472ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 04:04:50 +0000

    Add package Tmsaa7/0.1.0/tmsaa7-0.1.0.tar.gz

---
+++ packages/Tmsaa7/tmsaa7-0.1.0.tar.gz/tmsaa7-0.1.0/Tmsaa7/__init__.py
@@ -0,0 +1,1 @@
+from .main import tmsa7
---
+++ packages/Tmsaa7/tmsaa7-0.1.0.tar.gz/tmsaa7-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Tmsaa7',
+    version='0.1.0',
+   # description='A small example library',
+   # long_description=open('README.md').read(),
+   # long_description_content_type='text/markdown',
+  #  author='Your Name',
+  #  author_email='your.email@example.com',
+  #  url='https://github.com/yourusername/my_library',
+    packages=find_packages(),
+  #  classifiers=[
+  #      'Programming Language :: Python :: 3',
+  #      'License :: OSI Approved :: MIT License',
+  #      'Operating System :: OS Independent',
+  #  ],
+ #   python_requires='>=3.6',
+    install_requires=[],
+)
