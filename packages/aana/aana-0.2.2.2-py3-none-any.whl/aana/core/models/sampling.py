9e9937366dee0de204f86292b0a03b99621d179b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:06 +0000

    Add package aana/0.2.2.2/aana-0.2.2.2-py3-none-any.whl

---
+++ packages/aana/aana-0.2.2.2-py3-none-any.whl/aana/core/models/sampling.py
@@ -0,0 +1,73 @@
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+__all__ = ["SamplingParams"]
+
+
+class SamplingParams(BaseModel):
+    """A model for sampling parameters of LLM.
+
+    Attributes:
+        temperature (float): Float that controls the randomness of the sampling. Lower
+            values make the model more deterministic, while higher values make
+            the model more random. Zero means greedy sampling.
+        top_p (float): Float that controls the cumulative probability of the top tokens
+            to consider. Must be in (0, 1]. Set to 1 to consider all tokens.
+        top_k (int): Integer that controls the number of top tokens to consider. Set
+            to -1 to consider all tokens.
+        max_tokens (int): The maximum number of tokens to generate.
+    """
+
+    temperature: float | None = Field(
+        default=None,
+        ge=0.0,
+        description=(
+            "Float that controls the randomness of the sampling. "
+            "Lower values make the model more deterministic, "
+            "while higher values make the model more random. "
+            "Zero means greedy sampling."
+        ),
+    )
+    top_p: float | None = Field(
+        default=None,
+        gt=0.0,
+        le=1.0,
+        description=(
+            "Float that controls the cumulative probability of the top tokens to consider. "
+            "Must be in (0, 1]. Set to 1 to consider all tokens."
+        ),
+    )
+    top_k: int | None = Field(
+        default=None,
+        description=(
+            "Integer that controls the number of top tokens to consider. "
+            "Set to -1 to consider all tokens."
+        ),
+    )
+    max_tokens: int | None = Field(
+        default=None, ge=1, description="The maximum number of tokens to generate."
+    )
+
+    @field_validator("top_k")
+    def check_top_k(cls, v: int):
+        """Validates a top_k argument.
+
+        Makes sure it is either -1, or at least 1.
+
+        Args:
+            v (int): Value to validate.
+
+        Raises:
+            ValueError: The value is not valid.
+
+        Returns:
+            The top_k value.
+        """
+        if v is None:
+            return v
+        if v < -1 or v == 0:
+            raise ValueError(f"top_k must be -1 (disable), or at least 1, got {v}.")  # noqa: TRY003
+        return v
+
+    model_config = ConfigDict(
+        json_schema_extra={"description": "Sampling parameters for generating text."}
+    )
---
+++ packages/aana/aana-0.2.2.2-py3-none-any.whl/aana/deployments/vllm_deployment.py
@@ -0,0 +1,174 @@
+import contextlib
+from collections.abc import AsyncGenerator
+from typing import Any
+
+from pydantic import AliasChoices, BaseModel, ConfigDict, Field
+from ray import serve
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.inputs import TokensPrompt
+
+from aana.core.models.base import merged_options
+from aana.core.models.custom_config import CustomConfig
+from aana.core.models.types import Dtype
+from aana.deployments.base_text_generation_deployment import (
+    BaseTextGenerationDeployment,
+    LLMOutput,
+)
+from aana.utils.gpu import get_gpu_memory
+
+with contextlib.suppress(ImportError):
+    from vllm.model_executor.utils import (
+        set_random_seed,  # Ignore if we don't have GPU and only run on CPU with test cache
+    )
+from vllm.sampling_params import SamplingParams as VLLMSamplingParams
+from vllm.utils import random_uuid
+
+from aana.core.models.base import pydantic_protected_fields
+from aana.core.models.sampling import SamplingParams
+from aana.exceptions.runtime import InferenceException, PromptTooLongException
+
+
+class VLLMConfig(BaseModel):
+    """The configuration of the vLLM deployment.
+
+    Attributes:
+        model_id (str): The model name.
+        dtype (Dtype): The data type. Defaults to Dtype.AUTO.
+        quantization (str | None): The quantization method. Defaults to None.
+        gpu_memory_reserved (float): The GPU memory reserved for the model in MB.
+        default_sampling_params (SamplingParams): The default sampling parameters.
+            Defaults to SamplingParams(temperature=0, max_tokens=256).
+        max_model_len (int | None): The maximum generated text length in tokens. Defaults to None.
+        chat_template (str | None): The name of the chat template. If not provided, the chat template
+            from the model will be used. Some models may not have a chat template.
+            Defaults to None.
+        enforce_eager (bool): Whether to enforce eager execution. Defaults to False.
+        engine_args (CustomConfig): Extra engine arguments. Defaults to {}.
+    """
+
+    model_id: str = Field(validation_alias=AliasChoices("model_id", "model"))
+    dtype: Dtype = Field(default=Dtype.AUTO)
+    quantization: str | None = Field(default=None)
+    gpu_memory_reserved: float
+    default_sampling_params: SamplingParams = SamplingParams(
+        temperature=0, max_tokens=256
+    )
+    max_model_len: int | None = Field(default=None)
+    chat_template: str | None = Field(default=None)
+    enforce_eager: bool = Field(default=False)
+    engine_args: CustomConfig = {}
+
+    model_config = ConfigDict(protected_namespaces=(*pydantic_protected_fields,))
+
+
+@serve.deployment
+class VLLMDeployment(BaseTextGenerationDeployment):
+    """Deployment to serve large language models using vLLM."""
+
+    async def apply_config(self, config: dict[str, Any]):
+        """Apply the configuration.
+
+        The method is called when the deployment is created or updated.
+
+        It loads the model and creates the engine.
+
+        The configuration should contain the following keys:
+        - model_id: the model name
+        - dtype: the data type (optional, default: "auto")
+        - quantization: the quantization method (optional, default: None)
+        - gpu_memory_reserved: the GPU memory reserved for the model in mb
+        - default_sampling_params: the default sampling parameters.
+        - max_model_len: the maximum generated text length in tokens (optional, default: None)
+        - chat_template: the name of the chat template (optional, default: None)
+        - enforce_eager: whether to enforce eager execution (optional, default: False)
+        - engine_args: extra engine arguments (optional, default: {})
+
+        Args:
+            config (dict): the configuration of the deployment
+        """
+        config_obj = VLLMConfig(**config)
+        self.model_id = config_obj.model_id
+        total_gpu_memory_bytes = get_gpu_memory()
+        total_gpu_memory_mb = total_gpu_memory_bytes / 1024**2
+        self.gpu_memory_utilization = (
+            config_obj.gpu_memory_reserved / total_gpu_memory_mb
+        )
+        self.default_sampling_params: SamplingParams = (
+            config_obj.default_sampling_params
+        )
+        self.chat_template_name = config_obj.chat_template
+
+        args = AsyncEngineArgs(
+            model=config_obj.model_id,
+            dtype=config_obj.dtype,
+            quantization=config_obj.quantization,
+            enforce_eager=config_obj.enforce_eager,
+            gpu_memory_utilization=self.gpu_memory_utilization,
+            max_model_len=config_obj.max_model_len,
+            **config_obj.engine_args,
+        )
+
+        # TODO: check if the model is already loaded.
+        # If it is and none of the model parameters changed, we don't need to reload the model.
+
+        # create the engine
+        self.engine = AsyncLLMEngine.from_engine_args(args)
+        self.tokenizer = self.engine.engine.tokenizer.tokenizer
+        self.model_config = await self.engine.get_model_config()
+
+    async def generate_stream(
+        self, prompt: str, sampling_params: SamplingParams | None = None
+    ) -> AsyncGenerator[LLMOutput, None]:
+        """Generate completion for the given prompt and stream the results.
+
+        Args:
+            prompt (str): the prompt
+            sampling_params (SamplingParams | None): the sampling parameters
+
+        Yields:
+            LLMOutput: the dictionary with the key "text" and the generated text as the value
+        """
+        prompt = str(prompt)
+
+        if sampling_params is None:
+            sampling_params = SamplingParams()
+        sampling_params = merged_options(self.default_sampling_params, sampling_params)
+
+        request_id = None
+        # tokenize the prompt
+        prompt_token_ids = self.tokenizer.encode(prompt)
+
+        if len(prompt_token_ids) > self.model_config.max_model_len:
+            raise PromptTooLongException(
+                prompt_len=len(prompt_token_ids),
+                max_len=self.model_config.max_model_len,
+            )
+
+        try:
+            # convert SamplingParams to VLLMSamplingParams
+            sampling_params_vllm = VLLMSamplingParams(
+                **sampling_params.model_dump(exclude_unset=True)
+            )
+            # start the request
+            request_id = random_uuid()
+            # set the random seed for reproducibility
+            set_random_seed(42)
+            results_generator = self.engine.generate(
+                sampling_params=sampling_params_vllm,
+                request_id=request_id,
+                inputs=TokensPrompt(prompt_token_ids=prompt_token_ids),
+            )
+
+            num_returned = 0
+            async for request_output in results_generator:
+                text_output = request_output.outputs[0].text[num_returned:]
+                yield LLMOutput(text=text_output)
+                num_returned += len(text_output)
+        except GeneratorExit:
+            # If the generator is cancelled, we need to cancel the request
+            if request_id is not None:
+                await self.engine.abort(request_id)
+            raise
+        except Exception as e:
+            raise InferenceException(model_name=self.model_id) from e
