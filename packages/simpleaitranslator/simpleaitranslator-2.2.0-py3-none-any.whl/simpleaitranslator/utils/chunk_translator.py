d6140efea192fc075e141fdce678bbc2c300b5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 06:03:50 +0000

    Add package simpleaitranslator/2.2.0/simpleaitranslator-2.2.0-py3-none-any.whl

---
+++ packages/simpleaitranslator/simpleaitranslator-2.2.0-py3-none-any.whl/simpleaitranslator/utils/chunk_translator.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+from simpleaitranslator.exceptions import MissingAPIKeyError
+from simpleaitranslator.utils.enums import ChatGPTModelForTranslator
+
+from openai import AsyncAzureOpenAI
+from openai import AsyncOpenAI
+
+
+
+
+
