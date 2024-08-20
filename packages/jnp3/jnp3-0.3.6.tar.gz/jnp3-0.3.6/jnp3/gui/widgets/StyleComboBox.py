a7c3c08a6637c4af01776da5e770b254429b04b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:50 +0000

    Add package jnp3/0.3.6/jnp3-0.3.6.tar.gz

---
+++ packages/jnp3/jnp3-0.3.6.tar.gz/jnp3-0.3.6/jnp3/gui/widgets/StyleComboBox.py
@@ -0,0 +1,53 @@
+# coding: utf8
+import sys
+from PySide6 import __version__ as pyside6_version
+from PySide6.QtCore import (
+    QAbstractListModel, QModelIndex,
+    Qt, QObject,
+)
+from PySide6.QtWidgets import (
+    QComboBox, QApplication, QWidget,
+)
+
+
+class _StyleListModel(QAbstractListModel):
+
+    def __init__(self, parent: QObject = None):
+        super().__init__(parent)
+        if sys.platform == "win32":
+            self.styles = ["windowsvista", "Fusion", "Windows"]
+            ver = list(map(lambda s: int(s), pyside6_version.split(".")))
+            if ver >= [6, 7, 0]:
+                self.styles.insert(0, "windows11")
+        elif sys.platform == "darwin":
+            self.styles = ["macOS", "Fusion", "Windows"]
+        elif sys.platform == "linux":
+            self.styles = ["Fusion", "Windows"]
+        else:
+            self.styles = []
+
+    def rowCount(self, parent: QModelIndex = ...):
+        return len(self.styles)
+
+    def data(self, index: QModelIndex, role: int = ...):
+        row = index.row()
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            return self.styles[row]
+
+
+class StyleComboBox(QComboBox):
+
+    def __init__(self, default_style: int = 0, parent: QWidget = None):
+        super().__init__(parent)
+        self.setModel(_StyleListModel(self))
+        self.currentIndexChanged.connect(self.on_self_current_index_changed)
+
+        self.setCurrentIndex(default_style)
+
+    def on_self_current_index_changed(self, index: int):
+        model = self.model()
+        idx = model.index(index, 0)
+        if idx.isValid():
+            style = model.data(idx, Qt.ItemDataRole.DisplayRole)
+            QApplication.setStyle(style)
