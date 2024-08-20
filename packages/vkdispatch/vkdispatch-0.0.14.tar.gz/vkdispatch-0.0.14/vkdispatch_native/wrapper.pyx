925340b8eebe97b2faab103de8daaf6ebd9974ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:30 +0000

    Add package vkdispatch/0.0.14/vkdispatch-0.0.14.tar.gz

---
+++ packages/vkdispatch/vkdispatch-0.0.14.tar.gz/vkdispatch-0.0.14/vkdispatch_native/wrapper.pyx
@@ -0,0 +1,18 @@
+# distutils: language=c++
+import numpy as np
+cimport numpy as np
+from libcpp cimport bool
+import sys
+
+cimport init
+cimport context
+cimport buffer
+cimport image
+cimport command_list
+cimport stage_transfer
+cimport stage_fft
+cimport stage_compute
+cimport descriptor_set
+cimport errors
+
+assert sizeof(int) == sizeof(np.int32_t)
