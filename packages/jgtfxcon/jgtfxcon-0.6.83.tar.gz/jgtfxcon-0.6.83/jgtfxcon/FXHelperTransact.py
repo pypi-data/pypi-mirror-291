9c922250bb21dc40195ff327ef8b5da286d7ef25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:24 +0000

    Add package jgtfxcon/0.6.83/jgtfxcon-0.6.83.tar.gz

---
+++ packages/jgtfxcon/jgtfxcon-0.6.83.tar.gz/jgtfxcon-0.6.83/jgtfxcon/FXHelperTransact.py
@@ -0,0 +1,22 @@
+
+import json
+from jgtutils.FXTransact import FXTrade,FXOrder
+
+
+def trade_row_to_trade_object(trade_row)->FXTrade:
+    string = _trade_row_to_string(trade_row)
+    return FXTrade.from_string(string)
+
+def _trade_row_to_string(trade_row)->str:
+    string=""
+    for column in trade_row.columns:
+        string += column.id + "=" + str(trade_row[column.id]) + "; "
+    return string
+  
+
+def print_jsonl_message(msg,extra_dict:dict=None):
+    o={}
+    o["message"]=msg
+    if extra_dict:
+        o.update(extra_dict)
+    print(json.dumps(o))
