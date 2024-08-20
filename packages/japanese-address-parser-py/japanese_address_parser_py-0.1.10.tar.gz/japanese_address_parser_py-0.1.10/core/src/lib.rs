459b7b4fab35e8667b3272f9c8cfbc913f82b896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:59 +0000

    Add package japanese-address-parser-py/0.1.10/japanese_address_parser_py-0.1.10.tar.gz

---
+++ packages/japanese-address-parser-py/japanese_address_parser_py-0.1.10.tar.gz/japanese_address_parser_py-0.1.10/core/src/lib.rs
@@ -0,0 +1,20 @@
+//! A Rust library to parse japanese addresses.
+//!
+//! ## Feature flags
+//! - `blocking`: Provide method that works synchronously
+//! - `city-name-correction`*(enabled by default)*: Enable autocorrection if ambiguous city name was typed
+
+#[cfg(all(target_family = "wasm", feature = "blocking"))]
+compile_error! {
+    "The `blocking` feature is not supported with wasm target."
+}
+
+pub mod api;
+mod domain;
+#[deprecated(since = "0.1.6", note = "This module will be deleted in v0.2")]
+pub mod entity;
+pub mod parser;
+mod repository;
+mod service;
+mod tokenizer;
+mod util;
