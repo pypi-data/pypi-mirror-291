fc4cb2d3c687796147d45cb513e3569ed6068f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:28 +0000

    Add package stidapi/1.2.1/stidapi-1.2.1-py3-none-any.whl

---
+++ packages/stidapi/stidapi-1.2.1-py3-none-any.whl/stidapi/utils.py
@@ -0,0 +1,48 @@
+import json
+from types import SimpleNamespace
+
+import requests
+
+
+def get_api_url():
+    return "https://stidapi.equinor.com/"
+
+
+def get_auth():
+    from msal_bearer.BearerAuth import BearerAuth, get_login_name
+
+    tenantID = "3aa4a235-b6e2-48d5-9195-7fcf05b459b0"
+    clientID = "1a35a8df-b48d-40df-987b-267968b1b198"  # stidapi-python
+    scopes = ["1734406c-3449-4192-a50d-7c3a63d3f57d/user_impersonation"]
+    auth = BearerAuth.get_auth(
+        tenantID=tenantID,
+        clientID=clientID,
+        scopes=scopes,
+        username=f"{get_login_name()}@equinor.com",
+    )
+
+    return auth
+
+
+def get_object_from_json(text: str):
+    if isinstance(text, list):
+        obj = [json.loads(x, object_hook=lambda d: SimpleNamespace(**d)) for x in text]
+    else:
+        obj = json.loads(text, object_hook=lambda d: SimpleNamespace(**d))
+    return obj
+
+
+def get_json(url: str):
+    response = requests.get(url, auth=get_auth())
+    # response.raise_for_status()
+    if response.status_code == 200:
+        try:
+            return response.json()
+        except json.JSONDecodeError:
+            print(
+                f"Warning: {str(url)} returned successfully, but not with a valid json response"
+            )
+    else:
+        print(f"Warning: {str(url)} returned status code {response.status_code}")
+
+    return []
