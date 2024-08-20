5b97a969528fe2735af3513cc98298d70d2c5c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:52 +0000

    Add package fitsImageProcess/0.4.0/fitsImageProcess-0.4.0-py3-none-any.whl

---
+++ packages/fitsImageProcess/fitsImageProcess-0.4.0-py3-none-any.whl/functions/__init__.py
@@ -0,0 +1,7 @@
+from .breakIntoChuncks import *
+# from .deepLearningCNN import *
+from .extraUtils import *
+from .find_rectangle import *
+from .functionsForAlignment import *
+from .manageCatalog import *
+from .manageSources import *
