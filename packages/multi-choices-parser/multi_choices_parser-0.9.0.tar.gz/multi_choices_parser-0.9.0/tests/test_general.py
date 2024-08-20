9292f7f4e6607c44adf8dcfe03dc0522a7a12781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 05:03:42 +0000

    Add package multi-choices-parser/0.9.0/multi_choices_parser-0.9.0.tar.gz

---
+++ packages/multi-choices-parser/multi_choices_parser-0.9.0.tar.gz/multi_choices_parser-0.9.0/tests/test_general.py
@@ -0,0 +1,66 @@
+import itertools
+from parser import MultiChoicesParser, end_symb
+
+
+def test_parse_incorrect() -> None:
+    l = [
+        ['the', 'an', "a", ""],
+        ['orange', 'apple', 'banana']
+    ]
+
+    parser = MultiChoicesParser(l)
+    to_parse_incorrect = [
+        ('z', True),
+        ("the", True),
+        ("appl", True),
+        ("a", True),
+        ("tzeorange", True)
+    ]
+
+    for p, check in to_parse_incorrect:
+        parser.reset()
+        for c in tuple(p) + (end_symb, ):
+            if check:
+                assert not parser.finished and not parser.success
+            parser.step(c)
+        assert not parser.success and parser.finished
+
+def test_parse_correct_without_empty():
+    l = [
+        ['the', 'an', "a"],
+        ['orange', 'apple', 'banana']
+    ]
+
+    parser = MultiChoicesParser(l)
+    to_parse_correct = [
+        x + y for x, y in itertools.product(l[0], l[1])
+    ]
+
+    for p in to_parse_correct:
+        parser.reset()
+        for c in tuple(p) + (end_symb, ):
+            assert not parser.finished and not parser.success
+            parser.step(c)
+        else:
+            print(p)
+            assert parser.finished and parser.success
+
+def test_parse_correct_with_empty():
+    l = [
+        ['the', 'an', "a", ""],
+        ['orange', 'apple', 'banana']
+    ]
+
+    parser = MultiChoicesParser(l)
+    to_parse_correct = [
+        x + y for x, y in itertools.product(l[0], l[1])
+    ]
+
+    for p in to_parse_correct:
+        parser.reset()
+        for c in tuple(p) + (end_symb, ):
+            assert not parser.finished and not parser.success
+            parser.step(c)
+        else:
+            print(p)
+            assert parser.finished and parser.success
