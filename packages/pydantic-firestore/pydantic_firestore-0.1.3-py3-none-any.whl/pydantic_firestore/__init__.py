eac8c55f6a0a6d3695ca3df5680b54b4cf4138a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:09 +0000

    Add package pydantic-firestore/0.1.3/pydantic_firestore-0.1.3-py3-none-any.whl

---
+++ packages/pydantic-firestore/pydantic_firestore-0.1.3-py3-none-any.whl/pydantic_firestore/__init__.py
@@ -0,0 +1,15 @@
+from .path import FirestorePath
+from .reference import FirestoreCollection, FirestoreDocument
+from .snapshot import FirestoreSnapshot
+from .location import FirestoreLocation
+
+
+__version__ = "0.1.3"
+
+__all__ = [
+    "FirestorePath",
+    "FirestoreCollection",
+    "FirestoreDocument",
+    "FirestoreSnapshot",
+    "FirestoreLocation",
+]
