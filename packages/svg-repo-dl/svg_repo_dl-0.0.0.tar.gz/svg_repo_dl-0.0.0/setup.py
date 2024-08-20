b89ca3b371709082198eb0aca3dd1eced5dc34e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 06:03:58 +0000

    Add package svg-repo-dl/0.0.0/svg_repo_dl-0.0.0.tar.gz

---
+++ packages/svg-repo-dl/svg_repo_dl-0.0.0.tar.gz/svg_repo_dl-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+    
+
+
+
+setup(
+    name             ='svg-repo-dl',
+    version          ='0.0.0',
+    description      ="SVGrepo-dl is a library & command-line tool for downloading SVG icons and illustrations from 'svgrepo.com'.",
+    #long_description              = open('README.md', 'r').read(),
+    #long_description_content_type = 'text/markdown',
+    author           ='Duckling Dean',
+    author_email     ='duckling.dean@proton.me',
+    packages         =["svgrepo_dl",],
+    #install_requires = [
+    #    "requests>=2.32.2,<3.0.0",
+    #   "fake-useragent==1.5.1",
+    #]
+)
+
+
