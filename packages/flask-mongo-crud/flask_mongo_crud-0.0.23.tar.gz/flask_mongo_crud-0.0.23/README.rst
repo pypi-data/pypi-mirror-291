d6b4c27238aecc005dd0d7e33c71c5de1b8fcf3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:03:50 +0000

    Add package flask-mongo-crud/0.0.23/flask_mongo_crud-0.0.23.tar.gz

---
+++ packages/flask-mongo-crud/flask_mongo_crud-0.0.23.tar.gz/flask_mongo_crud-0.0.23/README.rst
@@ -0,0 +1,25 @@
+=================================
+Flask-Mongo-CRUD
+=================================
+Flask-Mongo-CRUD is a Flask extension which generates CRUD endpoints out of the box.
+
+Features
+===============
+- Automatically generates CRUD endpoints from defined model.
+- Allows to specify custom MongoDB collection name of each model.
+- Allows to customize app base URL as well as each model's url prefix.
+- Allows to paginate when getting many documents from collection. ***TO BE IMPROVED***
+- Allows documents sorting (Ascending or Descending order). ***COMING SOON***
+
+Installation
+===============
+You can install Flask-Mongo-CRUD via Python Package Index:
+
+.. code:: bash
+
+    pip install flask-mongo-crud
+
+Documentation
+===============
+
+`See Flask Mongo CRUD's Documentation. <https://github.com/ValentineSean/flask-mongo-crud>`_
---
+++ packages/flask-mongo-crud/flask_mongo_crud-0.0.23.tar.gz/flask_mongo_crud-0.0.23/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+	"Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flask-mongo-crud",
+    version="0.0.23",
+    # description="This is a Flask Library that enables a developer to define mongo database models, and the library will automatically generate CRUD endpoints basing on Models defined. ;)",
+    # long_description=open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="",
+    author="Valentine Sean Chanengeta",
+    author_email="",
+    license="MIT",
+    classifiers=classifiers,
+    keywords=[
+        "flask-crud",
+        "flask-mongo-crud",
+        "flask-mongo-odm",
+        "flask-mongo-orm",
+	],
+    packages=find_packages(),
+    install_requires=[
+        "blinker==1.6.2",
+		"click==8.1.7",
+		"colorama==0.4.6",
+		"dnspython==2.4.2",
+		"executing==2.0.0",
+		"Flask==3.0.0",
+		"Flask-PyMongo==2.3.0",
+		"itsdangerous==2.1.2",
+		"Jinja2==3.1.2",
+		"MarkupSafe==2.1.3",
+		"pymongo==4.5.0",
+		"python-dotenv==1.0.0",
+		"varname==0.12.0",
+		"Werkzeug==3.0.0",
+	]
+)
