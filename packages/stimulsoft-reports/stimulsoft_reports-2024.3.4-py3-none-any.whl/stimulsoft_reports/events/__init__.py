1999af16d466ac1fd6a582d17b390bbcceb0bbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:02 +0000

    Add package stimulsoft-reports/2024.3.4/stimulsoft_reports-2024.3.4-py3-none-any.whl

---
+++ packages/stimulsoft-reports/stimulsoft_reports-2024.3.4-py3-none-any.whl/stimulsoft_reports/events/__init__.py
@@ -0,0 +1,7 @@
+from stimulsoft_data_adapters.events.StiConnectionEventArgs import StiConnectionEventArgs
+from stimulsoft_data_adapters.events.StiDataEventArgs import StiDataEventArgs
+
+from .StiEmailEventArgs import StiEmailEventArgs
+from .StiExportEventArgs import StiExportEventArgs
+from .StiReportEventArgs import StiReportEventArgs
+from .StiVariablesEventArgs import StiVariablesEventArgs
