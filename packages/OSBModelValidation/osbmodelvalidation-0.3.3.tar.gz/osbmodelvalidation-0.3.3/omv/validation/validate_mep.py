139096690e02004842f82491e3a25ce056281cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 04:04:56 +0000

    Add package OSBModelValidation/0.3.3/osbmodelvalidation-0.3.3.tar.gz

---
+++ packages/OSBModelValidation/osbmodelvalidation-0.3.3.tar.gz/osbmodelvalidation-0.3.3/omv/validation/validate_mep.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+from omv.validation.validate import OMVValidator
+from pkg_resources import resource_filename
+
+
+def validate(mep_src):
+    s = OMVValidator()
+    print("Validating document againt mep schema: ")
+    schema = resource_filename("omv", "schemata/mep.yaml")
+    print(s.validate(schema, mep_src))
+
+
+if __name__ == "__main__":
+    validate(sys.argv[1])
