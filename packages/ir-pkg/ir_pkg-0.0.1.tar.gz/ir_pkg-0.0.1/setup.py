d8acc5e10f0f52d641dc9c9c7d3233d78dc97fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:54 +0000

    Add package ir-pkg/0.0.1/ir_pkg-0.0.1.tar.gz

---
+++ packages/ir-pkg/ir_pkg-0.0.1.tar.gz/ir_pkg-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+# exclude server directory
+# exclude test files
+setup(
+    name='ir_pkg',
+    version='0.0.1',
+    url='https://github.com/vsaravind01/ir_pkg',
+    maintainer='Senapathy Veerasekaran',
+    classifiers=[],
+    description='description',
+    keywords=[],
+    project_urls={},
+    python_requires='>=3.7')
