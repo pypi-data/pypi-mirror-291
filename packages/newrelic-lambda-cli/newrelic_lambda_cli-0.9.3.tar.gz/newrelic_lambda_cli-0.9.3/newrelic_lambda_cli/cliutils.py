b9ae3be0d385ce6b3471ef6463cc361db445c0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:03:57 +0000

    Add package newrelic-lambda-cli/0.9.3/newrelic_lambda_cli-0.9.3.tar.gz

---
+++ packages/newrelic-lambda-cli/newrelic_lambda_cli-0.9.3.tar.gz/newrelic_lambda_cli-0.9.3/newrelic_lambda_cli/cliutils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import click
+import emoji
+
+from click.exceptions import Exit
+
+
+def done(message):
+    """Prints a done message to the terminal"""
+    click.echo(emoji.emojize(":sparkles: %s :sparkles:" % message, language="alias"))
+
+
+def failure(message, exit=False):
+    """Prints a failure message to the terminal"""
+    click.echo(
+        emoji.emojize(":heavy_multiplication_x: %s" % message, language="alias"),
+        color="red",
+        err=True,
+    )
+    if exit:
+        raise Exit(1)
+
+
+def success(message):
+    """Prints a success message to the terminal"""
+    click.echo(
+        emoji.emojize(":heavy_check_mark: %s" % message, language="alias"),
+        color="green",
+    )
+
+
+def warning(message):
+    """Prints a warningmessage to the terminal"""
+    click.echo(
+        emoji.emojize(":heavy_exclamation_mark: %s" % message, language="alias"),
+        color="blue",
+    )
