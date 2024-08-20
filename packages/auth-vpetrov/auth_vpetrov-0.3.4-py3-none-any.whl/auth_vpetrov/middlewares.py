74bc1773c3652a6532cf67527ebf664f6da10f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 06:04:15 +0000

    Add package auth-vpetrov/0.3.4/auth_vpetrov-0.3.4-py3-none-any.whl

---
+++ packages/auth-vpetrov/auth_vpetrov-0.3.4-py3-none-any.whl/auth_vpetrov/middlewares.py
@@ -0,0 +1,63 @@
+"""
+Necessary middlewares for the app.
+"""
+import requests
+from typing import Annotated
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from user_vpetrov.schemas import User, Admin
+from . import exceptions
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="http://localhost:8001/create/user")
+
+
+def require_admin_user(token: Annotated[str, Depends(oauth2_scheme)],):
+    """
+    Middleware to require an admin user.
+    """
+    # Request user from auth app.
+    print(token)
+    # Mock
+    admin = {
+        "id": 1,
+        "is_admin": True,
+        "name": "Admin User",
+        "email": "admin@domain.com"
+    }
+    if not admin:
+        raise exceptions.AuthorizationException()
+    return Admin(**admin)
+
+
+def require_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    """
+    Middleware to require a user.
+    """
+    url = "http://localhost/auth/user"
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise exceptions.AuthorizationException()
+    
+    # Request user from auth app.
+    print(token)
+    print(user)
+    user = response.json()
+    # Mock
+    # user = {
+    #     "id": 1,
+    #     "name": "Name",
+    #     "last_name": "Last Name",
+    #     "email": "email@domain.com",
+    #     "password": "passwordM@12",
+    #     "gender": "F",
+    #     "phone": "+56 9 1234 5678",
+    #     "rut": "11.111.111-1",
+    #     "birth_date": "1990-01-01"
+    # }
+    #   Decodificar el token JWT
+    if not user:
+        raise exceptions.AuthorizationException()
+    return User(**user)
