7a29dedf8b1529aac14e653a49f0c5714123caf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:01 +0000

    Add package composio-core/0.4.5/composio_core-0.4.5.tar.gz

---
+++ packages/composio-core/composio_core-0.4.5.tar.gz/composio_core-0.4.5/composio/tools/local/shelltool/shell_exec/tool.py
@@ -0,0 +1,20 @@
+"""Tool for executing shell commands."""
+
+import typing as t
+
+from composio.tools.local.base import Action, Tool
+from composio.tools.local.shelltool.shell_exec.actions.exec import ExecCommand
+from composio.tools.local.shelltool.shell_exec.actions.new import CreateShell
+from composio.tools.local.shelltool.shell_exec.actions.spawn import SpawnProcess
+
+
+class ShellTool(Tool):
+    """Tool for executing shell commands."""
+
+    def actions(self) -> t.List[t.Type[Action]]:
+        """Returns list of actions."""
+        return [ExecCommand, CreateShell, SpawnProcess]
+
+    def triggers(self) -> t.List:
+        """Returns list of triggers."""
+        return []
