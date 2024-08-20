63672e6eca48ac8624f56bb439724c81b436dd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 03:04:05 +0000

    Add package QuickColab/1.0.0/quickcolab-1.0.0.tar.gz

---
+++ packages/QuickColab/quickcolab-1.0.0.tar.gz/quickcolab-1.0.0/QuickColab/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+# 導入所需模組和類
+__version__ = "1.0.0"
+
+# /Users/timmylai/EasyColab/__init__.py
+import Element
+import Console
+import Client
+import prompts
+import tunnel.Audio
+import tunnel.QuickPrompt
+import config
+
+def main():
+    pass
