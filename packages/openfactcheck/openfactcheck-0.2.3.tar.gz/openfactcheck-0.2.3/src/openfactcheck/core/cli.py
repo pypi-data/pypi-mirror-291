ee5b3e701a787d2ed9f8b4a6b0d427c5247f6e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:58 +0000

    Add package openfactcheck/0.2.3/openfactcheck-0.2.3.tar.gz

---
+++ packages/openfactcheck/openfactcheck-0.2.3.tar.gz/openfactcheck-0.2.3/src/openfactcheck/core/cli.py
@@ -0,0 +1,29 @@
+import argparse
+
+from openfactcheck.core.base import OpenFactCheck
+from openfactcheck.lib.config import OpenFactCheckConfig
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Initialize OpenFactCheck with custom configuration.')
+    
+    # Add arguments here, example:
+    parser.add_argument("--config-path", 
+                        type=str, 
+                        help="Config File Path",
+                        default="config.json")
+    
+    # Parse arguments from command line
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    def callback(index, sample_name, solver_name, input_name, output_name, input, output, continue_run):
+        print(f"Callback: {index}, {sample_name}, {solver_name}, {input_name}, {output_name}, {input}, {output}, {continue_run}")
+
+    config = OpenFactCheckConfig(args.config_path)
+    results = OpenFactCheck(config).LLMEvaluator.evaluate(model_name="gpt2", 
+                                                          input_path="src/openfactcheck/templates/llm/responses_test.csv")
+
+                                            
