4fc2df3487219d4301440098d2fc89c937c79dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 05:04:24 +0000

    Add package changelist-sort/0.1/changelist_sort-0.1.tar.gz

---
+++ packages/changelist-sort/changelist_sort-0.1.tar.gz/changelist_sort-0.1/changelist_sort/input/__init__.py
@@ -0,0 +1,41 @@
+"""The Input Package for Changelist Sort
+"""
+from pathlib import Path
+
+from changelist_sort.input.argument_data import ArgumentData
+from changelist_sort.input.argument_parser import parse_arguments
+from changelist_sort.input.file_validation import validate_input_file
+from changelist_sort.input.input_data import InputData
+from changelist_sort.sorting.sort_mode import SortMode
+
+
+def validate_input(args_list: list[str]) -> InputData:
+    """
+    Validate the arguments and gather program input into InputData object.
+    - Parses command line strings into ArgumentData object.
+    - Finds Workspace file and reads it to a string.
+
+    Returns:
+    InputData - container for the program input.
+    """
+    arg_data = parse_arguments(args_list)
+    ws_path = _find_workspace_file(arg_data)
+    return InputData(
+        workspace_xml=validate_input_file(ws_path),
+        workspace_path=ws_path,
+        sort_mode=SortMode.DEVELOPER if arg_data.developer_sort else SortMode.MODULE,
+    )
+
+
+def _find_workspace_file(arg_data: ArgumentData) -> Path:
+    """
+    Generate a Path to the Workspace File.
+    - If Path is given by CLI arguments, use it.
+    - Otherwise, assume current directory is project root.
+
+    Returns:
+    Path - A Path object. Note, the actual file may not exist.
+    """
+    if arg_data.workspace_path is None:
+        return Path('.') / '.idea' / 'workspace.xml'
+    return Path(arg_data.workspace_path)
---
+++ packages/changelist-sort/changelist_sort-0.1.tar.gz/changelist_sort-0.1/changelist_sort/input/argument_data.py
@@ -0,0 +1,20 @@
+"""The Arguments Received from the Command Line Input.
+
+This DataClass is created after the argument syntax is validated.
+
+Syntax Validation:
+-
+"""
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ArgumentData:
+    """
+    The syntactically valid arguments recevied by the Program.
+
+    Fields:
+    - workspace_path (str): The path to the workspace file.
+    """
+    workspace_path: str | None
+    developer_sort: bool = False
