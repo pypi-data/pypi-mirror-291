2773cf8bc5ac84945c53864d6edfd604aba00021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 15:03:45 +0000

    Add package fake-bpy-module-latest/20240820/fake_bpy_module_latest-20240820-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240820-py3-none-any.whl/_bpy_internal/__init__.pyi
@@ -0,0 +1,9 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import addons as addons
+from . import extensions as extensions
+from . import freedesktop as freedesktop
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
