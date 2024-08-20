7f474897651041a2dbdba2b06061b816ca6dbf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 05:03:51 +0000

    Add package datoso-seed-fbneo/1.0.0/datoso_seed_fbneo-1.0.0.tar.gz

---
+++ packages/datoso-seed-fbneo/datoso_seed_fbneo-1.0.0.tar.gz/datoso_seed_fbneo-1.0.0/src/datoso_seed_fbneo/__init__.py
@@ -0,0 +1,6 @@
+"""Initialize seed variables."""
+__all__ = ['__version__', '__author__', '__description__']
+__version__ = '1.0.0'
+__author__ = 'Lacides Miranda'
+__description__ = 'Emulator for Arcade Games & Select Consoles.'
+__prefix__ = 'fbneo'
