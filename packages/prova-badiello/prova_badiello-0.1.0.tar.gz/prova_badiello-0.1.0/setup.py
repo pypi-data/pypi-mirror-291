25f85a1d0147ada661193888c9f3738a31134ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 14:04:34 +0000

    Add package prova-badiello/0.1.0/prova_badiello-0.1.0.tar.gz

---
+++ packages/prova-badiello/prova_badiello-0.1.0.tar.gz/prova_badiello-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prova_badiello",
+    version="0.1.0",
+    description="A simple package that prints Hello World",
+    author="Il tuo nome",
+    author_email="marco.baddy04@gmial.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
