47ca529a2efa2302651182812efe3f01af33462f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 20:04:29 +0000

    Add package micro-smart-hub/0.1.8/micro_smart_hub-0.1.8.tar.gz

---
+++ packages/micro-smart-hub/micro_smart_hub-0.1.8.tar.gz/micro_smart_hub-0.1.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="micro-smart-hub",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flake8',
+        'numpy',
+        'requests',
+        'pyyaml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'micro-smart-server=micro_smart_server.micro_smart_server:main',
+        ],
+    },
+    author="Aleksander Stanik (Olek)",
+    author_email="aleksander.stanik@hammerheadsengineers.com",
+    description="A small smart hub building blocks package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/AleksanderStanikHE/micro-smart-hub.git",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
