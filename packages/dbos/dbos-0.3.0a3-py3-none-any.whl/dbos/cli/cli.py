c6753a6a4e119b145743d4f653c61b1069c4e29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 05:04:49 +0000

    Add package dbos/0.3.0a3/dbos-0.3.0a3-py3-none-any.whl

---
+++ packages/dbos/dbos-0.3.0a3-py3-none-any.whl/dbos/cli/cli.py
@@ -0,0 +1,80 @@
+import subprocess
+
+import typer
+
+from dbos.application_database import ApplicationDatabase
+from dbos.dbos_config import load_config
+from dbos.system_database import SystemDatabase
+
+app = typer.Typer()
+
+
+@app.command()
+def start() -> None:
+    config = load_config()
+    start_commands = config["runtimeConfig"]["start"]
+    for command in start_commands:
+        typer.echo(f"Executing: {command}")
+        result = subprocess.run(command, shell=True, text=True)
+        if result.returncode != 0:
+            typer.echo(f"Command failed: {command}")
+            typer.echo(result.stderr)
+            raise typer.Exit(code=1)
+
+
+@app.command()
+def create() -> None:
+    pass
+
+
+@app.command()
+def migrate() -> None:
+    config = load_config()
+    if not config["database"]["password"]:
+        typer.echo(
+            "DBOS configuration does not contain database password, please check your config file and retry!"
+        )
+        raise typer.Exit(code=1)
+    app_db_name = config["database"]["app_db_name"]
+
+    typer.echo(f"Starting schema migration for database {app_db_name}")
+
+    # First, run DBOS migrations on the system database and the application database
+    app_db = None
+    sys_db = None
+    try:
+        sys_db = SystemDatabase(config)
+        app_db = ApplicationDatabase(config)
+    except Exception as e:
+        typer.echo(f"DBOS system schema migration failed: {e}")
+    finally:
+        if sys_db:
+            sys_db.destroy()
+        if app_db:
+            app_db.destroy()
+
+    # Next, run any custom migration commands specified in the configuration
+    try:
+        migrate_commands = (
+            config["database"]["migrate"]
+            if "migrate" in config["database"] and config["database"]["migrate"]
+            else []
+        )
+        for command in migrate_commands:
+            typer.echo(f"Executing migration command: {command}")
+            result = subprocess.run(command, shell=True, text=True)
+            if result.returncode != 0:
+                typer.echo(f"Migration command failed: {command}")
+                typer.echo(result.stderr)
+                raise typer.Exit(1)
+            if result.stdout:
+                typer.echo(result.stdout.rstrip())
+    except Exception as e:
+        typer.echo(f"An error occurred during schema migration: {e}")
+        raise typer.Exit(code=1)
+
+    typer.echo(f"Completed schema migration for database {app_db_name}")
+
+
+if __name__ == "__main__":
+    app()
