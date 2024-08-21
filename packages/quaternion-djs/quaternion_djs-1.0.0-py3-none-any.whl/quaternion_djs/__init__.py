17e20d0cbd328c2ff231c8ef54fc269bc7cfe6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:23 +0000

    Add package quaternion-djs/1.0.0/quaternion_djs-1.0.0-py3-none-any.whl

---
+++ packages/quaternion-djs/quaternion_djs-1.0.0-py3-none-any.whl/quaternion_djs/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024 James Strudwick
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+"""Entry point for the package."""
+
+from .quaternion import Quaternion as Quaternion
