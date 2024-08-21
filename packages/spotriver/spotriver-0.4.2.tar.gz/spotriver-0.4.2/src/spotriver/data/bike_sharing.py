24ff622a9ff06ca7d606aac1d1c3e322bb268aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:44 +0000

    Add package spotriver/0.4.2/spotriver-0.4.2.tar.gz

---
+++ packages/spotriver/spotriver-0.4.2.tar.gz/spotriver-0.4.2/src/spotriver/data/bike_sharing.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import fetch_openml
+
+
+def get_bike_sharing_data(train_size=0.6):
+    """
+    Fetches the Bike Sharing Demand dataset from OpenML and splits it into training and test sets.
+
+    Args:
+        train_size (float):
+            The proportion of the dataset to include in the training set. Default value: 0.6
+
+    Returns:
+        (tuple): tuple containing:
+            df (pd.DataFrame): The full dataset.
+            train (pd.DataFrame): The training set.
+            test (pd.DataFrame): The test set.
+
+    Examples:
+        >>> from spotriver.data.bike_sharing import get_bike_sharing_data
+        >>> df, train, test = get_bike_sharing_data(train_size=0.6)
+    """
+
+    bike_sharing = fetch_openml("Bike_Sharing_Demand", version=2, as_frame=True, parser="pandas")
+    df = bike_sharing.frame
+    # Normalize the count column
+    df["count"] = df["count"] / df["count"].max()
+    # Replace heavy_rain with rain in the weather column
+    df["weather"].replace(to_replace="heavy_rain", value="rain", inplace=True)
+    n = df.shape[0]
+    # Calculate the number of rows in the training set
+    k = int(n * train_size)
+    # Split the data into training and test sets
+    train = df[0:k]
+    test = df[k:n]
+    return df, train, test
---
+++ packages/spotriver/spotriver-0.4.2.tar.gz/spotriver-0.4.2/src/spotriver/plot/stats.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+
+def corrplot(df: pd.DataFrame, numeric_only=True) -> None:
+    """Function plots a graphical correlation matrix for each pair of columns in the dataframe.
+        The function takes a DataFrame df as input and generates a graphical correlation matrix
+        for each pair of columns in the dataframe.
+        The upper triangle of the correlation matrix is masked out and set to NaN values.
+        The resulting matrix is then styled and returned as a heatmap with colors
+        ranging from blue (for negative correlations) to red (for positive correlations).
+
+    Args:
+        df (pd.DataFrame): Data to plot.
+        numeric_only (bool, default True):
+            Include only float, int or boolean data.
+    Returns:
+        (pd.DataFrame): A styled correlation matrix heatmap.
+
+    Examples:
+        >>> from spotriver.plot.stats import corrplot
+            import pandas as pd
+            X = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]})
+            corrplot(X)
+
+    """
+
+    # Compute the correlation matrix
+    corr = df.corr(numeric_only=numeric_only)
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr, dtype=bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set values in the upper triangle to NaN
+    corr[mask] = np.nan
+
+    # Apply styling to the correlation matrix and return it as a heatmap
+    return corr.style.background_gradient(cmap="coolwarm", axis=None, vmin=-1, vmax=1).highlight_null(color="#f1f1f1")
