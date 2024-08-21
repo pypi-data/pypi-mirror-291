8448953ea7779cff4c3b8571fc0682ceaf0951a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:02 +0000

    Add package self-python-instr/0.1.3/self_python_instr-0.1.3.tar.gz

---
+++ packages/self-python-instr/self_python_instr-0.1.3.tar.gz/self_python_instr-0.1.3/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    # name='skyeye-python-instrumentator',
+    # version='0.1.20',
+    name='self-python-instr',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'starlette'
+    ],
+    description='Skyeye Python FastAPI R.E.D ',
+    author='zr',
+    author_email='yihengrui1206@163.com',
+    url='https://artifact.srdcloud.cn/artifactory/api/pypi/ctcai-oshare-pypi-mc/skyeye-python-instrumentator',
+)
