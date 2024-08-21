a8ebe8962351058722f9769d502c852f87bb248e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:04:42 +0000

    Add package graph-vect-rag/0.5/graph_vect_rag-0.5.tar.gz

---
+++ packages/graph-vect-rag/graph_vect_rag-0.5.tar.gz/graph_vect_rag-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graph_vect_rag",
+    version=0.5,
+    packages=find_packages(),
+    install_requires=[
+        "llama-index",
+        "llama-index-llms-groq",
+        "llama-index-embeddings-huggingface",
+        "llama-index-graph-stores-neo4j",
+        "sentence-transformers",
+        "python-dotenv",
+        "setuptools",
+    ],
+)
