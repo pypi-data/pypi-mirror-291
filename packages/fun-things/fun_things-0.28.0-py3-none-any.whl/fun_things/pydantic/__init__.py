d58a2390f8efd6c367d5ef51e036c66820bc186a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:10 +0000

    Add package fun-things/0.28.0/fun_things-0.28.0-py3-none-any.whl

---
+++ packages/fun-things/fun_things-0.28.0-py3-none-any.whl/fun_things/pydantic/__init__.py
@@ -0,0 +1,3 @@
+from .mongo_annotation import MongoAnnotation, ignore_null
+from .mongo_annotation_post_processor import MongoAnnotationUpdatePostProcessor
+from .mongo_annotation_field import MongoAnnotationField
