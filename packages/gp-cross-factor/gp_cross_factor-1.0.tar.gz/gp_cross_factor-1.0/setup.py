c8887ce9465e55a473e75e6e3d302a70e389a25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:46 +0000

    Add package gp-cross-factor/1.0/gp_cross_factor-1.0.tar.gz

---
+++ packages/gp-cross-factor/gp_cross_factor-1.0.tar.gz/gp_cross_factor-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gp_cross_factor',  
+    version='1.0',  
+    author='AlfredCYL',  
+    author_email='alfred.yl@outlook.com', 
+    description='A tool for genetic programming based cross-factor analysis',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
+    url='https://github.com/AlfredCYL/gplearn_cross_factor',  
+    packages=find_packages(), 
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'scikit-learn',
+        'joblib',
+    ],
+    include_package_data=False, # exclude non python files
+)
