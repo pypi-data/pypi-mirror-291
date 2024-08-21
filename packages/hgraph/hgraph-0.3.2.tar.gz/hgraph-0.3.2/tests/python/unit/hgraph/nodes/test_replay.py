e989879536dc2cbc2148a0a7d3402b268d52f600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:04:39 +0000

    Add package hgraph/0.3.2/hgraph-0.3.2.tar.gz

---
+++ packages/hgraph/hgraph-0.3.2.tar.gz/hgraph-0.3.2/tests/python/unit/hgraph/nodes/test_replay.py
@@ -0,0 +1,14 @@
+from hgraph import graph, TS, run_graph, print_, replay
+from hgraph._impl._operators._record_replay_in_memory import SimpleArrayReplaySource, set_replay_values
+
+
+def test_replay_simple():
+
+    set_replay_values("test", SimpleArrayReplaySource(["1", "2", "3"]))
+
+    @graph
+    def main():
+        values = replay("test", TS[str])
+        print_(values)
+
+    run_graph(main)
