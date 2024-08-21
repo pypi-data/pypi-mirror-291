219d859307927a201aee886cec05713fa602452b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:05:10 +0000

    Add package udata-hydra/2.0.0.dev2867/udata_hydra-2.0.0.dev2867.tar.gz

---
+++ packages/udata-hydra/udata_hydra-2.0.0.dev2867.tar.gz/udata_hydra-2.0.0.dev2867/udata_hydra/analysis/helpers.py
@@ -0,0 +1,28 @@
+from datetime import date, datetime
+from typing import Union
+
+from dateparser import parse as date_parser
+from dateutil.parser import ParserError
+from dateutil.parser import parse as dateutil_parser
+
+
+def to_json(value: str) -> str:
+    """Convenience method, should be casted from string directly by postgres"""
+    return value
+
+
+def _parse_dt(value: str) -> Union[datetime, None]:
+    """For performance reasons, we try first with dateutil and fallback on dateparser"""
+    try:
+        return dateutil_parser(value)
+    except ParserError:
+        return date_parser(value)
+
+
+def to_date(value: str) -> Union[date, None]:
+    parsed = _parse_dt(value)
+    return parsed.date() if parsed else None
+
+
+def to_datetime(value: str) -> Union[datetime, None]:
+    return _parse_dt(value)
