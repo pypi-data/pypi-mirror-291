597498590afff1b58c3528e83fdb6493b5fd9650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:05 +0000

    Add package apropos-ai/0.1.6/apropos_ai-0.1.6.tar.gz

---
+++ packages/apropos-ai/apropos_ai-0.1.6.tar.gz/apropos_ai-0.1.6/apropos/src/bench/__init__.py
@@ -0,0 +1,7 @@
+from .bigcodebench.main import BigCodeBenchComplete_Benchmark
+from .bigcodebench.single_step_dag import code_problem_single_step
+
+from .gradeschool_math.main import GSM8k_Benchmark
+from .hendryks_math.main import HendryksMath_Benchmark
+
+from .crafter.game_dynamics import StatefulCrafterACI
