e4831a3ac82cad3b99f5109ca60f735c3969294e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:45 +0000

    Add package luxa/0.1.0/luxa-0.1.0.tar.gz

---
+++ packages/luxa/luxa-0.1.0.tar.gz/luxa-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luxa",
+    version="0.1.0",
+    description="Fast build system for jailbreak tweaks",
+    author="Madeline Göttlicher",
+    packages=find_packages()
+)
