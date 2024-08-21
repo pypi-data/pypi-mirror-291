f88aede3f73c2fed244dccb534cc281728daa3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:40 +0000

    Add package fennel-ai/1.5.5/fennel_ai-1.5.5.tar.gz

---
+++ packages/fennel-ai/fennel_ai-1.5.5.tar.gz/fennel_ai-1.5.5/fennel/__init__.py
@@ -0,0 +1,19 @@
+import fennel.client
+import fennel.connectors
+from fennel.datasets import (
+    dataset,
+    field,
+    pipeline,
+    Dataset,
+    Count,
+    Distinct,
+    Sum,
+    LastK,
+    Min,
+    Max,
+    Average,
+    AggregateType,
+    Stddev,
+)
+from fennel.featuresets import featureset, feature, extractor
+from fennel.lib import meta
