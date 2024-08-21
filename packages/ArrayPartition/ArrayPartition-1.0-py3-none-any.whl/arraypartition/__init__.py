1c7235406e77ebe65612039856fc80c8221534ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:04:31 +0000

    Add package ArrayPartition/1.0/ArrayPartition-1.0-py3-none-any.whl

---
+++ packages/ArrayPartition/ArrayPartition-1.0-py3-none-any.whl/arraypartition/__init__.py
@@ -0,0 +1,11 @@
+from .partition import (
+    ArrayLike,
+    SuperLazyArrayLike,
+    ArrayPartition,
+    get_chunk_extent,
+    get_chunk_positions,
+    get_chunk_shape,
+    get_chunk_space,
+    get_dask_chunks,
+    combine_slices
+)
