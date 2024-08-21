e6474797b74c777546d1f15d16861dbf94c72f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 07:03:57 +0000

    Add package Trinsic-Connect/0.2.3/Trinsic_Connect-0.2.3-py3-none-any.whl

---
+++ packages/Trinsic-Connect/Trinsic_Connect-0.2.3-py3-none-any.whl/trinsic_connect/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from trinsic_connect.api.attachments_api import AttachmentsApi
+from trinsic_connect.api.network_api import NetworkApi
+from trinsic_connect.api.sessions_api import SessionsApi
+
