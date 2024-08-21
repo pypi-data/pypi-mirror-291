b1b51fe41c7dc16549f4cddf31799ab977c9cf2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:36 +0000

    Add package python-cdf/0.9.3/python_cdf-0.9.3-py3-none-any.whl

---
+++ packages/python-cdf/python_cdf-0.9.3-py3-none-any.whl/src/cdf/proxy/__init__.py
@@ -0,0 +1,13 @@
+"""The proxy module provides a MySQL proxy server for the CDF.
+
+The proxy server is used to intercept MySQL queries and execute them using SQLMesh.
+This allows it to integrate with BI tools and other MySQL clients. Furthermore,
+during interception, the server can rewrite queries expanding semantic references
+making it an easy to use semantic layer for SQLMesh.
+"""
+
+from cdf.proxy.mysql import run_mysql_proxy
+from cdf.proxy.planner import run_plan_server
+
+
+__all__ = ["run_mysql_proxy", "run_plan_server"]
