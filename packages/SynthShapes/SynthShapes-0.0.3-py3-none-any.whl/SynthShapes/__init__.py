430f515297a85982187b2f25b48fb37be502ca0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 06:05:26 +0000

    Add package SynthShapes/0.0.3/SynthShapes-0.0.3-py3-none-any.whl

---
+++ packages/SynthShapes/SynthShapes-0.0.3-py3-none-any.whl/SynthShapes/__init__.py
@@ -0,0 +1,13 @@
+from . import blending
+from . import filters
+from . import hist
+from . import shapes
+from . import texturizing
+from . import utils
+
+from blending import *
+from filters import *
+from hist import *
+from shapes import *
+from texturizing import *
+from utils import *
