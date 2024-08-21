3a63dab72cd4a1b88c43842211206dcdb85c71e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:16 +0000

    Add package pkrsplitter/1.1.3/pkrsplitter-1.1.3.tar.gz

---
+++ packages/pkrsplitter/pkrsplitter-1.1.3.tar.gz/pkrsplitter-1.1.3/pkrsplitter/runs/local/split_new_histories.py
@@ -0,0 +1,7 @@
+"""This script is used to split new raw histories in the local file system."""
+from pkrsplitter.splitters.local import LocalFileSplitter
+from pkrsplitter.settings import DATA_DIR
+
+if __name__ == "__main__":
+    splitter = LocalFileSplitter(DATA_DIR)
+    splitter.split_new_histories()
