8db44f6ea9a193ff49e6cad220c1a4a6cdf70331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:04:54 +0000

    Add package ssscoring/1.5.0/ssscoring-1.5.0-py3-none-any.whl

---
+++ packages/ssscoring/ssscoring-1.5.0-py3-none-any.whl/ssscoring/__init__.py
@@ -0,0 +1,33 @@
+# See: https://github.com/pr3d4t0r/SSScoring/blob/master/LICENSE.txt
+
+"""
+Speed Skydiving Scoring tools package for processing FlySight v1 and v2 CSV data
+files.
+
+This library relies on NumPy and pandas.  When running in a Lucyfer notebook,
+it also requires the Bokeh plotting library.
+
+The documentation for each module in this package is linked from the navigation
+bar.  The **fs1** module contains functions that process v1 files.  The v2
+implementation is in progress in a private Git branch.
+
+## Source code
+
+You are welcome to **<a href='https://github.com/pr3d4t0r/SSScoring' target='_new'>fork SSScoring</a>**
+on GitHub.  You will need Python 3.9 or later, pandas, and NumPy as minimum
+requirements.
+
+You may see the source files here:
+
+<a href='https://github.com/pr3d4t0r/SSScoring/tree/master/ssscoring' target='_new'>https://github.com/pr3d4t0r/SSScoring/tree/master/ssscoring</a>
+"""
+
+import importlib.metadata
+
+
+__VERSION__ = importlib.metadata.version('ssscoring')
+"""
+@public
+"""
+
+
