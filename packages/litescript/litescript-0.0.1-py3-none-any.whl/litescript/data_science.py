f8ea06c60af28037de952fe10558c3c4d66ec5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:04:28 +0000

    Add package litescript/0.0.1/litescript-0.0.1-py3-none-any.whl

---
+++ packages/litescript/litescript-0.0.1-py3-none-any.whl/litescript/data_science.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+from .vis_utils import *
+from .logging_module import setup_logger
+from .scripting import *
+
+__all__ = [
+    "np",
+    "pd",
+    "setup_logger",
+    "os",
+    "sys",
+    "json",
+    "Path",
+    "datetime",
+    "tqdm",
+    "glob",
+    "yaml",
+    "Console",
+    "Table",
+    "rprint",
+]
---
+++ packages/litescript/litescript-0.0.1-py3-none-any.whl/litescript/scripting.py
@@ -0,0 +1,31 @@
+"""
+Scripting related libraries 
+"""
+
+import os
+import sys
+import json
+from pathlib import Path
+from datetime import datetime
+from tqdm import tqdm
+from glob import glob
+import yaml
+from rich.console import Console
+from rich.table import Table
+from rich import print as rprint
+import time
+
+__all__ = [
+    "os",
+    "time",
+    "sys",
+    "json",
+    "Path",
+    "datetime",
+    "tqdm",
+    "glob",
+    "yaml",
+    "Console",
+    "Table",
+    "rprint",
+]
