6cc5f8223df479c48ca8733a9a124b072f754fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:05:12 +0000

    Add package packstack/23.0.0.0rc2.dev2/packstack-23.0.0.0rc2.dev2.tar.gz

---
+++ packages/packstack/packstack-23.0.0.0rc2.dev2.tar.gz/packstack-23.0.0.0rc2.dev2/packstack/puppet/modules/packstack/manifests/swift/ringbuilder.pp
@@ -0,0 +1,33 @@
+class packstack::swift::ringbuilder ()
+{
+    class { 'swift::ringbuilder':
+      part_power     => '18',
+      replicas       => lookup('CONFIG_SWIFT_STORAGE_REPLICAS'),
+      min_part_hours => 1,
+      require        => Class['swift'],
+    }
+
+    if ($::operatingsystem == 'CentOS') and (versioncmp($::operatingsystemmajrelease, '9') == 0) {
+      # sets up an rsync db that can be used to sync the ring DB
+      class { 'swift::ringserver':
+        local_net_ip => lookup('CONFIG_STORAGE_HOST_URL'),
+        rsync_use_xinetd => false,
+      }
+    } else {
+      # sets up an rsync db that can be used to sync the ring DB
+      class { 'swift::ringserver':
+        local_net_ip => lookup('CONFIG_STORAGE_HOST_URL'),
+      }
+    }
+
+    if str2bool($::selinux) {
+      selboolean { 'rsync_export_all_ro':
+        value      => on,
+        persistent => true,
+      }
+    }
+
+    create_resources(ring_account_device, lookup('SWIFT_RING_ACCOUNT_DEVICES', undef, undef, {}))
+    create_resources(ring_object_device, lookup('SWIFT_RING_OBJECT_DEVICES', undef, undef, {}))
+    create_resources(ring_container_device, lookup('SWIFT_RING_CONTAINER_DEVICES', undef, undef, {}))
+}
