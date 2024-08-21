f43fcd0d21ef40c82c72555d65118b2874c4a4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:04:39 +0000

    Add package modoboa/2.3.1/modoboa-2.3.1.tar.gz

---
+++ packages/modoboa/modoboa-2.3.1.tar.gz/modoboa-2.3.1/modoboa/frontend_dist/assets/logs-BYhk589h.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./index-DF9UIFAh.js";const t="logs",a={getAuditTrail(s){return e.get(`/${t}/audit-trail/`,{params:s})},getMessages(s){return e.get(`/${t}/messages/`,{params:s})}};export{a as l};
