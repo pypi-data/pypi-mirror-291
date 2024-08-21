438aa77eb7785d49a41cb3f7bdff4c3e9eb50e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:04:28 +0000

    Add package litescript/0.0.1/litescript-0.0.1.tar.gz

---
+++ packages/litescript/litescript-0.0.1.tar.gz/litescript-0.0.1/tests/test_datascience.py
@@ -0,0 +1,20 @@
+from litescript.data_science import *  # noqa
+
+
+def test_exports():
+    assert {
+        "np",
+        "pd",
+        "setup_logger",
+        "os",
+        "sys",
+        "json",
+        "Path",
+        "datetime",
+        "tqdm",
+        "glob",
+        "yaml",
+        "Console",
+        "Table",
+        "rprint",
+    } < set(globals())
