1ea077a2d965501c122f5c4db99e4dc28bcb2de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:05:23 +0000

    Add package credsweeper/1.8.4/credsweeper-1.8.4-py3-none-any.whl

---
+++ packages/credsweeper/credsweeper-1.8.4-py3-none-any.whl/credsweeper/filters/value_allowlist_check.py
@@ -0,0 +1,39 @@
+import re
+
+from credsweeper.config import Config
+from credsweeper.credentials import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters import Filter
+from credsweeper.utils import Util
+
+
+class ValueAllowlistCheck(Filter):
+    """Check that patterns from the list is not present in the candidate value."""
+
+    ALLOWED = [
+        r"ENC\(.*\)", r"ENC\[.*\]", r"\$\{.*\}", r"#\{.*\}", r"\{\{.+\}\}", r"([.a-z0-9]|->)+\(.*\)", r"\S{0,5}\*{5,}",
+        r".*@@@hl@@@(암호|비번|PW|PASS)@@@endhl@@@.*"
+    ]
+    ALLOWED_PATTERN = re.compile(  #
+        Util.get_regex_combine_or(ALLOWED),  #
+        flags=re.IGNORECASE)
+
+    def __init__(self, config: Config = None) -> None:
+        pass
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received credential candidate data 'line_data'.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, if need to filter candidate and False if left
+
+        """
+
+        if self.ALLOWED_PATTERN.match(line_data.value):
+            return True
+
+        return False
---
+++ packages/credsweeper/credsweeper-1.8.4-py3-none-any.whl/credsweeper/filters/value_last_word_check.py
@@ -0,0 +1,30 @@
+import re
+
+from credsweeper.config import Config
+from credsweeper.credentials import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters import Filter
+
+
+class ValueLastWordCheck(Filter):
+    """Check that secret is not short value that ends with `:`."""
+
+    NOT_ALLOWED_COLON_PATTERN = re.compile(".*:$", flags=re.IGNORECASE)
+
+    def __init__(self, config: Config = None) -> None:
+        pass
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received credential candidate data 'line_data'.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, if need to filter candidate and False if left
+
+        """
+        if len(line_data.value) < 16 and self.NOT_ALLOWED_COLON_PATTERN.search(line_data.value):
+            return True
+        return False
