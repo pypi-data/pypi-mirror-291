c9930df699b69fe443a796f5a057510963c94ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:50 +0000

    Add package resizewin/0.1.1/resizewin-0.1.1.tar.gz

---
+++ packages/resizewin/resizewin-0.1.1.tar.gz/resizewin-0.1.1/docs/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+import resizewin
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'resizewin'
+copyright = '2024, Eugene Kim'
+author = 'Eugene Kim'
+release = resizewin.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+default_role = 'any'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
---
+++ packages/resizewin/resizewin-0.1.1.tar.gz/resizewin-0.1.1/docs/index.rst
@@ -0,0 +1,15 @@
+.. resizewin documentation master file, created by
+   sphinx-quickstart on Tue Aug 20 15:15:33 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+`resizewin`
+===========
+
+.. automodule:: resizewin
+   :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
