ed8df36ead579f3b253bd4ec8c6f952d5818c13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:02 +0000

    Add package syngular/0.2.3/syngular-0.2.3-py3-none-any.whl

---
+++ packages/syngular/syngular-0.2.3-py3-none-any.whl/syngular/__init__.py
@@ -0,0 +1,9 @@
+TIMEOUT = 60  # seconds  # noqa
+DEGBOUND = 0  # 0 = no-bound  # noqa
+
+from .ideal import Ideal  # noqa
+from .ring import Ring  # noqa
+from .qring import QuotientRing, QRing  # noqa
+from .tools import SingularException  # noqa
+from .field import Field  # noqa
+from .polynomial import Polynomial, Monomial  # noqa
