2a5c8b704fbd18d67beb5e6d25bf81fb143d4998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 06:05:26 +0000

    Add package meta-edc/0.3.22/meta_edc-0.3.22.tar.gz

---
+++ packages/meta-edc/meta_edc-0.3.22.tar.gz/meta_edc-0.3.22/meta_reports/admin/__init__.py
@@ -0,0 +1,8 @@
+from .endpoints_admin import EndpointAdmin
+from .unmanaged import (
+    MissingScreeningOgttAdmin,
+    PatientHistoryMissingBaselineCd4Admin,
+    UnattendedThreeInRow2Admin,
+    UnattendedThreeInRowAdmin,
+    UnattendedTwoInRowAdmin,
+)
---
+++ packages/meta-edc/meta_edc-0.3.22.tar.gz/meta_edc-0.3.22/meta_reports/models/__init__.py
@@ -0,0 +1,8 @@
+from .dbviews import (
+    MissingScreeningOgtt,
+    PatientHistoryMissingBaselineCd4,
+    UnattendedThreeInRow,
+    UnattendedThreeInRow2,
+    UnattendedTwoInRow,
+)
+from .endpoints import Endpoints
