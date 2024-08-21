b0a2b3ab76afd72d25fe0747485b6234ddb95d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:34 +0000

    Add package block-recurrent-transformer-pytorch/0.4.4/block_recurrent_transformer_pytorch-0.4.4.tar.gz

---
+++ packages/block-recurrent-transformer-pytorch/block_recurrent_transformer_pytorch-0.4.4.tar.gz/block_recurrent_transformer_pytorch-0.4.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'block-recurrent-transformer-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.4.4',
+  license='MIT',
+  description = 'Block Recurrent Transformer - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/block-recurrent-transformer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'recurrence'
+  ],
+  install_requires=[
+    'beartype',
+    'einops>=0.6.1',
+    'memorizing-transformers-pytorch>=0.4.0',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
