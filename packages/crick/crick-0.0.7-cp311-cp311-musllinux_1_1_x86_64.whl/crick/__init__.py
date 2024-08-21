6ba6bd87647b998cbff7f17eaf006a2e3d685722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:19 +0000

    Add package crick/0.0.7/crick-0.0.7-cp311-cp311-musllinux_1_1_x86_64.whl

---
+++ packages/crick/crick-0.0.7-cp311-cp311-musllinux_1_1_x86_64.whl/crick/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+from ._version import get_versions
+from .space_saving import SpaceSaving
+from .stats import SummaryStats
+from .tdigest import TDigest
+
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
