51d33aa08534105fcac276d994545b6bcf888948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 06:05:16 +0000

    Add package simpleaitranslator/2.2.4/simpleaitranslator-2.2.4.tar.gz

---
+++ packages/simpleaitranslator/simpleaitranslator-2.2.4.tar.gz/simpleaitranslator-2.2.4/simpleaitranslator/utils/chunk_translator.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+from simpleaitranslator.exceptions import MissingAPIKeyError
+from simpleaitranslator.utils.enums import ModelForTranslator
+
+from openai import AsyncAzureOpenAI
+from openai import AsyncOpenAI
+
+
+
+
+
