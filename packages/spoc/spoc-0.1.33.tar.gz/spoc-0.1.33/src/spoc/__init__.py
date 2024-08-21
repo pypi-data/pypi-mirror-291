74b70cd32234af803ca9a67fff75f70b4c68b6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:30 +0000

    Add package spoc/0.1.33/spoc-0.1.33.tar.gz

---
+++ packages/spoc/spoc-0.1.33.tar.gz/spoc-0.1.33/src/spoc/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Elastic Framework Builder
+"""
+
+from .installer import start_project
+from .workers import BaseProcess, BaseServer, BaseThread
+
+try:
+    # Core Tools
+    from .components import component, is_component
+
+    # Frame Work
+    from .framework import Spoc as App
+    from .importer import frozendict
+    from .importer.base import search_method as search
+    from .importer.tools import get_fields
+    from .singleton import singleton
+
+    # Globals
+    base_dir = App.base_dir
+    config = App.config
+    mode = App.mode
+    env = App.environment
+    settings = App.settings
+except ImportError:
+    pass
+
+__all__ = (
+    # Workers
+    "BaseProcess",
+    "BaseThread",
+    "BaseServer",
+    # Tools
+    "start_project",
+    "component",
+    "frozendict",
+    "get_fields",
+    "is_component",
+    "search",
+    "singleton",
+    # Globals
+    "base_dir",
+    "config",
+    "mode",
+    "settings",
+)
