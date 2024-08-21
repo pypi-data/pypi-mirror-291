1cbb3d6f3c9b7f8676ea43cda1422f24f9077d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:22 +0000

    Add package biomeanalyzer/0.0.32/biomeanalyzer-0.0.32.tar.gz

---
+++ packages/biomeanalyzer/biomeanalyzer-0.0.32.tar.gz/biomeanalyzer-0.0.32/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='biomeanalyzer',
+    version='0.0.32',
+    python_requires='>=3.9',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    package_data={'biomeanalyzer': ['data/rrnDB-5.8_pantaxa_stats_NCBI.tsv']},
+    zip_safe=False,
+    install_requires=['numpy', 'pandas', 'scipy'],
+    author='',
+    author_email='',
+    description='BiomeAnalyzer is a Python package for analyzing microbiome data.',
+)
