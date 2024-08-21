d1e515da38a26b4a5961befa3cf9b940ebec7d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:07 +0000

    Add package msfabricpysdkcore/0.1.6/msfabricpysdkcore-0.1.6.tar.gz

---
+++ packages/msfabricpysdkcore/msfabricpysdkcore-0.1.6.tar.gz/msfabricpysdkcore-0.1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msfabricpysdkcore',
+    version='1.6',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.30.0',
+        'azure-identity>=1.15.0',
+    ]
+)
