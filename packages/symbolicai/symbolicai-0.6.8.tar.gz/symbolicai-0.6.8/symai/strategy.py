f190d8b5af2d241524a6027cd8bc3d23d5f79924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:43 +0000

    Add package symbolicai/0.6.8/symbolicai-0.6.8.tar.gz

---
+++ packages/symbolicai/symbolicai-0.6.8.tar.gz/symbolicai-0.6.8/symai/strategy.py
@@ -0,0 +1,25 @@
+import logging
+from pydoc import locate
+
+from .symbol import Expression
+
+
+class Strategy(Expression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(__name__)
+
+    def __new__(self, module: str, *args, **kwargs):
+        module = module.lower()
+        module = module.replace('-', '_')
+        self._module = module
+        self.module_path = f'symai.extended.strategies.{module}'
+        return Strategy.load_module_class(self.module_path, self._module)(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    @staticmethod
+    def load_module_class(module_path, class_name):
+        module_ = locate(module_path)
+        return getattr(module_, class_name)
