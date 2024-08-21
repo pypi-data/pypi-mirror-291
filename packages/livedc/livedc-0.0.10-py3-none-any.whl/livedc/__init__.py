b6fc5ae474bd127e6acc716c830a5bd336f6e965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:05:09 +0000

    Add package livedc/0.0.10/livedc-0.0.10-py3-none-any.whl

---
+++ packages/livedc/livedc-0.0.10-py3-none-any.whl/livedc/__init__.py
@@ -0,0 +1,18 @@
+# from .desc import add_rdoc
+import os
+
+prefix = os.path.dirname(__file__)
+url_vrpkl = os.path.join( prefix,  'vr/tdm23.vr.pkl') 
+url_vrlib   = os.path.join( prefix,  'vr/tdm23.vr') 
+"""
+import pickle
+with open(url_vrpkl, "rb") as f:
+    prereq = pickle.load(f)
+tdmvr, installed_packages = prereq.env(furl=url_vrlib)
+"""
+
+"""
+python -c "import livedc"
+"""
+
+__version__ = "0.0.10"
