0c99f878ad7a70c75b2b363cf1691b6f33e0ee13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:04:35 +0000

    Add package inspirational-quotes/0.0.11.dev1/inspirational_quotes-0.0.11.dev1.tar.gz

---
+++ packages/inspirational-quotes/inspirational_quotes-0.0.11.dev1.tar.gz/inspirational_quotes-0.0.11.dev1/inspirational_quotes/inspirational_quotes.py
@@ -0,0 +1,12 @@
+import json,random
+from os import path
+
+def quote():
+    _path = path.dirname(__file__)
+    _path = path.join(_path,"quotes/quotes.json")
+    _quotes = json.loads(open(_path, 'rb').read())
+    _quote = _quotes[random.randint(0,len(_quotes)-1)]
+    _res = {'quote':_quote['text'],"author":_quote['from']}
+    return _res
+
+
