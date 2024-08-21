8be33850933a8e654daad0a8cae6ed30a08f1ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:04:31 +0000

    Add package units-pani/0.4/units_pani-0.4.tar.gz

---
+++ packages/units-pani/units_pani-0.4.tar.gz/units_pani-0.4/README.md
@@ -0,0 +1,9 @@
+Use:
+
+Value * Unit = Value in Base unit
+Value / Unit = Value in specified unit
+
+Examples:
+
+val = 5*ms  | val = 0.005
+5*inch      | val = 0.0245...cm
---
+++ packages/units-pani/units_pani-0.4.tar.gz/units_pani-0.4/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'units_pani',         # How you named your package folder (MyLib)
+  packages = ['units_pani'],   # Chose the same as "name"
+  version = '0.4',      # Start with a small number and increase it with every change you make
+  license='GNU GENERAL PUBLIC LICENSE',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'units',   # Give a short description about your library
+  author = 'Dominik Panstingl',                   # Type in your name
+  author_email = 'dominik.panstingl@gmx.at',      # Type in your E-Mail
+  url = 'https://github.com/pani0815/units',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/pani0815/units/archive/refs/tags/0.4.tar.gz',    # I explain this later on
+  keywords = ['units', 'physics'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+
+       # Again, pick a license
+
+    'Programming Language :: Python :: 3.11',
+  ],
+)
