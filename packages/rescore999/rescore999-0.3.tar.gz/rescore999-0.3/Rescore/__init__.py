3c28b26bb32e657eba128cd679b417e070d46286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:04:43 +0000

    Add package rescore999/0.3/rescore999-0.3.tar.gz

---
+++ packages/rescore999/rescore999-0.3.tar.gz/rescore999-0.3/Rescore/__init__.py
@@ -0,0 +1,5 @@
+# rescore999/__init__.py
+
+from .logger import Logger, Color
+
+__all__ = ["Logger", "Color"]
---
+++ packages/rescore999/rescore999-0.3.tar.gz/rescore999-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rescore999",
+    version="0.3",
+    packages=find_packages(),
+    description="A custom logger library with easy color options",
+    author="Rescore",
+    author_email="admin@rescore.lol",
+    url="https://github.com/rescore9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
