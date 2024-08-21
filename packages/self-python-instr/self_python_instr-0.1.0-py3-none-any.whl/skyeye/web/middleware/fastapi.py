a50331c885602bf883cc17e1b6676d13ea1bc7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:20 +0000

    Add package self-python-instr/0.1.0/self_python_instr-0.1.0-py3-none-any.whl

---
+++ packages/self-python-instr/self_python_instr-0.1.0-py3-none-any.whl/skyeye/web/middleware/fastapi.py
@@ -0,0 +1,20 @@
+# fastapi.py
+from fastapi import FastAPI, Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+import time
+from .otel import report_red
+
+
+class APMReporter(BaseHTTPMiddleware):
+
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.perf_counter()
+        response = await call_next(request)
+        process_time = (time.perf_counter() - start_time)*1000
+
+        if isinstance(response.status_code, int):
+            await report_red(request.url.path, response.status_code, process_time)
+        else:
+            await report_red(request.url.path, 0, process_time)
+
+        return response
