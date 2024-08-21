90be0878e40201bc39d90044c51578f3431e2e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:04:41 +0000

    Add package movoid-robotframework/1.5.1/movoid_robotframework-1.5.1-py3-none-any.whl

---
+++ packages/movoid-robotframework/movoid_robotframework-1.5.1-py3-none-any.whl/RobotFrameworkBasic/action/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/2/21 1:29
+# Description   : 
+"""
+from .calculate import BasicCalculate
+from .config import BasicConfig
