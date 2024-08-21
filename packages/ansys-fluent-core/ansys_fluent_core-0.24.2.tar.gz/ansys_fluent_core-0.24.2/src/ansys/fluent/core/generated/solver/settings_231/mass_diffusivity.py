600ddd5de4ae7f52acb7591f79e5c6b2b67a7f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:22 +0000

    Add package ansys-fluent-core/0.24.2/ansys_fluent_core-0.24.2.tar.gz

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.24.2.tar.gz/ansys_fluent_core-0.24.2/src/ansys/fluent/core/generated/solver/settings_231/mass_diffusivity.py
@@ -0,0 +1,25 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import (
+    _ChildNamedObjectAccessorMixin,
+    CreatableNamedObjectMixin,
+    _NonCreatableNamedObjectMixin,
+    AllowedValuesMixin,
+    _InputFile,
+    _OutputFile,
+    _InOutFile,
+)
+
+
+class mass_diffusivity(Real):
+    """
+    'mass_diffusivity' child.
+    """
+
+    fluent_name = "mass-diffusivity"
+
+    return_type = "<object object at 0x7f10c88ed6b0>"
