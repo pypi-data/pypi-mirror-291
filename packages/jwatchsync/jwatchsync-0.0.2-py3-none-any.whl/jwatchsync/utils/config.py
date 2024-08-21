563de88f87a7a0b830b50a2d32707fd5cd79793f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:21 +0000

    Add package jwatchsync/0.0.2/jwatchsync-0.0.2-py3-none-any.whl

---
+++ packages/jwatchsync/jwatchsync-0.0.2-py3-none-any.whl/jwatchsync/utils/config.py
@@ -0,0 +1,4 @@
+JWATCH_URL = {
+    'DEV': 'http://localhost:3001/api/v1/track/', # 'https://wa696floa4.execute-api.us-east-1.amazonaws.com/api/v1/track/', # 'http://localhost:3001/api/v1/track/'
+    'PROD': 'https://wa696floa4.execute-api.us-east-1.amazonaws.com/api/v1/track/'
+}
