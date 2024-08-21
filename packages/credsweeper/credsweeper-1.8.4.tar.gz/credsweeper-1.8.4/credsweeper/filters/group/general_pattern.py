11c8a05b8004479f4575244357848fc4e6d414bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:05:23 +0000

    Add package credsweeper/1.8.4/credsweeper-1.8.4.tar.gz

---
+++ packages/credsweeper/credsweeper-1.8.4.tar.gz/credsweeper-1.8.4/credsweeper/filters/group/general_pattern.py
@@ -0,0 +1,12 @@
+from credsweeper.common.constants import GroupType
+from credsweeper.config import Config
+from credsweeper.filters import ValueUselessWordCheck
+from credsweeper.filters.group import Group
+
+
+class GeneralPattern(Group):
+    """GeneralPattern"""
+
+    def __init__(self, config: Config) -> None:
+        super().__init__(config, GroupType.PATTERN)
+        self.filters.extend([ValueUselessWordCheck()])
