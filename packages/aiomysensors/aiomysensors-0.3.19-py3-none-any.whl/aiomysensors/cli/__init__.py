2306447f8c876249e0242836d9b88ec26666a987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 06:05:22 +0000

    Add package aiomysensors/0.3.19/aiomysensors-0.3.19-py3-none-any.whl

---
+++ packages/aiomysensors/aiomysensors-0.3.19-py3-none-any.whl/aiomysensors/cli/__init__.py
@@ -0,0 +1,31 @@
+"""Provide a CLI for aiomysensors."""
+
+import logging
+
+import click
+
+from aiomysensors import __version__
+
+from .gateway_mqtt import mqtt_gateway
+from .gateway_serial import serial_gateway
+from .gateway_tcp import tcp_gateway
+
+SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+@click.group(
+    options_metavar="", subcommand_metavar="<command>", context_settings=SETTINGS
+)
+@click.option("--debug", is_flag=True, help="Start aiomysensors in debug mode.")
+@click.version_option(__version__)
+def cli(debug: bool) -> None:
+    """Run aiomysensors as an app for testing purposes."""
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+cli.add_command(mqtt_gateway)
+cli.add_command(serial_gateway)
+cli.add_command(tcp_gateway)
