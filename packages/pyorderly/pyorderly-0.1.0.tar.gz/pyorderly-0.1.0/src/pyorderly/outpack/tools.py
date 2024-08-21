33724a13ee563d9cab9d915283a8c710ee69a18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:06 +0000

    Add package pyorderly/0.1.0/pyorderly-0.1.0.tar.gz

---
+++ packages/pyorderly/pyorderly-0.1.0.tar.gz/pyorderly-0.1.0/src/pyorderly/outpack/tools.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import List
+
+import pygit2
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class GitInfo:
+    sha: str
+    branch: str
+    url: List[str]
+
+
+def git_info(path):
+    repo = pygit2.discover_repository(path)
+    if not repo:
+        return None
+    repo = pygit2.Repository(repo)
+    sha = str(repo.head.target)
+    branch = repo.head.shorthand
+    url = [x.url for x in repo.remotes]
+    return GitInfo(sha, branch, url)
