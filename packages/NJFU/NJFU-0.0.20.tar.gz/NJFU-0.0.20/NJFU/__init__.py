980bd0e043f52d87059f0c9aff04444034bce87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:01 +0000

    Add package NJFU/0.0.20/NJFU-0.0.20.tar.gz

---
+++ packages/NJFU/NJFU-0.0.20.tar.gz/NJFU-0.0.20/NJFU/__init__.py
@@ -0,0 +1,6 @@
+from .hello import hello
+from .yolo import TXT2XML, XML2TXT, extend, SplitDataset, split_and_copy_dataset
+from .pcd import STD_TXT
+from .visdom_json import visdom_json_to_csv
+
+name = "NJFU"
