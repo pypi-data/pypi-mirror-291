5e1fef510a3665053b69c7b55278ba5811065c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 06:04:57 +0000

    Add package exoatlas/0.4.2/exoatlas-0.4.2.tar.gz

---
+++ packages/exoatlas/exoatlas-0.4.2.tar.gz/exoatlas-0.4.2/exoatlas/populations/__init__.py
@@ -0,0 +1,12 @@
+from .population import (
+    Population,
+    attribute_columns,
+    method_columns,
+)
+from .predefined import PredefinedPopulation
+from .exoplanets import *
+from .solarsystem import *
+
+from .summary import *
+from .column_descriptions import *
+from .collections import *
