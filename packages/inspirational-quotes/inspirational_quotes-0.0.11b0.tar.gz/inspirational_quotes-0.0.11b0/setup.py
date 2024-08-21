88f5e1c07c64cb366bb80c658841fbaa7c558a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:54 +0000

    Add package inspirational-quotes/0.0.11b0/inspirational_quotes-0.0.11b0.tar.gz

---
+++ packages/inspirational-quotes/inspirational_quotes-0.0.11b0.tar.gz/inspirational_quotes-0.0.11b0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="inspirational_quotes",
+    version="0.0.11b",
+    author="saip007",
+    author_email="saip4622@outlook.com",
+    description="Python package that delivers a curated collection of inspiring and motivational quotes to uplift your day. Ideal for developers seeking daily inspiration or anyone looking to add a touch of positivity to their projects. Easily integrate these quotes into your applications, websites, or personal projects to spread motivation and wisdom",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saip007/inspirational_quotes",
+    keywords=["quotes","python","development"],
+    install_requires=['ujson'],
+    platforms = ['any'],
+    package_data={'': ['*.json']},
+    packages=setuptools.find_packages(include=['inspirational_quotes', 'inspirational_quotes.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
