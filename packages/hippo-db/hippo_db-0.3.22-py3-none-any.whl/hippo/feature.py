7d27d91b870758543cd737ed7ba92d717708d3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:04:20 +0000

    Add package hippo-db/0.3.22/hippo_db-0.3.22-py3-none-any.whl

---
+++ packages/hippo-db/hippo_db-0.3.22-py3-none-any.whl/hippo/feature.py
@@ -0,0 +1,39 @@
+
+import mcol
+
+from dataclasses import dataclass
+
+from .target import Target
+
+@dataclass
+class Feature:
+	"""Pharmocophoric feature in a protein"""
+
+	id: int
+	family: Target
+	target: int
+	chain_name: str
+	residue_name: str
+	residue_number: int
+	atom_names: str
+
+	def __str__(self):
+		return f'F{self.id}'
+
+	def __repr__(self):
+		return f'{mcol.bold}{mcol.underline}{self.family} {self.chain_name} {self.residue_name} {self.residue_number} [{self.atom_names}]{mcol.unbold}{mcol.ununderline}'
+
+	@property
+	def chain_res_name_number_str(self):
+		"""Return a string representation of the feature"""
+		return f'{self.chain_name} {self.residue_name} {self.residue_number}'
+
+	@property
+	def res_name_number_str(self):
+		"""Return a string representation of the feature"""
+		return f'{self.residue_name} {self.residue_number}'
+
+	@property
+	def res_name_number_family_str(self):
+		return f'{self.residue_name} {self.residue_number} {self.family}'
+	
