32d44461c3f9eec945dee92f0d6d29cf8e0699bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:05:04 +0000

    Add package nuctools/1.4.3/nuctools-1.4.3.tar.gz

---
+++ packages/nuctools/nuctools-1.4.3.tar.gz/nuctools-1.4.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+
+setup(
+    name='nuctools',
+    version='v1.4.3', # updated in CI/CD
+    description="nuclear data reduction tools",
+    author="Jesse Brown",
+    author_email="brownjm1968@gmail.com",
+    packages=['nuctools'],
+    include_package_data=True,
+    license='BSD 3-clause',
+    url='https://github.com/brownjm1968/nuctools',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    install_requires=['numpy','pandas','scipy','h5py','pyyaml',
+                      'matplotlib','pytest'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
+
