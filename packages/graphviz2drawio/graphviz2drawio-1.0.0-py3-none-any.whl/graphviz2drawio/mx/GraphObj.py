a0b91b39261db88a8f4449c08dfb6ae4a4c6317e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:04:59 +0000

    Add package graphviz2drawio/1.0.0/graphviz2drawio-1.0.0-py3-none-any.whl

---
+++ packages/graphviz2drawio/graphviz2drawio-1.0.0-py3-none-any.whl/graphviz2drawio/mx/GraphObj.py
@@ -0,0 +1,18 @@
+_blacklist_attrs = ["fill"]
+
+
+class GraphObj:
+
+    def __init__(self, sid: str, gid: str) -> None:
+        self.sid = sid
+        self.gid = gid
+
+    def enrich_from_graph(self, attrs: dict | None) -> None:
+        if attrs is None:
+            return
+        for k, v in attrs.items():
+            if v == "" and k in self.__dict__ and self.__dict__[k] is not None:
+                continue
+            if k in _blacklist_attrs:
+                continue
+            self.__setattr__(k, v)
