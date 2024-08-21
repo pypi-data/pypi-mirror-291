76e75107040a70dbba12befca815b6b3e766e103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 07:03:58 +0000

    Add package agda/2.6.4.3.post1/agda-2.6.4.3.post1-cp37-cp37m-win_amd64.whl

---
+++ packages/agda/agda-2.6.4.3.post1-cp37-cp37m-win_amd64.whl/agda/__init__.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from ._binding import (
+    unsafe_hs_agda_version,
+    unsafe_hs_agda_main,
+    unsafe_hs_init,
+    unsafe_hs_exit,
+)
+
+VERSION: str = "2.6.4.3"
+
+
+def version() -> str:
+    try:
+        unsafe_hs_init([])
+        return str(unsafe_hs_agda_version())
+    finally:
+        unsafe_hs_exit()
+
+
+def main(args: List[str]) -> int:
+    try:
+        unsafe_hs_init(args)
+        return unsafe_hs_agda_main()
+    finally:
+        unsafe_hs_exit()
