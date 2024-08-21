47df5a88857139bde1b8492010d33b946d130eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:22 +0000

    Add package microdados/0.0.1/microdados-0.0.1.tar.gz

---
+++ packages/microdados/microdados-0.0.1.tar.gz/microdados-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="microdados",
+    version="0.0.1",
+    author="Cleomar Felipe Rabelo Antoszczyszyn",
+    author_email="cleomarfelipe@gmail.com",
+    description="A Python library designed to simplify the acquisition and analysis of public microdata. This library streamlines the process of downloading, organizing, and preparing microdata, allowing users to focus on insights and analysis rather than data wrangling.",
+    packages=["microdados"],
+    python_requires='>=3.7',
+)
