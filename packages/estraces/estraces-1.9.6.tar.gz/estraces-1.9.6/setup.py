fe6d73810333ac7e7de4443918e4a048467b371c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:10 +0000

    Add package estraces/1.9.6/estraces-1.9.6.tar.gz

---
+++ packages/estraces/estraces-1.9.6.tar.gz/estraces-1.9.6/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import versioneer
+
+from setuptools import setup
+from setuptools.command.test import test
+
+
+class PyTest(test):
+    user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import pytest
+        import shlex
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(cmdclass={"test": PyTest})
+)
