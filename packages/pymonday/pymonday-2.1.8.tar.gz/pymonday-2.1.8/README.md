b0ce0235df36e77c1d2bf374c42bfd1e241fbb00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:07 +0000

    Add package pymonday/2.1.8/pymonday-2.1.8.tar.gz

---
+++ packages/pymonday/pymonday-2.1.8.tar.gz/pymonday-2.1.8/README.md
@@ -0,0 +1,5 @@
+# Pymonday
+
+Project documentation is available on Read The Docs:
+
+https://pymonday.readthedocs.io/en/latest/
---
+++ packages/pymonday/pymonday-2.1.8.tar.gz/pymonday-2.1.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymonday',
+    version='2.1.8',
+    packages=['pymonday'],
+    include_package_data=True,
+    description="PyMonday is a monday.com API Python Client Library, compatible with API version 2023-10 and later.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['httpx', 'python-dotenv', 'PyYAML', 'asyncio']
+)
