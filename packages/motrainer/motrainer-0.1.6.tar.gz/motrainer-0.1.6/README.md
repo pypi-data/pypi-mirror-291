d8c3973ec76c4cec15dcf31ce8e81a00022676f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:05:08 +0000

    Add package motrainer/0.1.6/motrainer-0.1.6.tar.gz

---
+++ packages/motrainer/motrainer-0.1.6.tar.gz/motrainer-0.1.6/README.md
@@ -0,0 +1,38 @@
+# MOTrainer: Measurement Operator Trainer
+
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.7540442.svg)](https://doi.org/10.5281/zenodo.7540442)
+[![PyPI](https://img.shields.io/pypi/v/motrainer.svg?colorB=blue)](https://pypi.python.org/project/motrainer/)
+[![Build](https://github.com/VegeWaterDynamics/motrainer/actions/workflows/build.yml/badge.svg)](https://github.com/VegeWaterDynamics/motrainer/actions/workflows/build.yml)
+[![Ruff](https://github.com/VegeWaterDynamics/motrainer/actions/workflows/lint.yml/badge.svg)](https://github.com/VegeWaterDynamics/motrainer/actions/workflows/lint.yml)
+
+Measurement Operator Trainer is a Python package training measurement operators (MO) for data assimilations purposes. It is specifically designed for the applications where one needs to split large spatio-temporal data into independent partitions, and then train separate ML models for each partition.
+
+Please refer to the [MOtrainer documentation](https://vegewaterdynamics.github.io/motrainer/) for more details.
+
+## Installation
+
+Python version `>=3.10` is required to install MOTrainer.
+
+MOTrainer can be installed from PyPI:
+
+```sh
+pip install motrainer
+```
+
+We suggest using [`mamba`](https://mamba.readthedocs.io/en/latest/installation/mamba-installation.html#fresh-install-recommended) to create an isolated environment for the installation to avoid conflicts.
+
+For more details and trouble shooting of the installation process, please refer to the [installation guide](https://vegewaterdynamics.github.io/motrainer/setup/) for more details.
+
+## Contributing to MOTrainer
+
+We welcome any kind of contribution to our software. Please refer to the [Contributing Guidelines](https://vegewaterdynamics.github.io/motrainer/CONTRIBUTING/) or [Contributing.md](./docs/CONTRIBUTING.md).
+
+## License
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+## Credits
+
+This package was created with [Cookiecutter](https://github.com/audreyr/cookiecutter) and the [NLeSC/python-template](https://github.com/NLeSC/python-template).
---
+++ packages/motrainer/motrainer-0.1.6.tar.gz/motrainer-0.1.6/tests/test_util.py
@@ -0,0 +1,80 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+import pytest
+import tensorflow as tf
+from sklearn.linear_model import LinearRegression
+
+from motrainer import util
+
+
+def init_trainning():
+    x = np.linspace(-0.5, 0.5, 100)
+    y = np.linspace(-0.5, 0.5, 100)
+    datain = np.column_stack((x, x))
+    dataout = np.column_stack((y, y))
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(5, input_shape=(2, )))
+    model.add(tf.keras.layers.Dense(units=5))
+    model.add(tf.keras.layers.Dense(units=2))
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.MeanSquaredError(),
+                  metrics=['mae'])
+    model.fit(x=datain, y=dataout, epochs=20, verbose=0, validation_split=0.2)
+    return model
+
+
+class TestUtil(unittest.TestCase):
+    model = init_trainning()
+
+    def test_performance(self):
+        x = np.linspace(-0.5, 0.5, 1000)
+        y = np.linspace(-0.5, 0.5, 1000)
+        datain = np.column_stack((x, x))
+        data_label = np.column_stack((y, y))
+
+        for method in ['rmse', 'mae', 'pearson', 'spearman']:
+            perf = util.performance(datain, data_label, TestUtil.model, method)
+            self.assertIsNotNone(perf)
+
+    def test_normalize_standard(self):
+        x = np.linspace(-0.5, 0.5, 200) + 1
+        datain = np.column_stack((x, x))
+        data_norm, scaler = util.normalize(datain, 'standard')
+        self.assertAlmostEqual(data_norm.mean(), 0.0, 10)
+        self.assertIsNotNone(scaler)
+
+    def test_normalize_minmax(self):
+        x = np.linspace(-0.5, 0.5, 200) + 1
+        datain = np.column_stack((x, x))
+        data_norm, scaler = util.normalize(datain, 'min_max')
+        self.assertAlmostEqual(data_norm.min(), 0.0, 10)
+        self.assertIsNotNone(scaler)
+
+
+@pytest.fixture
+def sklearn_model():
+    # create a simple sklearn model
+    df = pd.DataFrame({
+        'feature': [1, 2, 3, 4, 5],
+        'target': [10, 20, 30, 40, 50]
+        })
+    model = LinearRegression()
+    model.fit(df[['feature']], df['target'])
+    return model
+
+
+class TestUtilSklearnIO:
+
+    def test_sklearn_io(self, sklearn_model, tmp_path):
+        model_path = tmp_path / 'test.h5'
+        util.sklearn_save(sklearn_model, model_path, meta_data={'test': 1})
+
+        # load the model
+        model, metadata = util.sklearn_load(model_path)
+
+        # check the model
+        assert isinstance(model, LinearRegression)
+        # check the metadata
+        assert metadata['test'] == 1
