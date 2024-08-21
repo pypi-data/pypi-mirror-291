bb7f6dbd85d291bb7570392dcfaeb4d1dd9ef85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:05:08 +0000

    Add package ibm-data-engine/0.4.0rc1/ibm_data_engine-0.4.0rc1-py3-none-any.whl

---
+++ packages/ibm-data-engine/ibm_data_engine-0.4.0rc1-py3-none-any.whl/ibm_data_engine/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Abhay Ratnaparkhi, Michal Siedlaczek
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Implementation of the offline store backed by the IBM Cloud Data Engine."""
+
+from ibm_data_engine.data_engine_offline_store import (
+    DataEngineDataSource,
+    DataEngineOfflineStore,
+    DataEngineOfflineStoreConfig,
+    DataEngineRetrievalJob,
+    DataEngineSchemaError,
+)
