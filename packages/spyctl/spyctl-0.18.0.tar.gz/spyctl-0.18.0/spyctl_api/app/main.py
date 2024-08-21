5804876fea56bf7ba14278fbcf07b43d0edc6118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:04:25 +0000

    Add package spyctl/0.18.0/spyctl-0.18.0.tar.gz

---
+++ packages/spyctl/spyctl-0.18.0.tar.gz/spyctl-0.18.0/spyctl_api/app/main.py
@@ -0,0 +1,44 @@
+import logging
+from contextlib import asynccontextmanager
+
+import spyctl.config.configs as cfg
+from fastapi import FastAPI
+
+from app import config
+from app.api import create, diff, merge, report, validate
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger = logging.getLogger("uvicorn")
+    logger.setLevel(logging.INFO)
+    logger.info("Starting spyctl API server")
+    await config.load_config()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/alive")
+async def alive():
+    return {"message": "Alive"}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Alive2"}
+
+
+app.include_router(create.router)
+app.include_router(diff.router)
+app.include_router(merge.router)
+app.include_router(validate.router)
+app.include_router(report.router)
+cfg.set_api_call()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
