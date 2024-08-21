26a726eb1ddbb1e0ba7f0ff0a223fa0f6116edb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 03:04:26 +0000

    Add package frametree/0.12.6/frametree-0.12.6-py3-none-any.whl

---
+++ packages/frametree/frametree-0.12.6-py3-none-any.whl/frametree/core/cli/tests/test_cli_apply.py
@@ -0,0 +1,47 @@
+from frametree.core.set.base import Dataset
+from fileformats.text import TextFile
+from frametree.core.cli.apply import apply_pipeline
+from frametree.core.utils import show_cli_trace
+
+
+def test_apply_pipeline_cli(saved_dataset: Dataset, concatenate_task, cli_runner):
+    # Get CLI name for dataset (i.e. file system path prepended by 'dirtree//')
+    # Start generating the arguments for the CLI
+    # Add source to loaded dataset
+    duplicates = 5
+    saved_dataset.add_source("file1", TextFile)
+    saved_dataset.add_source("file2", TextFile)
+    saved_dataset.add_sink("concatenated", TextFile)
+    saved_dataset.apply_pipeline(
+        name="a_pipeline",
+        workflow=concatenate_task(name="workflow", duplicates=duplicates),
+        inputs=[("file1", "in_file1"), ("file2", "in_file2")],
+        outputs=[("concatenated", "out_file")],
+    )
+    # Add source column to saved dataset
+    result = cli_runner(
+        apply_pipeline,
+        [
+            saved_dataset.locator,
+            "a_pipeline",
+            "frametree.testing.tasks:" + concatenate_task.__name__,
+            "--source",
+            "file1",
+            "in_file1",
+            "text/text-file",
+            "--source",
+            "file2",
+            "in_file2",
+            "text/text-file",
+            "--sink",
+            "concatenated",
+            "out_file",
+            "text/text-file",
+            "--parameter",
+            "duplicates",
+            str(duplicates),
+        ],
+    )
+    assert result.exit_code == 0, show_cli_trace(result)
+    loaded_dataset = Dataset.load(saved_dataset.locator)
+    assert saved_dataset.pipelines == loaded_dataset.pipelines
