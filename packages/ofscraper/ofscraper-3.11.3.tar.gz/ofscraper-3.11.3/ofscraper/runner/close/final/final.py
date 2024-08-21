786df3060de010ef5dc778f3a2d7603d9e9f6161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 14:04:25 +0000

    Add package ofscraper/3.11.3/ofscraper-3.11.3.tar.gz

---
+++ packages/ofscraper/ofscraper-3.11.3.tar.gz/ofscraper-3.11.3/ofscraper/runner/close/final/final.py
@@ -0,0 +1,21 @@
+r"""
+                                                             
+ _______  _______         _______  _______  _______  _______  _______  _______  _______ 
+(  ___  )(  ____ \       (  ____ \(  ____ \(  ____ )(  ___  )(  ____ )(  ____ \(  ____ )
+| (   ) || (    \/       | (    \/| (    \/| (    )|| (   ) || (    )|| (    \/| (    )|
+| |   | || (__     _____ | (_____ | |      | (____)|| (___) || (____)|| (__    | (____)|
+| |   | ||  __)   (_____)(_____  )| |      |     __)|  ___  ||  _____)|  __)   |     __)
+| |   | || (                   ) || |      | (\ (   | (   ) || (      | (      | (\ (   
+| (___) || )             /\____) || (____/\| ) \ \__| )   ( || )      | (____/\| ) \ \__
+(_______)|/              \_______)(_______/|/   \__/|/     \||/       (_______/|/   \__/
+                                                                                      f
+"""
+
+from ofscraper.runner.close.final.final_log import final_log
+from ofscraper.runner.close.final.final_script import final_script
+
+
+def final(normal_data=None, scrape_paid_data=None, user_first_data=None, userdata=None):
+
+    final_script(userdata or [])
+    final_log(normal_data or [] + scrape_paid_data or [] + user_first_data or [])
