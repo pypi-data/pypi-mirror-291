66c7ca41b51becb15af75519e3fcdc583a17111a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:05:13 +0000

    Add package ckanext-attribution/1.2.11/ckanext_attribution-1.2.11-py3-none-any.whl

---
+++ packages/ckanext-attribution/ckanext_attribution-1.2.11-py3-none-any.whl/ckanext/attribution/model/crud/package_contribution_activity.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-attribution
+# Created by the Natural History Museum in London, UK
+
+from ckanext.attribution.model.package_contribution_activity import (
+    PackageContributionActivity,
+    package_contribution_activity_table,
+)
+from ckan.model import Session
+from ._base import BaseQuery
+
+
+class PackageContributionActivityQuery(BaseQuery):
+    """
+    
+    """
+
+    # model and table (subclasses should override)
+    m = PackageContributionActivity
+    t = package_contribution_activity_table
+
+    @classmethod
+    def read_package(cls, pkg_id):
+        return (
+            Session.query(PackageContributionActivity)
+            .filter(PackageContributionActivity.package_id == pkg_id)
+            .all()
+        )
