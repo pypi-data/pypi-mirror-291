4f85418852438a91053e3e312f465b039ec31670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 16:04:46 +0000

    Add package pyanimeplanet/0.1.1/pyanimeplanet-0.1.1.tar.gz

---
+++ packages/pyanimeplanet/pyanimeplanet-0.1.1.tar.gz/pyanimeplanet-0.1.1/pyanimeplanet/__init__.py
@@ -0,0 +1,10 @@
+from pyanimeplanet import set_username
+from pyanimeplanet import anime_stats
+from pyanimeplanet import watched_list
+from pyanimeplanet import watching_list
+from pyanimeplanet import want_to_watch_list
+from pyanimeplanet import stalled_list
+from pyanimeplanet import dropped_list
+from pyanimeplanet import wont_watch_list
+from pyanimeplanet import full_info
+
---
+++ packages/pyanimeplanet/pyanimeplanet-0.1.1.tar.gz/pyanimeplanet-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+DESCRIPTION = 'A python module to extract information from anime-planet website'
+LONG_DESCRIPTION = 'A package that allows to extract information from anime planet website without logging.'
+
+setup(
+    name="pyanimeplanet",
+    version=VERSION,
+    author="Vishal Rashmika",
+    author_email="<vishal.rashmika.perera@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests==2.32.3', 'bs4'],
+    keywords=['python', 'web-scraping', 'animeplanet', 'anime'],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Unix",
+    ]
+)
