236950bb91f2bce2aa0487b70f222cbe736a510b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:05:30 +0000

    Add package pyinnovativetrend/0.1/pyinnovativetrend-0.1.tar.gz

---
+++ packages/pyinnovativetrend/pyinnovativetrend-0.1.tar.gz/pyinnovativetrend-0.1/pyinnovativetrend/__init__.py
@@ -0,0 +1,4 @@
+from .pyinnovativetrend import *
+from .visualization import *
+__all__ = [ITA_single, ITA_multiple_by_column, ITA_multiple_by_station, ITA_single_vis, ITA_multiple_vis_by_column, ITA_multiple_vis_by_station]
+
