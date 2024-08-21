cc40ed2abb45110630c599bed363c758a248b529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 15:04:19 +0000

    Add package fun-things/0.27.1/fun_things-0.27.1.tar.gz

---
+++ packages/fun-things/fun_things-0.27.1.tar.gz/fun_things-0.27.1/fun_things/pydantic/mongo_annotation_payload.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from .mongo_annotation import MongoAnnotation
+
+
+@dataclass(frozen=True)
+class MongoAnnotationPayload:
+    annotation: MongoAnnotation
+    key: str
+    value: Any
