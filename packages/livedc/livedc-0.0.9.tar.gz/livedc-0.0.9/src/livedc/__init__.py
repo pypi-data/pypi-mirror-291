76866c2b03cb4922b48d944a87290e4191e858d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 05:05:02 +0000

    Add package livedc/0.0.9/livedc-0.0.9.tar.gz

---
+++ packages/livedc/livedc-0.0.9.tar.gz/livedc-0.0.9/src/livedc/__init__.py
@@ -0,0 +1,13 @@
+# from .desc import add_rdoc
+import os
+
+prefix = os.path.dirname(__file__)
+url_vrpkl = os.path.join( prefix,  'vr/tdm23.vr.pkl') 
+url_vrlib   = os.path.join( prefix,  'vr/tdm23.vr') 
+
+
+"""
+python -c "import livedc"
+"""
+
+__version__ = "0.0.9"
