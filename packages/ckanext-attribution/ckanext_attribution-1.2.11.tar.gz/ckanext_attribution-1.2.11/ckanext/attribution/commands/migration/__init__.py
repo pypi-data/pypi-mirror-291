b1d6d52c240d8af9f488d4da2e264af8aa3459c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-21 04:05:13 +0000

    Add package ckanext-attribution/1.2.11/ckanext_attribution-1.2.11.tar.gz

---
+++ packages/ckanext-attribution/ckanext_attribution-1.2.11.tar.gz/ckanext_attribution-1.2.11/ckanext/attribution/commands/migration/__init__.py
@@ -0,0 +1,4 @@
+from .parser import Parser
+from .common import multi_choice, rgx
+from .combiner import Combiner
+from .api_search import APISearch
---
+++ packages/ckanext-attribution/ckanext_attribution-1.2.11.tar.gz/ckanext_attribution-1.2.11/ckanext/attribution/model/crud/agent_affiliation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-attribution
+# Created by the Natural History Museum in London, UK
+
+from ckanext.attribution.model.agent_affiliation import (
+    AgentAffiliation,
+    agent_affiliation_table,
+)
+
+from ._base import BaseQuery
+from sqlalchemy import or_, and_
+
+
+class AgentAffiliationQuery(BaseQuery):
+    """
+    
+    """
+
+    # model and table (subclasses should override)
+    m = AgentAffiliation
+    t = agent_affiliation_table
+
+    @classmethod
+    def read_agent(cls, agent_id, package_id=None):
+        filters = [or_(cls.m.agent_a_id == agent_id, cls.m.agent_b_id == agent_id)]
+        if package_id is not None:
+            filters.append(cls.m.package_id == package_id)
+        return cls.search(and_(*filters))
