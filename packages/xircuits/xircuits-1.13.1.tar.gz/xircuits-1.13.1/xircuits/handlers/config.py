5a1372aee7ddc52794f3daf77d9faeb72858a4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:32 +0000

    Add package xircuits/1.13.1/xircuits-1.13.1.tar.gz

---
+++ packages/xircuits/xircuits-1.13.1.tar.gz/xircuits-1.13.1/xircuits/handlers/config.py
@@ -0,0 +1,67 @@
+import json
+
+import tornado
+from jupyter_server.base.handlers import APIHandler
+import traceback
+import os
+from configparser import ConfigParser
+
+def get_config():
+    config = ConfigParser()
+    config.read([
+        os.path.join(os.path.dirname(__file__), "..", "..", "xai_components", ".xircuits", "config.ini"),
+        os.path.expanduser("~/.xircuits/config.ini"),
+        ".xircuits/config.ini"])
+    return config
+
+class RunConfigRouteHandler(APIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.finish(json.dumps({"data": "This is /config/run endpoint!"}))
+
+    @tornado.web.authenticated
+    def post(self):
+        input_data = self.get_json_body()
+        cfg = get_config()
+        config_request = input_data["config_request"]
+        run_types = []
+        configurations = []
+        err_msg = ""
+
+        try:
+            get_run_types = str(cfg['REMOTE_EXECUTION'][config_request]).split('\n')
+            for id, run_type_name in enumerate(get_run_types):
+                run_configs = str(cfg['RUN_TYPES'][run_type_name]).split('\n')
+                for id, run_config_name in enumerate(run_configs):
+                    run_cfg = cfg[run_config_name]
+                    configurations.append(
+                        {
+                            "id" : id,
+                            "run_type" : run_type_name,
+                            "run_config_name": run_cfg["name"],
+                            "command" : run_cfg["command"],
+                            "msg" : run_cfg["msg"],
+                            "url" : run_cfg["url"]
+                        })
+                run_types.append({"run_type": run_type_name})
+        except Exception:
+            err_msg = traceback.format_exc()
+            pass
+
+        data = {
+            "run_types": run_types,
+            "run_types_config": configurations,
+            "err_msg": err_msg
+            }
+        self.finish(json.dumps(data))
+
+
+class SplitModeConfigHandler(APIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        cfg = get_config()
+        try:
+            split_mode = cfg.get('UI', 'splitMode', fallback='split-bottom')
+            self.finish(json.dumps({"splitMode": split_mode}))
+        except Exception as e:
+            self.finish(json.dumps({"error": str(e)}))
