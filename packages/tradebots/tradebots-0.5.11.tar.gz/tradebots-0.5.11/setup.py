3b32564873832dd3c074bfd048efb05ce96140ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:04:58 +0000

    Add package tradebots/0.5.11/tradebots-0.5.11.tar.gz

---
+++ packages/tradebots/tradebots-0.5.11.tar.gz/tradebots-0.5.11/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tradebots",
+    version="0.5.11",
+    author="Kryzhanovskiy Maxim",
+    author_email="kryzhanovskiymax@mail.ru",
+    description="Package for building tradebots",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/your_package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'typing==3.7.4.3',
+        "requests==2.32.3",
+        "fastapi==0.112.0",
+        "httpx==0.20.0"
+    ]
+)
