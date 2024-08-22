8e522ddba96fa89bd316a02a8120ca6599df4ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:08:01 +0000

    Add package chumpy-fork/0.71/chumpy_fork-0.71.tar.gz

---
+++ packages/chumpy-fork/chumpy_fork-0.71.tar.gz/chumpy_fork-0.71/setup.py
@@ -0,0 +1,61 @@
+"""
+Author(s): Matthew Loper
+
+See LICENCE.txt for licensing and contact information.
+"""
+
+from distutils.core import setup
+
+#try:  # for pip >= 10
+#    from pip._internal.req import parse_requirements
+#except ImportError:  # for pip <= 9.0.3
+#    from pip.req import parse_requirements
+from runpy import run_path
+
+#install_reqs = parse_requirements("requirements.txt", session=False)
+#try:  # for pip < 20.1
+#    install_requires = [str(ir.req) for ir in install_reqs]
+#except AttributeError:  # for pip >= 20.1
+#    install_requires = [str(ir.requirement) for ir in install_reqs]
+
+
+def get_version():
+    namespace = run_path("chumpy_fork/version.py")
+    return namespace["version"]
+
+
+setup(
+    name="chumpy_fork",
+    version=get_version(),
+    packages=["chumpy_fork"],
+    author="Matthew Loper",
+    author_email="matt.loper@gmail.com",
+    url="https://github.com/var/chumpy",
+    description="chumpy fork, with latest python, pip and numpy support",
+    license="MIT",
+    install_requires=[
+        "numpy>=1.8.1",
+        "scipy>=0.13.0",
+        "six>=1.11.0",
+    ],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
