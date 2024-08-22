538a51f9dc3db5b5ec412797bdecaa544605b87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:03 +0000

    Add package process-assistant/0.0.103/process_assistant-0.0.103.tar.gz

---
+++ packages/process-assistant/process_assistant-0.0.103.tar.gz/process_assistant-0.0.103/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open('./README.md', 'r', encoding='utf-8') as fh:
+  readme = fh.read()
+
+setup(
+  name='process_assistant',
+  version='0.0.103',
+  description='',
+  long_description=readme,
+  long_description_content_type='text/markdown',
+  author='Pedro Jesús Pérez Hernández',
+  author_email='pedrojesus.perez@welldex.mx',
+  url='https://gitwell.gwldx.com:2443/python_libraries/envOS.git',
+  install_requires=[],
+  license='MIT',
+  packages=find_packages(),
+  include_package_data=True,
+  entry_points={ 
+        'console_scripts': [
+            'testlib = process_assistant.main:main' 
+        ]
+    }
+)
