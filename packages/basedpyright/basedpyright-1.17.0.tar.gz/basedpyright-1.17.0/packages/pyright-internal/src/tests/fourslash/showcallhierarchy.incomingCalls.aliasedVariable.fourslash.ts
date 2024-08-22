ba66c145e158d5b00227d16d038ac0e00fbedfe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:47 +0000

    Add package basedpyright/1.17.0/basedpyright-1.17.0.tar.gz

---
+++ packages/basedpyright/basedpyright-1.17.0.tar.gz/basedpyright-1.17.0/packages/pyright-internal/src/tests/fourslash/showcallhierarchy.incomingCalls.aliasedVariable.fourslash.ts
@@ -0,0 +1,17 @@
+/// <reference path="fourslash.ts" />
+
+// @filename: declare.py
+//// my_variable = "Hello, world!"
+
+// @filename: consume.py
+//// from my_module import my_variable as /*marker*/greeting
+////
+//// print(greeting)
+
+{
+    helper.verifyShowCallHierarchyGetIncomingCalls({
+        marker: {
+            items: [],
+        },
+    });
+}
