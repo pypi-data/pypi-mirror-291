8fdd1a77049e5118ae0c9c0167ed7fc582ce96ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:33 +0000

    Add package linkahead/0.15.1/linkahead-0.15.1.tar.gz

---
+++ packages/linkahead/linkahead-0.15.1.tar.gz/linkahead-0.15.1/src/linkahead/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM linkahead SETUP.PY
+#
+short_version = '0.15.1'
+version = '0.15.1'
+full_version = '0.15.1'
+git_revision = '86ffb96add53e78c0de8f5024a072d974f958adb'
+release = True
+
+if not release:
+    version = full_version
