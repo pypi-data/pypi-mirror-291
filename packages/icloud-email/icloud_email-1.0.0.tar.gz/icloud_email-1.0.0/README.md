238251b858d93041ddc70613b9af4ab396c8453a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:34 +0000

    Add package icloud-email/1.0.0/icloud_email-1.0.0.tar.gz

---
+++ packages/icloud-email/icloud_email-1.0.0.tar.gz/icloud_email-1.0.0/README.md
@@ -0,0 +1,39 @@
+# icloud-email
+
+This is a Python package that provides the function `send_email`:
+
+    def send_email(service_name, recipient, subject, html, attachments=[]):
+        sender = f"{service_name} <{SENDER_EMAIL_ADDRESS}>"
+
+        msg = MIMEMultipart()
+        msg['From'] = sender
+        msg['To'] = recipient
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = subject
+        msg.attach(MIMEText(html, 'html'))
+        for attachment in attachments:
+            msg.attach(attachment)
+        # Send it off via smtp.mail.me.com
+        with SMTP_SSL("smtp.mail.me.com", 587) as smtp:
+            smtp.login(ICLOUD_USERNAME, ICLOUD_PASSWORD)
+            smtp.sendmail(sender, recipient, msg.as_string())
+
+## Installation
+
+    pip install icloud-email
+
+## Usage
+
+    from email.mime.application import MIMEApplication
+    from icloud_email import send_email
+
+    attachment = MIMEApplication(b"File contents", name="filename.txt")
+    attachment['Content-Disposition'] = 'attachment; filename="filename.txt"'
+
+    send_email(
+        "Example Service",
+        "recipient@example.org",
+        "Re: Example Subject",
+        "<h1>This is an example email</h1>",
+        [attachment],
+    )
---
+++ packages/icloud-email/icloud_email-1.0.0.tar.gz/icloud_email-1.0.0/src/icloud_email/__init__.py
@@ -0,0 +1,38 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from smtplib import SMTP_SSL
+try:
+    from config import ICLOUD_USERNAME
+except:
+    print("Error: sendgrid-email requires ICLOUD_USERNAME to be defined in config.py")
+    exit(1)
+try:
+    from config import ICLOUD_PASSWORD
+except:
+    print("Error: sendgrid-email requires ICLOUD_PASSWORD to be defined in config.py")
+    exit(1)
+try:
+    from config import SENDER_EMAIL_ADDRESS
+except:
+    print("Error: sendgrid-email requires SENDER_EMAIL_ADDRESS to be defined in config.py")
+    exit(1)
+
+#
+# Put an email together in MIMEMultipart and send it off.
+#
+def send_email(service_name, recipient, subject, html, attachments=[]):
+    sender = f"{service_name} <{SENDER_EMAIL_ADDRESS}>"
+
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(html, 'html'))
+    for attachment in attachments:
+        msg.attach(attachment)
+    # Send it off via smtp.mail.me.com
+    with SMTP_SSL("smtp.mail.me.com", 587) as smtp:
+        smtp.login(ICLOUD_USERNAME, ICLOUD_PASSWORD)
+        smtp.sendmail(sender, recipient, msg.as_string())
