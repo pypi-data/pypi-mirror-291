d6d1859515625d15d302b22f5faddd6262b27dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:13 +0000

    Add package prettyid/0.3.0/prettyid-0.3.0.tar.gz

---
+++ packages/prettyid/prettyid-0.3.0.tar.gz/prettyid-0.3.0/pretty_id/ext/sqlalchemy.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pretty_id.type import PrettyId
+
+try:
+    from sqlalchemy.types import BINARY, TypeDecorator
+    from sqlalchemy.engine import Dialect
+except ImportError:
+    raise RuntimeError("SQLAlchemy is not installed")
+
+if TYPE_CHECKING:
+    from sqlalchemy.types import BINARY, TypeDecorator
+    from sqlalchemy.engine import Dialect
+
+
+class PrettyIdBinaryType(TypeDecorator[PrettyId]):
+    impl = BINARY(16)
+    cache_ok = True
+
+    @property
+    def python_type(self) -> type[PrettyId]:
+        return PrettyId
+
+    def __init__(self, type: str) -> None:
+        self._type = type
+
+    def process_bind_param(
+        self,
+        value: str | PrettyId | None,
+        dialect: Dialect,
+    ) -> bytes | None:
+        if value is None:
+            return None
+        if isinstance(value, PrettyId):
+            return value.bytes
+        try:
+            return PrettyId(value).bytes
+        except (TypeError, ValueError):
+            return None
+
+    def process_result_value(
+        self,
+        value: bytes | None,
+        dialect: Dialect,
+    ) -> PrettyId | None:
+        if value is None:
+            return None
+        try:
+            return PrettyId.from_bytes(id=value, type=self._type)
+        except Exception:
+            return None
