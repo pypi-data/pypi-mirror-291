ff19bcc9b883099376132c6c3ab4662013a17e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:11 +0000

    Add package tradebots/0.5.17/tradebots-0.5.17-py3-none-any.whl

---
+++ packages/tradebots/tradebots-0.5.17-py3-none-any.whl/tradebots/__init__.py
@@ -0,0 +1,13 @@
+from .tradebot import TradeBot, DullBot, TradeBotInput, TradeBotOutput
+from .server import TradeBotServer
+from .interface import TradeBotInterface
+
+
+__all__ = [
+    "TradeBot",
+    "DullBot",
+    "TradeBotInput",
+    "TradeBotOutput",
+    "TradeBotServer",
+    "TradeBotInterface"
+]
