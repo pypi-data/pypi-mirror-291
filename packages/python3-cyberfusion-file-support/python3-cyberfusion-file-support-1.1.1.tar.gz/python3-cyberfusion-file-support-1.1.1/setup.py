d76e7a84b947ff27bf424e26a6c0707ecb3823fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:28 +0000

    Add package python3-cyberfusion-file-support/1.1.1/python3-cyberfusion-file-support-1.1.1.tar.gz

---
+++ packages/python3-cyberfusion-file-support/python3-cyberfusion-file-support-1.1.1.tar.gz/python3-cyberfusion-file-support-1.1.1/setup.py
@@ -0,0 +1,21 @@
+"""A setuptools based setup module."""
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python3-cyberfusion-file-support",
+    version="1.1.1",
+    description="Library for idempotent writing to files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Cyberfusion",
+    author_email="support@cyberfusion.io",
+    url="https://vcs.cyberfusion.nl/shared/python3-cyberfusion-file-support",
+    platforms=["linux"],
+    packages=["cyberfusion.FileSupport"],
+    package_dir={"": "src"},
+    data_files=[],
+)
