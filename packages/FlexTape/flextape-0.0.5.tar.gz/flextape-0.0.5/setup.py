f0c94752a7d9e9fc25906154a524bc9a55aa2f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:25 +0000

    Add package FlexTape/0.0.5/flextape-0.0.5.tar.gz

---
+++ packages/FlexTape/flextape-0.0.5.tar.gz/flextape-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+    
+print(find_packages(where="flextape"))
+
+setup(
+    name             ='FlexTape',
+    version          ='0.0.5',
+    description      ='Python library to download multiple images at once with a request limit.',
+    long_description              = open('README.md', 'r').read(),
+    long_description_content_type = 'text/markdown',
+    author           ='Duckling Dean',
+    author_email     ='duckling.dean@proton.me',
+    packages         =find_packages(where="flextape"),
+    install_requires = [
+        "requests>=2.32.2,<3.0.0",
+        "fake-useragent==1.5.1",
+    ]
+)
+
