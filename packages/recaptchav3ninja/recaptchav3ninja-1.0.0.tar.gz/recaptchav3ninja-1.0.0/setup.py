cb55373c96505b1e33d7cc3ca4d6632732e0b786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:08 +0000

    Add package recaptchav3ninja/1.0.0/recaptchav3ninja-1.0.0.tar.gz

---
+++ packages/recaptchav3ninja/recaptchav3ninja-1.0.0.tar.gz/recaptchav3ninja-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='recaptchav3ninja',
+    version='1.0.0',
+    description='A flexible and customizable library for bypassing Google reCAPTCHA v3 challenges, with both synchronous and asynchronous support.',
+    author='70L0-0j0',
+    author_email='70L0-0j0@lamia.xyz',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/70L0-0j0/RecaptchaV3Ninja',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'asyncio',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+)
