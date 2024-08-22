f332c7bdf0cb963f1092020be07e926f779e90e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:04:59 +0000

    Add package odoo14-addon-energy-project/14.0.3.4.3/odoo14-addon-energy_project-14.0.3.4.3.tar.gz

---
+++ packages/odoo14-addon-energy-project/odoo14-addon-energy_project-14.0.3.4.3.tar.gz/odoo14-addon-energy_project-14.0.3.4.3/odoo/addons/energy_project/models/project.py
@@ -0,0 +1,53 @@
+from odoo import _, fields, models
+
+STATE_VALUES = [
+    ("draft", _("Draft")),
+    ("inscription", _("In Inscription")),
+    ("activation", _("In Activation")),
+    ("active", _("Active")),
+]
+
+
+class Project(models.Model):
+    _name = "energy_project.project"
+    _description = "Energy project"
+
+    name = fields.Char(required=True)
+    type = fields.Many2one("energy_project.project_type", required=True, readonly=True)
+    state = fields.Selection(STATE_VALUES, default="draft", required=True)
+    company_id = fields.Many2one(
+        "res.company", default=lambda self: self.env.company, readonly=True
+    )
+    company_name = fields.Char(
+        string="Name of the company", related="company_id.name", store=False
+    )
+
+    inscription_ids = fields.One2many(
+        "energy_project.inscription",
+        "project_id",
+    )
+
+    active = fields.Boolean(default=True)
+    service_contract_ids = fields.One2many(
+        "energy_project.service_contract", "project_id", auto_join=True
+    )
+
+    # address fields
+    street = fields.Char(required=True)
+    street2 = fields.Char()
+    zip = fields.Char(change_default=True, required=True)
+    city = fields.Char(required=True)
+    state_id = fields.Many2one(
+        "res.country.state",
+        string="State",
+        ondelete="restrict",
+        domain="[('country_id', '=?', country_id)]",
+        required=True,
+    )
+    country_id = fields.Many2one(
+        "res.country",
+        string="Country",
+        ondelete="restrict",
+        required=True,
+        default=lambda self: self.env.ref("base.es"),
+    )
---
+++ packages/odoo14-addon-energy-project/odoo14-addon-energy_project-14.0.3.4.3.tar.gz/odoo14-addon-energy_project-14.0.3.4.3/odoo/addons/energy_project/models/provider.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class Provider(models.Model):
+    _name = "energy_project.provider"
+    _description = "Energy Service Provider"
+
+    name = fields.Char()
+    service_available_ids = fields.One2many(
+        "energy_project.service_available", "provider_id"
+    )
+    user_id = fields.Many2one("res.users", required=True)
