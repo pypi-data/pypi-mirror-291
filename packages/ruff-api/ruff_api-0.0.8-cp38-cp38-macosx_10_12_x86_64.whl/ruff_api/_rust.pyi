3ccfca14a9681dce2b0c537a5ad7b37e1751f21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:28 +0000

    Add package ruff-api/0.0.8/ruff_api-0.0.8-cp38-cp38-macosx_10_12_x86_64.whl

---
+++ packages/ruff-api/ruff_api-0.0.8-cp38-cp38-macosx_10_12_x86_64.whl/ruff_api/_rust.pyi
@@ -0,0 +1,24 @@
+from typing import List
+
+class FormatOptions:
+    def __init__(
+        self,
+        target_version: str | None = None,
+        line_width: int | None = None,
+        preview: bool = False,
+    ): ...
+
+def format_string(
+    path: str, source: str, options: FormatOptions | None = None
+) -> str: ...
+
+class SortOptions:
+    def __init__(
+        self,
+        first_party_modules: List[str] | None = None,
+        standard_library_modules: List[str] | None = None,
+    ): ...
+
+def isort_string(
+    path: str, source: str, options: SortOptions | None = None, root: str | None = None
+) -> str: ...
