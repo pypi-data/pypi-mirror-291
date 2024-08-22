4601b7f81ab93f23540efdb8e4213d90a7cb2fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:11 +0000

    Add package shotgun-extensions/1.0.4/shotgun_extensions-1.0.4.tar.gz

---
+++ packages/shotgun-extensions/shotgun_extensions-1.0.4.tar.gz/shotgun_extensions-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as ld:
+    long_description = ld.read()
+
+setup(
+    name='shotgun_extensions',
+    author='Ryan Neldner',
+    author_email='rneldner@gmail.com',
+    description='shotgun_extensions extends the functionality of shotgun_api3 Python package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.0.4',
+    packages=find_packages(),
+    url='https://github.com/mbsurfer/shotgun-extensions',
+    install_requires=[
+        'shotgun_api3',
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7.0",
+)
