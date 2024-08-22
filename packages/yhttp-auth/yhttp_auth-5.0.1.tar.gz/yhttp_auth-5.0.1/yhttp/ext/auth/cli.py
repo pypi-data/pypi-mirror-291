c58e8fc838b21877dda244464aa5404b869d4f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:23 +0000

    Add package yhttp-auth/5.0.1/yhttp_auth-5.0.1.tar.gz

---
+++ packages/yhttp-auth/yhttp_auth-5.0.1.tar.gz/yhttp_auth-5.0.1/yhttp/ext/auth/cli.py
@@ -0,0 +1,37 @@
+from easycli import SubCommand, Argument
+import json
+
+from .authentication import Authenticator
+
+
+class Create(SubCommand):
+    __command__ = 'create'
+    __aliases__ = ['c']
+    __arguments__ = [
+        Argument(
+            'id', help='example: alice'
+        ),
+        Argument(
+            'payload', default='', nargs='?', help='example: {"foo": "bar"}'
+        ),
+        Argument(
+            '--maxage', type=int, help='Token maxage in seconds.'
+        ),
+    ]
+
+    def __call__(self, args):
+        settings = args.application.settings.auth
+        jwt = Authenticator(settings)
+        if args.payload:
+            payload = json.loads(args.payload)
+        else:
+            payload = ''
+
+        print(jwt.dump(args.id, payload, maxage=args.maxage))
+
+
+class AuthenticatorCLI(SubCommand):
+    __command__ = 'auth'
+    __arguments__ = [
+        Create,
+    ]
