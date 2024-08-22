1a73822bf9a28bdc683c42c0b4f7b0cffa15cec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:07:02 +0000

    Add package nervous-sensors/0.0.5/nervous_sensors-0.0.5.tar.gz

---
+++ packages/nervous-sensors/nervous_sensors-0.0.5.tar.gz/nervous_sensors-0.0.5/README.md
@@ -0,0 +1,44 @@
+# Nervous Sensors
+
+This project aims at implementing drivers in python to handle connection to
+Bluetooth Low Energy sensors developed in the frame of ANR project RENFORCE.
+It also implements an interface to PLUME Unity plugin to collect data within a
+virtual environment for synchronous recording as well as real-time feedback to
+the user.
+
+## Installation
+
+> [!NOTE]
+> Requires python version 3.10, 3.11 or 3.12
+> ```bash
+> pip install nervous-sensors
+> ```
+
+## CLI
+
+```text
+Usage: nervous [OPTIONS]
+
+Options:
+  -s, --sensors TEXT      ECGxxx,EDAxxx... : Give the name of the sensors you
+                          want to use. Make sure to put 'ECG' or 'EDA' in
+                          their name so the program will know which type of
+                          sensor you want to use (not case sensitive).
+  -g, --gui               Show real-time data graph.
+  -f, --folder PATH       Save CSV data files in folder.
+  -l, --lsl               Send sensor data on LSL outlets.
+  -p, --parallel INTEGER  Number of parallel connection tentatives authorized.
+  --help                  Show this message and exit.
+```
+
+### Example
+
+```bash
+nervous -g -f data/csv/ -l -s ECG6543,EDA7201
+```
+
+## CPU consumption
+
+- BLE : no significant CPU usage was noticed with Bleak
+- Data storage : `TRESH1` & `TRESH2` values in `reforce_lsl.py` file may be
+adjusted according to your system's performance
---
+++ packages/nervous-sensors/nervous_sensors-0.0.5.tar.gz/nervous_sensors-0.0.5/nervous_sensors/cli_utils.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+
+RED = "\033[31m"
+GREEN = "\033[32m"
+RESET = "\033[0m"
+
+SENSORS_HELP = (
+    '"ECG XXXX","EDA XXXX" : Give the name of the sensors you want to use. '
+    "XXXX should be replaced with the serial number of the sensor."
+)
+GUI_HELP = "Show real-time data graph."
+FOLDER_HELP = "Save CSV data files in folder."
+LSL_HELP = "Send sensor data on LSL outlets."
+PARALLEL_HELP = "Number of parallel connection tentatives authorized."
+
+
+def printf(*arg, **kwarg):
+    timestamp = datetime.now().strftime("%H:%M:%S")
+    print(timestamp, *arg, **kwarg)
+
+
+def print_start_info(info):
+    printf(f"{GREEN}{info}{RESET}")
+
+
+def print_stop_info(info):
+    printf(f"{RED}{info}{RESET}")
+
+
+def print_bold_section(section):
+    printf(f"\n\033[1m{section}\033[22m\n")
+
+
+def print_general_info(info):
+    printf(f"\033[38;5;245m{info}\033[0m")
+
+
+def get_color(i):
+    return [
+        # "\033[34m",  # blue
+        # "\033[35m",  # magenta skip magenta because of PS background color
+        # "\033[36m",  # cyan
+        # "\033[33m",  # yellow
+        "\033[91m",  # light red
+        "\033[92m",  # light green
+        "\033[93m",  # light yellow
+        "\033[94m",  # light blue
+        "\033[95m",  # light magenta
+        "\033[96m",  # light cyan
+        "\033[38;5;208m",  # orange
+        "\033[38;5;214m",  # gold
+        "\033[38;5;172m",  # brown
+        "\033[38;5;105m",  # purple
+        "\033[38;5;130m",  # violet
+    ][i % 15]
