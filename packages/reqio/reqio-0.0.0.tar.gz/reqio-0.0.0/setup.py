221f90bf94edb6726c9bc6cf6654e66f1af4bb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:04:58 +0000

    Add package reqio/0.0.0/reqio-0.0.0.tar.gz

---
+++ packages/reqio/reqio-0.0.0.tar.gz/reqio-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+    
+
+setup(
+    name             ='reqio',
+    version          ='0.0.0',
+    description      ='Initial',
+    author           ='Duckling Dean',
+    author_email     ='duckling.dean@proton.me',
+    project_urls     ={
+        "Source" : "https://github.com/DucklingDean/reqio",
+    },
+)
+
