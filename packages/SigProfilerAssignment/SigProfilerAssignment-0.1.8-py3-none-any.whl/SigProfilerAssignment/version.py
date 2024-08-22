748294267f361f9aef0215f5b561d3c8e2f8a07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:12 +0000

    Add package SigProfilerAssignment/0.1.8/SigProfilerAssignment-0.1.8-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.1.8-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.1.8'
+version = '0.1.8'
+Update = 'v0.1.8: Add Dockerfile and remove uncessary dependencies'
+
+    
+    
