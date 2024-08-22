64cc5e94ba56c3e58d85639dd81e10341d459750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 14:07:37 +0000

    Add package THISDLChatBot/2.0.14/THISDLChatBot-2.0.14-py3-none-any.whl

---
+++ packages/THISDLChatBot/THISDLChatBot-2.0.14-py3-none-any.whl/THISDLChatBot/Logger.py
@@ -0,0 +1,69 @@
+import sys
+import time
+import colorama
+import getpass
+import _io
+
+
+class Logger:
+    """日志类"""
+    def __init__(self, stdout: _io.TextIOWrapper = sys.stdout, stdin: _io.TextIOWrapper = sys.stdin) -> None:
+        """创建日志对象的方法"""
+        self.stdout = stdout
+        self.stdin = stdin
+
+    def _output(self, level: str, out: str):
+        """输出信息的内部方法"""
+        now = time.localtime()
+        for i in out.split('\n'):
+            self.stdout.write(
+                colorama.Fore.RESET + f"{time.strftime('[%Y-%m-%d %H:%M:%S]', now)}"
+                + f"[{level}]" + i + '\n')
+
+    def success(self, out):
+        """输出成功信息的方法"""
+        self._output(f"{colorama.Fore.GREEN}SUCCESS{colorama.Fore.RESET}", str(out))
+
+    def info(self, out):
+        """输出普通信息的方法"""
+        self._output(f"{colorama.Fore.BLUE}INFO{colorama.Fore.RESET}", str(out))
+
+    def warn(self, out):
+        """输出警告信息的方法"""
+        self._output(f"{colorama.Fore.YELLOW}WARN{colorama.Fore.RESET}", str(out))
+
+    def error(self, out):
+        """输出错误信息的方法"""
+        self._output(f"{colorama.Fore.RED}ERROR{colorama.Fore.RESET}", str(out))
+
+    def input(self, out):
+        """普通输入的方法"""
+        out = str(out)
+        now = time.localtime()
+        for i in out.split('\n')[:-1]:
+            self.stdout.write(
+                colorama.Fore.RESET + f"{time.strftime('[%Y-%m-%d %H:%M:%S]', now)}"
+                + f"[{colorama.Fore.MAGENTA}INPUT{colorama.Fore.RESET}]" + i + '\n')
+        stdin = sys.stdin
+        sys.stdin = self.stdin
+        result = input(colorama.Fore.RESET + f"{time.strftime('[%Y-%m-%d %H:%M:%S]', now)}"
+                       + f"[{colorama.Fore.MAGENTA}INPUT{colorama.Fore.RESET}]" + out.split('\n')[-1])
+        sys.stdin = stdin
+        return result
+
+    def password(self, out):
+        """无回响输入的方法"""
+        out = str(out)
+        now = time.localtime()
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = self.stdout, self.stdin
+        for i in out.split('\n')[:-1]:
+            print(
+                colorama.Fore.RESET + f"{time.strftime('[%Y-%m-%d %H:%M:%S]', now)}"
+                + f"[{colorama.Fore.MAGENTA}PASSWD{colorama.Fore.RESET}]" + i)
+        result = getpass.getpass(
+            colorama.Fore.RESET + f"{time.strftime('[%Y-%m-%d %H:%M:%S]', now)}"
+            + f"[{colorama.Fore.MAGENTA}PASSWD{colorama.Fore.RESET}]"
+            + out.split('\n')[-1])
+        sys.stdout, sys.stdin = stdout, stdin
+        return result
