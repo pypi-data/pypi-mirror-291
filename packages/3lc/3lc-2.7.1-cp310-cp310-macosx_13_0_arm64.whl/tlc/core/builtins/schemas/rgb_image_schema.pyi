d5d7de57fed8c2fd2a9a568cc1335b4ce11a6d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:27 +0000

    Add package 3lc/2.7.1/3lc-2.7.1-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/3lc/3lc-2.7.1-cp310-cp310-macosx_13_0_arm64.whl/tlc/core/builtins/schemas/rgb_image_schema.pyi
@@ -0,0 +1,7 @@
+from tlc.core.builtins.constants.number_roles import NUMBER_ROLE_PIXEL_COUNT as NUMBER_ROLE_PIXEL_COUNT, NUMBER_ROLE_RGB_COMPONENT as NUMBER_ROLE_RGB_COMPONENT
+from tlc.core.schema import DimensionNumericValue as DimensionNumericValue, MapElement as MapElement, Schema as Schema, Uint8Value as Uint8Value
+
+def rgb_image_schema(image_width: int = -1, image_height: int = -1) -> Schema:
+    """
+    Returns a standard schema describing an RGB image
+    """
---
+++ packages/3lc/3lc-2.7.1-cp310-cp310-macosx_13_0_arm64.whl/tlcsaas/pyarmor_runtime_005329/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.11 (pro), 005329, 2024-08-21T15:47:47.004756
+from .pyarmor_runtime import __pyarmor__
