3e149d1f9758e6252dcbba97f8950f0b1dcf83c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:04:56 +0000

    Add package Trinsic-Api/0.2.3/Trinsic-Api-0.2.3.tar.gz

---
+++ packages/Trinsic-Api/Trinsic-Api-0.2.3.tar.gz/Trinsic-Api-0.2.3/trinsic-api/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from trinsic-api.api.attachments_api import AttachmentsApi
+from trinsic-api.api.network_api import NetworkApi
+from trinsic-api.api.sessions_api import SessionsApi
+
