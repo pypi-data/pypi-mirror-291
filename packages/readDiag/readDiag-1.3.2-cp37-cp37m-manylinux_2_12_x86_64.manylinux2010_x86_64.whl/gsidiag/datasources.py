f46a23131265a9aefb565c3abeb43dcac5deae6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:01 +0000

    Add package readDiag/1.3.2/readDiag-1.3.2-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/readDiag/readDiag-1.3.2-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/gsidiag/datasources.py
@@ -0,0 +1,63 @@
+import yaml
+from os import path
+
+class dataSourcesInfo:
+    """
+    A class to parse and store information from a YAML file containing observations data.
+
+    Attributes:
+        tab (dict): A dictionary to store the parsed observations data.
+    """
+    def __init__(self):
+        """
+        The constructor for dataSourcesInfo class. Reads from a YAML file and initializes the 'tab' attribute.
+        """
+        yaml_file = path.join(path.dirname(__file__), 'table.yml')
+
+        with open(yaml_file, 'r') as file:
+            self.data = yaml.safe_load(file)
+
+        self.tab = {}
+        for observation in self.data['observations']:
+            kx = observation['kx']
+            self.tab[kx] = {}
+            for detail in observation['details']:
+                var = detail['var']
+                self.tab[kx][var] = detail
+
+
+def getVarInfo(kx, var, feature):
+    """
+    Retrieves information for a specified feature based on the kx and variable.
+
+    Args:
+        kx (int): The kx value to look up.
+        var (str): The variable to look up.
+        feature (str): The specific feature to retrieve information about.
+
+    Returns:
+        The value of the requested feature if found, otherwise None.
+    """
+    dataInfo = dataSourcesInfo()
+
+    # verify if request exists
+    if kx in dataInfo.tab:
+        if var in dataInfo.tab[kx]:
+            if feature in dataInfo.tab[kx][var]:
+                return dataInfo.tab[kx][var][feature]
+            else:
+                print('Invalid Feature request:', feature)
+                print('Try using one of:')
+                for item in dataInfo.tab[kx][var]:
+                    print('\t*', item)
+        else:
+            print('Variable', var, 'doesn\'t exist in kx', kx)
+            print('Try using one of:')
+            for item in dataInfo.tab[kx]:
+                print('\t*', item)
+    else:
+        print('Invalid kx request:', kx)
+        print('Try using one of:')
+        for item in dataInfo.tab:
+            print('\t*', item)
+
