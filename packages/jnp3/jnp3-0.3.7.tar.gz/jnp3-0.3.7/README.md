6d1f71dcf3dcdd79ddf363b021ec69b668fe61e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 15:05:36 +0000

    Add package jnp3/0.3.7/jnp3-0.3.7.tar.gz

---
+++ packages/jnp3/jnp3-0.3.7.tar.gz/jnp3-0.3.7/README.md
@@ -0,0 +1,25 @@
+# jnp3
+
+一个 Python 语言的辅助工具集，用于记录个人感觉比较有用的辅助函数或者类。
+
+> 名称中的 3 是指 python3，同时也是为了避免和已存在的包冲突。
+
+## 安装
+
+```sh
+pip install jnp3
+```
+
+### 安装可选的 pyside6 工具集
+
+```shell
+pip install jnp3[gui]
+```
+
+> 注意，0.3.5 版本不要使用上述命令，有bug，该bug在 0.3.6 修复
+
+如果要使用 *检查更新* 按钮（0.3.5 以上）
+
+```shell
+pip install jnp3[gui-with-update]
+```
---
+++ packages/jnp3/jnp3-0.3.7.tar.gz/jnp3-0.3.7/jnp3/gui/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf8
+try:
+    import PySide6
+except ModuleNotFoundError:
+    raise ImportError("PySide6 is not installed, see README for instructions on how to use gui.")
+
+
+from .misc import (
+    accept_warning, get_icon_from_svg, create_mono_icon,
+    create_round_icon_from_pixmap,
+)
+from .thread import run_some_task
+from .widgets import *
+
+
+__all__ = ["accept_warning", "StyleComboBox", "PushButtonWithItem",
+           "HorizontalLine", "VerticalLine", "Card", "CardsArea",
+           "IconPushButton", "get_icon_from_svg", "DebugOutputButton",
+           "run_some_task", "CheckUpdateButton", "create_mono_icon",
+           "create_round_icon_from_pixmap"]
