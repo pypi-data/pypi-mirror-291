7121a6141ffbbef0b93c7c1dcebe4c1fa34a8084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:29 +0000

    Add package fast-blockcheck/1.0/fast_blockcheck-1.0-py3-none-any.whl

---
+++ packages/fast-blockcheck/fast_blockcheck-1.0-py3-none-any.whl/fbc/targets.py
@@ -0,0 +1,11 @@
+default_urls = [
+    "https://en.wikipedia.org",
+    "https://www.github.com",
+    "https://www.habr.com",
+    "https://www.ya.ru",
+    "https://www.youtube.com",
+    "https://discord.com"
+]
+
+def get_urls():
+    return default_urls
