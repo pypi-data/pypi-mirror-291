973e74756d4b064059a1f39c420d8d056e48c2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:23 +0000

    Add package readDiag/1.3.1/readDiag-1.3.1-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/readDiag/readDiag-1.3.1-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/gsidiag/__init__.py
@@ -0,0 +1,32 @@
+#-----------------------------------------------------------------------------#
+#           Group on Data Assimilation Development - GDAD/CPTEC/INPE          #
+#-----------------------------------------------------------------------------#
+#BOP
+#
+# !SCRIPT:
+#
+# !DESCRIPTION:
+#
+# !CALLING SEQUENCE:
+#
+# !REVISION HISTORY: 
+# 13 Apr 2018 - J. G. de Mattos - Initial Version
+#
+# !REMARKS:
+#
+#EOP
+#-----------------------------------------------------------------------------#
+#BOC
+"""
+This package defines some functions to read and plot gsi diagnostic files.\
+For help please use help() function.
+"""
+from .__main__ import (help,getColor,geoMap,setcolor,read_diag,plot_diag)
+from .datasources import getVarInfo
+
+__name__    = 'readDiag'
+__version__ = '1.3.1'
+
+#EOC
+#-----------------------------------------------------------------------------#
+
