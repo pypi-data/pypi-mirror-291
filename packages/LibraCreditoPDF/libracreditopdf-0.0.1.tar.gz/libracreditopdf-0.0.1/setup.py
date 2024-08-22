0dced41f9a3ea71e5890a585b47de5ab2ed7f771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:25 +0000

    Add package LibraCreditoPDF/0.0.1/libracreditopdf-0.0.1.tar.gz

---
+++ packages/LibraCreditoPDF/libracreditopdf-0.0.1.tar.gz/libracreditopdf-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='LibraCreditoPDF',
+    version='0.0.1',
+    license='MIT License',
+    author='Matheus Nascimento',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='matheuskater02@outlook.com',
+    keywords='Libra crédito',
+    description=u'Leitor de PDF extrato',
+    packages=['leitor_all'],
+    install_requires=['tabula', 'PyPDF2', 'pandas', 'pdfminer', 'pdfminer.six'])
+
+
