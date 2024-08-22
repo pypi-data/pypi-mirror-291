d6654b5eff1d8fd7b132e80eae2d6beee76bc585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:24 +0000

    Add package LaFamilia013/0.0.1/LaFamilia013-0.0.1-py3-none-any.whl

---
+++ packages/LaFamilia013/LaFamilia013-0.0.1-py3-none-any.whl/LaFamilia/teste.py
@@ -0,0 +1,12 @@
+
+class somandoValores:
+
+    def __init__(self):
+        pass
+
+    def somando_valores(self, valor1, valor2):
+
+        resultado = valor1 + valor2
+
+        return resultado
+    
