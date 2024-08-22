cd8c26abc0a07abd788f5be64d118affdd0b150a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:25 +0000

    Add package yhttp-auth/5.0.0/yhttp_auth-5.0.0-py3-none-any.whl

---
+++ packages/yhttp-auth/yhttp_auth-5.0.0-py3-none-any.whl/yhttp/ext/auth/install.py
@@ -0,0 +1,12 @@
+from .authentication import Authenticator
+from .cli import AuthenticatorCLI
+
+
+def install(app):
+    app.cliarguments.append(AuthenticatorCLI)
+    app.settings.merge('auth: {}')
+    app.settings['auth'].merge(Authenticator.default_settings)
+
+    @app.when
+    def ready(app):
+        app.auth = Authenticator(app.settings.auth)
