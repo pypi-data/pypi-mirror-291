3565154f1429e1061fe6dbb9df09d942227bde40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:30 +0000

    Add package gammarers.aws-daily-cloud-watch-logs-archive-stack/2.8.1/gammarers_aws_daily_cloud_watch_logs_archive_stack-2.8.1.tar.gz

---
+++ packages/gammarers.aws-daily-cloud-watch-logs-archive-stack/gammarers_aws_daily_cloud_watch_logs_archive_stack-2.8.1.tar.gz/gammarers_aws_daily_cloud_watch_logs_archive_stack-2.8.1/src/gammarers/aws_daily_cloud_watch_logs_archive_stack/_jsii/__init__.py
@@ -0,0 +1,32 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import gammarers.aws_secure_bucket._jsii
+import gammarers.aws_secure_log_bucket._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@gammarers/aws-daily-cloud-watch-logs-archive-stack",
+    "2.8.1",
+    __name__[0:-6],
+    "aws-daily-cloud-watch-logs-archive-stack@2.8.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
