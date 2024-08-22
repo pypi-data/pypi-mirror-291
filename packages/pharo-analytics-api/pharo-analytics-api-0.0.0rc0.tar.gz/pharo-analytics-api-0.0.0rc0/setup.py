4f15f6d24eefea823c7e3400414a5cd18a4b7876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:01 +0000

    Add package pharo-analytics-api/0.0.0rc0/pharo-analytics-api-0.0.0rc0.tar.gz

---
+++ packages/pharo-analytics-api/pharo-analytics-api-0.0.0rc0.tar.gz/pharo-analytics-api-0.0.0rc0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name='pharo-analytics-api',
+    version='0.0.0rc0',
+    description='Pharo Package',
+    author='pharo-itops',
+    packages=['pharo-analytics-api'],
+)
