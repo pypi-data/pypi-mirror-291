d92e01257041ba586ab8266e1c97d7154d6acd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:28 +0000

    Add package python-isodata/0.0.15/python-isodata-0.0.15.tar.gz

---
+++ packages/python-isodata/python-isodata-0.0.15.tar.gz/python-isodata-0.0.15/src/isodata/connector.py
@@ -0,0 +1,22 @@
+from loguru import logger
+
+
+class Connector:
+    token = None
+    required = []
+
+    def authorize(self, **kwargs):
+        """Set all dynamic attributes."""
+
+        for r in self.required:
+            try:
+                setattr(self, r, kwargs[r])
+            except IndexError:
+                logger.error("Cannot authorize: Missing '[%s]' credential" % r)
+
+        try:
+            self.token = self.get_token()
+            logger.debug('Received a token of length %s characters.' % len(self.token))
+        except AttributeError as e:
+            logger.error(e)
+            self.token = None
