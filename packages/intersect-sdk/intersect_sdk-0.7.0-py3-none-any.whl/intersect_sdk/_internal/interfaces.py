152a7bcb79df9a33a6c6352a12b0abece71a1221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:01 +0000

    Add package intersect-sdk/0.7.0/intersect_sdk-0.7.0-py3-none-any.whl

---
+++ packages/intersect-sdk/intersect_sdk-0.7.0-py3-none-any.whl/intersect_sdk/_internal/interfaces.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from uuid import UUID
+
+    from ..service_callback_definitions import (
+        INTERSECT_SERVICE_RESPONSE_CALLBACK_TYPE,
+    )
+    from ..shared_callback_definitions import (
+        IntersectDirectMessageParams,
+    )
+
+
+class IntersectEventObserver(ABC):
+    """Abstract definition of an entity which observes an INTERSECT event (i.e. IntersectService).
+
+    Used as the common interface for event emitters (i.e. CapabilityImplementations).
+    """
+
+    @abstractmethod
+    def _on_observe_event(self, event_name: str, event_value: Any, operation: str) -> None:
+        """How to react to an event being fired.
+
+        Args:
+            event_name: The key of the event which is fired.
+            event_value: The value of the event which is fired.
+            operation: The source of the event (generally the function name, not directly invoked by application devs)
+        """
+        ...
+
+    @abstractmethod
+    def create_external_request(
+        self,
+        request: IntersectDirectMessageParams,
+        response_handler: INTERSECT_SERVICE_RESPONSE_CALLBACK_TYPE | None = None,
+    ) -> UUID:
+        """Observed entity (capabilitiy) tells observer (i.e. service) to send an external request.
+
+        Params:
+          - request: the request we want to send out, encapsulated as an IntersectClientMessageParams object
+          - response_handler: optional callback for how we want to handle the response from this request.
+
+        Returns:
+          - generated RequestID associated with your request
+        """
+        ...
---
+++ packages/intersect-sdk/intersect_sdk-0.7.0-py3-none-any.whl/intersect_sdk/schema.py
@@ -0,0 +1,78 @@
+"""This module is responsible for generating the core interface definitions of any Service.
+
+There are two things to generate:
+
+###
+Schema
+###
+
+JSON schema advertises the interfaces to other systems. This is necessary for creating scientific campaigns, and the schema
+is extensible to other clients.
+
+Parts of the schema will be generated from users' own definitions. Functions are represented under "channels",
+while Pydantic models defined by users and used as request or response types in their functions will have their schemas generated here.
+
+There are also several parameters mainly for use by the central INTERSECT microservices, largely encapsulated from users.
+
+###
+Code Interface
+###
+
+When external systems send a message to this system, there are two important things in determining what executes: an operation id string,
+and a payload which can be serialized into an auto-validated object. The operation id string is what can be mapped into various function definitions.
+
+Users are able to define their response and request content-types, as well as their response data platform, for each exposed function (on the annotation).
+
+The SDK needs to be able to dynamically look up functions, validate the request structure and request parameters, and handle responses.
+"""
+
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+)
+
+from ._internal.schema import get_schema_and_functions_from_capability_implementations
+
+if TYPE_CHECKING:
+    from .capability.base import IntersectBaseCapabilityImplementation
+    from .config.shared import HierarchyConfig
+
+
+def get_schema_from_capability_implementation(
+    capability_type: type[IntersectBaseCapabilityImplementation],
+    service_name: HierarchyConfig,
+) -> dict[str, Any]:
+    """The goal of this function is to be able to generate a complete schema matching the AsyncAPI spec 2.6.0 from a BaseModel class.
+
+    Params:
+      - capability_type - the SDK user will provide the class of their capability handler, which generates the schema
+      - service_name - ideally, this could be scanned by the package name. Meant to be descriptive, i.e. "nionswift"
+
+
+    SOME NOTES ABOUT THE SCHEMA
+
+    - Inspired on AsyncAPI 2.6.0 (https://www.asyncapi.com/docs/reference/specification/v2.6.0) and OpenAPI 3.1.0 (https://swagger.io/specification/).
+      Currently, the generated object tries to follow only AsyncAPI closely, though ideas from OpenAPI are used.
+
+    - While all _required_ fields from the AsyncAPI spec are included, not all information is exposed.
+
+    - There is currently no need to store information about the "servers" object, as this will be handled
+      entirely via the SDK and its services. In the instance that multiple brokers are used,
+      this field could potentially be useful to store information about the broker.
+
+    - Not listing security mechanisms, as this gets handled by the SDK and its services.
+
+    - No need to describe operation bindings for now. NOTE: this might be useful later on
+      if we need to handle various protocols (i.e. http, mqtt, ws, nats, pulsar...)
+
+    - Channel names just mimic the function names for now
+    """
+    cap_instance = capability_type()
+    schemas, _, _, _, _, _ = get_schema_and_functions_from_capability_implementations(
+        [cap_instance],
+        service_name,
+        set(),  # assume all data handlers are configured if user is just checking their schema
+    )
+    return schemas
