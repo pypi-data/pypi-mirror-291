830793320d2e2ad76d88b969431aa38726e75b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 15:05:30 +0000

    Add package sl-ai-models/0.0.1/sl_ai_models-0.0.1-py3-none-any.whl

---
+++ packages/sl-ai-models/sl_ai_models-0.0.1-py3-none-any.whl/ai_models/__init__.py
@@ -0,0 +1,11 @@
+from ai_models.gpt4ovision import Gpt4oVision
+from ai_models.gpt4o import Gpt4o
+from ai_models.perplexity import Perplexity
+from ai_models.claude35sonnet import Claude35Sonnet
+from ai_models.exa_searcher import ExaSearcher
+from ai_models.gpt4omini import Gpt4oMini
+from ai_models.deepgram_nova2 import DeepgramNova2
+
+
+from ai_models.resource_managers.monetary_cost_manager import MonetaryCostManager
+from ai_models.utils.ai_misc import clean_indents
