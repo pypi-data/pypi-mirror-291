6ad8dadfe5df1dc2ad7e356d52a5e0c2529eb254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:19 +0000

    Add package indexify/0.0.40/indexify-0.0.40.tar.gz

---
+++ packages/indexify/indexify-0.0.40.tar.gz/indexify-0.0.40/indexify/extractor_sdk/__init__.py
@@ -0,0 +1,14 @@
+from .data import ContentMetadata, Content, Feature
+from .extractor import Extractor, extractor, EmbeddingSchema, ExtractorMetadata
+from .utils import SampleExtractorData
+
+__all__ = [
+    "ContentMetadata",
+    "Content",
+    "Feature",
+    "Extractor",
+    "extractor",
+    "EmbeddingSchema",
+    "ExtractorMetadata",
+    "SampleExtractorData",
+]
