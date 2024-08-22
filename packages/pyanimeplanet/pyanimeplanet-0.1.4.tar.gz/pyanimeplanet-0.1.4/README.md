1f448562bbac823baeac8dd47c9800f3ff50144b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:28 +0000

    Add package pyanimeplanet/0.1.4/pyanimeplanet-0.1.4.tar.gz

---
+++ packages/pyanimeplanet/pyanimeplanet-0.1.4.tar.gz/pyanimeplanet-0.1.4/README.md
@@ -0,0 +1,2 @@
+# pyanimeplanet
+A python module to extract information from anime-planet website (no login required)
---
+++ packages/pyanimeplanet/pyanimeplanet-0.1.4.tar.gz/pyanimeplanet-0.1.4/pyanimeplanet/__init__.py
@@ -0,0 +1,10 @@
+from pyanimeplanet.pyanimeplanet import set_username
+from pyanimeplanet.pyanimeplanet import anime_stats
+from pyanimeplanet.pyanimeplanet import watched_list
+from pyanimeplanet.pyanimeplanet import watching_list
+from pyanimeplanet.pyanimeplanet import want_to_watch_list
+from pyanimeplanet.pyanimeplanet import stalled_list
+from pyanimeplanet.pyanimeplanet import dropped_list
+from pyanimeplanet.pyanimeplanet import wont_watch_list
+from pyanimeplanet.pyanimeplanet import full_info
+
