0c3e86a630bae2933f48ab16774c9edccef3c915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:02 +0000

    Add package bitsightpy/0.1.2/bitsightpy-0.1.2-py3-none-any.whl

---
+++ packages/bitsightpy/bitsightpy-0.1.2-py3-none-any.whl/bitsightpy/__init__.py
@@ -0,0 +1,12 @@
+"""
+Initialization file for the bitsightpy package.
+"""
+
+from .get_endpoints import get_endpoints
+from . import users
+from . import insights
+from . import portfolio
+from . import companies
+from . import folders
+
+__surprise__ = b"\xe2\x9c\xa8\xe2\x9c\xa8\xe2\x9c\xa8 Have a great day!".decode("utf-8")
