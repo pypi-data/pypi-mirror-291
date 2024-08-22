9abed6cc096907403969303f2a397e9fd51b3f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:01 +0000

    Add package pharo-taco-toolbox/0.0.0rc0/pharo-taco-toolbox-0.0.0rc0.tar.gz

---
+++ packages/pharo-taco-toolbox/pharo-taco-toolbox-0.0.0rc0.tar.gz/pharo-taco-toolbox-0.0.0rc0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name='pharo-taco-toolbox',
+    version='0.0.0rc0',
+    description='Pharo Package',
+    author='pharo-itops',
+    packages=['pharo-taco-toolbox'],
+)
