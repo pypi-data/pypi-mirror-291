0dcc0e59bcc10b060005d48ff0700c253e54f346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:06:18 +0000

    Add package gundi-core/1.6.0/gundi_core-1.6.0-py3-none-any.whl

---
+++ packages/gundi-core/gundi_core-1.6.0-py3-none-any.whl/gundi_core/events/dispatchers.py
@@ -0,0 +1,25 @@
+from gundi_core.schemas.v2 import DispatchedObservation, UpdatedObservation, CustomDispatcherLog
+from .core import SystemEventBaseModel
+
+
+class ObservationDelivered(SystemEventBaseModel):
+    payload: DispatchedObservation
+
+
+class ObservationDeliveryFailed(SystemEventBaseModel):
+    payload: DispatchedObservation
+
+
+class ObservationUpdated(SystemEventBaseModel):
+    payload: UpdatedObservation
+
+
+class ObservationUpdateFailed(SystemEventBaseModel):
+    payload: UpdatedObservation
+
+
+class DispatcherCustomLog(SystemEventBaseModel):
+    payload: CustomDispatcherLog
+
+
+
