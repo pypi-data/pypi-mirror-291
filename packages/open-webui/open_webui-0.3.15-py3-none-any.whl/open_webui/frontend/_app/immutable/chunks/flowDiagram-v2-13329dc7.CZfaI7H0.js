4aac4307f1aec125808837abf2bd0ef9ad2e5624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 14:05:27 +0000

    Add package open-webui/0.3.15/open_webui-0.3.15-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.3.15-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/flowDiagram-v2-13329dc7.CZfaI7H0.js
@@ -0,0 +1,2 @@
+import{p as a,f as o}from"./flowDb-c1833063.BAQRZ_RZ.js";import{f as e,a as t}from"./styles-483fbfea.DyengysC.js";import{y as s}from"./Messages.QXyfMqyv.js";import"./graph.CB9isd7R.js";import"./layout.DIRsSntE.js";import"./dayjs.min.CnqrW7zs.js";const b={parser:a,db:o,renderer:e,styles:t,init:r=>{r.flowchart||(r.flowchart={}),r.flowchart.arrowMarkerAbsolute=r.arrowMarkerAbsolute,s({flowchart:{arrowMarkerAbsolute:r.arrowMarkerAbsolute}}),e.setConf(r.flowchart),o.clear(),o.setGen("gen-2")}};export{b as diagram};
+//# sourceMappingURL=flowDiagram-v2-13329dc7.CZfaI7H0.js.map
