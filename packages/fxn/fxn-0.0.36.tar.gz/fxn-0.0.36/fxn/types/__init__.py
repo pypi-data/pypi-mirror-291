984f24b5481183485e1265012fbbea441ea712ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:33 +0000

    Add package fxn/0.0.36/fxn-0.0.36.tar.gz

---
+++ packages/fxn/fxn-0.0.36.tar.gz/fxn-0.0.36/fxn/types/__init__.py
@@ -0,0 +1,12 @@
+# 
+#   Function
+#   Copyright © 2024 NatML Inc. All Rights Reserved.
+#
+
+from .dtype import Dtype
+from .environment import EnvironmentVariable
+from .prediction import Prediction, PredictionResource
+from .predictor import Acceleration, AccessMode, EnumerationMember, Parameter, Predictor, PredictorStatus, Signature
+from .profile import Profile
+from .storage import UploadType
+from .user import User
