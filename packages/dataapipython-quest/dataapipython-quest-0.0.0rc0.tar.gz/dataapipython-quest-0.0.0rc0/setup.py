64ccfefda247104ce4e4d244b499ed53d814f14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:01 +0000

    Add package dataapipython-quest/0.0.0rc0/dataapipython-quest-0.0.0rc0.tar.gz

---
+++ packages/dataapipython-quest/dataapipython-quest-0.0.0rc0.tar.gz/dataapipython-quest-0.0.0rc0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name='dataapipython-quest',
+    version='0.0.0rc0',
+    description='Pharo Package',
+    author='pharo-itops',
+    packages=['dataapipython-quest'],
+)
