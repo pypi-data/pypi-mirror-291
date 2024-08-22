9bae08ef1734e3ee4af7badabbde7cdfb2ded0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:54 +0000

    Add package psshiva-python-functions/0.0.2/psshiva_python_functions-0.0.2.tar.gz

---
+++ packages/psshiva-python-functions/psshiva_python_functions-0.0.2.tar.gz/psshiva_python_functions-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+DESCRIPTION = "PS.Shiva's Python Functions"
+LONG_DESCRIPTION = "Package containing all the latest algorithms and functions by PS.Shiva"
+
+
+setup(
+    name="psshiva_python_functions",
+    version=VERSION,
+    author="Pratik Satpathy",
+    author_email="satpathypratik@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires = [],
+    keywords=['psshiva', 'ps.shiva', 'PS.Shiva', 'PSSHIVA', 'python', 'functions', 'algorithms'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        
+    ]
+)
