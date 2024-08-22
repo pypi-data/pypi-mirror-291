d6d5a5c2f758bd4471eb53ac7210e82f74113d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:13 +0000

    Add package python3-cyberfusion-queue-support/1.1.1.1.3/python3-cyberfusion-queue-support-1.1.1.1.3.tar.gz

---
+++ packages/python3-cyberfusion-queue-support/python3-cyberfusion-queue-support-1.1.1.1.3.tar.gz/python3-cyberfusion-queue-support-1.1.1.1.3/setup.py
@@ -0,0 +1,25 @@
+"""A setuptools based setup module."""
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python3-cyberfusion-queue-support",
+    version="1.1.1.1.3",
+    description="Library to queue actions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Cyberfusion",
+    author_email="support@cyberfusion.io",
+    url="https://github.com/CyberfusionIO/python3-cyberfusion-queue-support",
+    platforms=["linux"],
+    packages=[
+        "cyberfusion.QueueSupport",
+        "cyberfusion.QueueSupport.items",
+        "cyberfusion.QueueSupport.exceptions",
+    ],
+    package_dir={"": "src"},
+    data_files=[],
+)
