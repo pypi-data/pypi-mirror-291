3ad18bd230712734ca67cb1be34daad351387084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:06:49 +0000

    Add package easyi2l/0.0.1/easyi2l-0.0.1.tar.gz

---
+++ packages/easyi2l/easyi2l-0.0.1.tar.gz/easyi2l-0.0.1/easyi2l/config.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+IP2LOCATION_URL = "https://www.ip2location.com/download/?token={TOKEN}&file={DATABASE_CODE}"
+
+# load .env
+load_dotenv()
+
+IP2LOCATION_TOKEN = os.getenv("IP2LOCATION_TOKEN")
+if not IP2LOCATION_TOKEN:
+    raise ValueError("Please provide IP2LOCATION_TOKEN environment variable")
+
+db_folder = Path(__file__).parent / "IP2LOCATION"
+db_folder.mkdir(parents=True, exist_ok=True)
