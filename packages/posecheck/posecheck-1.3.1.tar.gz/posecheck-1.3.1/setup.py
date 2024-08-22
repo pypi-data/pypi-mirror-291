27ac5b5d8d3b5e4a4a4f5f93a3a0385bdef03f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:41 +0000

    Add package posecheck/1.3.1/posecheck-1.3.1.tar.gz

---
+++ packages/posecheck/posecheck-1.3.1.tar.gz/posecheck-1.3.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt and store its contents in a list
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+# with open('README.md', 'r') as f:
+#     long_description = f.read()
+
+long_description = "PoseCheck is a library for benchmarking poses of 3D SBDD models."
+
+setup(
+    name="posecheck",
+    version="1.3.1",
+    description="A library for benchmarking poses of 3D SBDD models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Charles Harris",
+    author_email="cch57@cam.ac.uk",
+    packages=find_packages(include=["posecheck",
+                                    "posecheck.*"]),
+    install_requires=required,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
