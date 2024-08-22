352827d5f4641f70a616ba8c6fccd878c79aecb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:01 +0000

    Add package phylum/0.49.0/phylum-0.49.0-py3-none-any.whl

---
+++ packages/phylum/phylum-0.49.0-py3-none-any.whl/phylum/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for phylum."""
+
+from importlib.metadata import metadata, version
+import logging
+
+PKG_METADATA = metadata(__name__)
+
+__version__ = version(__name__)
+__author__ = PKG_METADATA["Author"]
+__email__ = PKG_METADATA["Author-email"]
+
+PKG_NAME = PKG_METADATA["Name"]
+PKG_SUMMARY = PKG_METADATA["Summary"]
+
+LOG = logging.getLogger(PKG_NAME)
