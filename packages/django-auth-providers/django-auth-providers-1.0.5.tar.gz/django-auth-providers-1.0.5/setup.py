6233049a9caeefe9a7edbec3e9d8e17c912d793e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:07:46 +0000

    Add package django-auth-providers/1.0.5/django-auth-providers-1.0.5.tar.gz

---
+++ packages/django-auth-providers/django-auth-providers-1.0.5.tar.gz/django-auth-providers-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0.5"
+setup(
+    name="django-auth-providers",
+    version=__version__,
+    description=(
+        "django-auth-providers is a django app that lets you"
+        " authenticate to external authentication providers"
+    ),
+    author="Petter Elenius Moe",
+    author_email="pettermoe9530@gmail.com",
+    url="https://github.com/pettermoe95/django-ext-auth/tree/main",
+    packages=['ext_auth'] + ['ext_auth.' + pkg for pkg in find_packages('ext_auth')],
+)
