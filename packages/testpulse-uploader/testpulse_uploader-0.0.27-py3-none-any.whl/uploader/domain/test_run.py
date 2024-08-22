e7f4ea8ca526d076a21e4446d89348f3af27bbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:02 +0000

    Add package testpulse-uploader/0.0.27/testpulse_uploader-0.0.27-py3-none-any.whl

---
+++ packages/testpulse-uploader/testpulse_uploader-0.0.27-py3-none-any.whl/uploader/domain/test_run.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from argparse import Namespace
+
+from uploader.domain.domain import TestRun
+from uploader.domain.ci.github_test_run import GitHubTestRun
+from uploader.domain.ci.jenkins_test_run import JenkinsTestRun
+
+logger = logging.getLogger(__name__)
+
+
+def create_test_run(args: Namespace) -> TestRun:
+    if 'GITHUB_JOB' in os.environ:
+        logger.info("GitHub detected...")
+        return GitHubTestRun(args=args)
+    if 'JENKINS_URL' in os.environ:
+        logger.info("Jenkins detected...")
+        return JenkinsTestRun(args=args)
+
+    raise Exception("We did not detect a supported CI. Please send us a message and "
+                    "we will reply ASAP: contact@testpulse.io")
