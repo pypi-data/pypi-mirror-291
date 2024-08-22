b4b1b53d036a317bb1a7a929cd56970886ebebdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 14:06:35 +0000

    Add package taiao-declare-service-py/0.7.0/taiao_declare_service_py-0.7.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/taiao-declare-service-py/taiao_declare_service_py-0.7.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/taiao_declare_service_py/__init__.py
@@ -0,0 +1,5 @@
+from .taiao_declare_service_py import *
+
+__doc__ = taiao_declare_service_py.__doc__
+if hasattr(taiao_declare_service_py, "__all__"):
+    __all__ = taiao_declare_service_py.__all__
