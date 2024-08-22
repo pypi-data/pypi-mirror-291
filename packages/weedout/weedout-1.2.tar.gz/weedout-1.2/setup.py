bfaef21048628af2e662423af74aa0f42879d517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 14:05:51 +0000

    Add package weedout/1.2/weedout-1.2.tar.gz

---
+++ packages/weedout/weedout-1.2.tar.gz/weedout-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='weedout', 
+    version='1.2', 
+    packages=find_packages(), 
+    install_requires=[
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'scikit-learn',
+        'statsmodels',
+        'imbalanced-learn',
+        'scipy',
+        'typing'
+    ],
+    long_description=description,
+    long_description_content_type ="text/markdown",
+
+)
