5f1e3e8d483a70e2ccf10d62937f8aeeff6445c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:04 +0000

    Add package user-vpetrov/0.1.9/user_vpetrov-0.1.9.tar.gz

---
+++ packages/user-vpetrov/user_vpetrov-0.1.9.tar.gz/user_vpetrov-0.1.9/setup.py
@@ -0,0 +1,20 @@
+"""
+Setup file for user_utils package
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='user_vpetrov',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'pydantic',
+        'sqlalchemy',
+        'alembic',
+        'email-validator',
+        'python-dateutil',
+        'passlib',
+        'rut-chile'
+    ]
+)
