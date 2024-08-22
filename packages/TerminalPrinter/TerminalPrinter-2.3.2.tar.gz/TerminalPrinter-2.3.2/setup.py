611b6518fcf58125707619ccfd63cc865a5fc3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 15:05:54 +0000

    Add package TerminalPrinter/2.3.2/TerminalPrinter-2.3.2.tar.gz

---
+++ packages/TerminalPrinter/TerminalPrinter-2.3.2.tar.gz/TerminalPrinter-2.3.2/setup.py
@@ -0,0 +1,42 @@
+# coding=utf8
+
+from setuptools import setup
+from printer.version import __version__, __author__, __url__, __project__
+
+setup(
+    name=__project__,
+    version=__version__,
+    keywords=['字符画', '终端打印'],
+    description="终端图片、文字生成器",
+    license='MIT',
+    author=__author__,
+    author_email='hellflamedly@gmail.com',
+    url=__url__,
+    packages=["printer"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.10',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        'Topic :: Text Processing :: General',
+        'Topic :: Terminals',
+        'Topic :: Games/Entertainment'
+    ],
+    install_requires=[
+        'Pillow'
+    ],
+    entry_points={
+        'console_scripts': [
+            'terminalprint=printer.run:run'
+        ]
+    }
+)
+
+
