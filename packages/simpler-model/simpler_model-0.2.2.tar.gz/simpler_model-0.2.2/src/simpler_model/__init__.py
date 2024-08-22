1640403ef1126ec8e35f3780511a5b5deb94a61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:16 +0000

    Add package simpler-model/0.2.2/simpler_model-0.2.2.tar.gz

---
+++ packages/simpler-model/simpler_model-0.2.2.tar.gz/simpler_model-0.2.2/src/simpler_model/__init__.py
@@ -0,0 +1,13 @@
+from simpler_model.models.attribute import Attribute
+from simpler_model.models.attribute_modifier import AttributeModifier
+from simpler_model.models.cardinality import Cardinality
+from simpler_model.models.entity import Entity
+from simpler_model.models.entity_modifier import EntityModifier
+from simpler_model.models.format import Format
+from simpler_model.models.model_schema import ModelSchema
+from simpler_model.models.relation import Relation
+from simpler_model.models.relation_modifier import RelationModifier
+
+VERSION = '0.2.0'
+
+__all__ = ['Attribute', 'AttributeModifier', 'Cardinality', 'Entity', 'EntityModifier', 'Format', 'ModelSchema', 'Relation', 'RelationModifier']
