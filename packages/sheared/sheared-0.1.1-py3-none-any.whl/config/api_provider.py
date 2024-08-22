ff0eaa26bd0311f37033547b9e928844f5db7051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:06:36 +0000

    Add package sheared/0.1.1/sheared-0.1.1-py3-none-any.whl

---
+++ packages/sheared/sheared-0.1.1-py3-none-any.whl/config/api_provider.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+from shared.config.settings import logger
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+class SettingsAPIKeyProvider:
+
+    @staticmethod
+    def load_google_api_key() -> str:
+        try:
+            return os.getenv("GOOGLE_API_KEY")
+        except AttributeError:
+            logger.error(f"Failed to load google API key from settings: {os.getenv('GOOGLE_API_KEY')}")
+            raise
