4900723579d4568901c5adbee3ba8ab8e5d091d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:42 +0000

    Add package SciQLopPlots/0.8.2/sciqlopplots-0.8.2-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/SciQLopPlots/sciqlopplots-0.8.2-cp39-cp39-macosx_11_0_arm64.whl/SciQLopPlots/__init__.py
@@ -0,0 +1,5 @@
+from PySide6 import QtCore, QtGui, QtWidgets, QtOpenGL, QtPrintSupport, QtSvg
+from .SciQLopPlotsBindings import *
+from .SciQLopPlotsBindings import _QCustomPlot as QCustomPlot
+
+__version__ = '0.8.2'
