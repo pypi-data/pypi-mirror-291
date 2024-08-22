3022c92d2220c378800169a91844fd07ef13ebec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 04:05:13 +0000

    Add package autogen-openai/0.0.1/autogen_openai-0.0.1.tar.gz

---
+++ packages/autogen-openai/autogen_openai-0.0.1.tar.gz/autogen_openai-0.0.1/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+setuptools.setup(
+    name="autogen-openai",
+    version="0.0.1",
+    author="AutoGen Developers",
+    author_email="your_email@example.com",
+    description="AutoGen Open AI",
+    long_description="AutoGen OpenAI",
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/autogen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
