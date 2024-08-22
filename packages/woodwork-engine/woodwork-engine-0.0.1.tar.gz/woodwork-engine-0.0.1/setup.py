830205878989c4fa02f902f45879b4a0af4c9254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:18 +0000

    Add package woodwork-engine/0.0.1/woodwork-engine-0.0.1.tar.gz

---
+++ packages/woodwork-engine/woodwork-engine-0.0.1.tar.gz/woodwork-engine-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(where='.'),
+    package_dir={'': '.'},
+    entry_points={
+        'console_scripts': [
+            'woodwork=woodwork.__main__:main',
+        ],
+    },
+)
