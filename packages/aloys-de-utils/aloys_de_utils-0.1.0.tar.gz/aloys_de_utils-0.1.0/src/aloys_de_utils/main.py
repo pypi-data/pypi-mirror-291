03f89b1536160ce8fd1ba2691e2ddb5a56c0e8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 03:05:30 +0000

    Add package aloys-de-utils/0.1.0/aloys_de_utils-0.1.0.tar.gz

---
+++ packages/aloys-de-utils/aloys_de_utils-0.1.0.tar.gz/aloys_de_utils-0.1.0/src/aloys_de_utils/main.py
@@ -0,0 +1,14 @@
+import click
+from .vm import start, stop, connect
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(start)
+cli.add_command(stop)
+cli.add_command(connect)
+
+
+if __name__ == '__main__':
+    cli()
