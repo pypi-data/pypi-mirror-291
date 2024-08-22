da8966d62e89c15ebb435a5da4db3338e36bb423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:07:53 +0000

    Add package PyRobbo/0.5.0/pyrobbo-0.5.0.tar.gz

---
+++ packages/PyRobbo/pyrobbo-0.5.0.tar.gz/pyrobbo-0.5.0/pyrobbo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding: utf8
+# Copyright (C) 2019 Maciej Dems <maciej.dems@p.lodz.pl>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the license, or (at your
+# opinion) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import robbo
+
+robbo.main()
