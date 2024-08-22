de4c19e294687c6134c4f027168cac8a0904e765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:20 +0000

    Add package code2claude/0.2.10/code2claude-0.2.10.tar.gz

---
+++ packages/code2claude/code2claude-0.2.10.tar.gz/code2claude-0.2.10/setup.py
@@ -0,0 +1,41 @@
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+version = "0.2.10"
+
+setup(
+    name="code2claude",
+    version=version,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={"": ["README.md"]},
+    python_requires=">=3.6",
+    include_package_data=True,
+    scripts=[],
+    license="BSD",
+    url="https://github.com/ad3002/code2claude",
+    author="Aleksey Komissarov",
+    author_email="ad3002@gmail.com",
+    description="A Python tool that consolidates code from a repository into a unified format for AI-assisted analysis and manipulation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    entry_points={
+        'console_scripts': [
+            'code2claude = code2claude.consolidate:main',
+        ],
+    },
+)
