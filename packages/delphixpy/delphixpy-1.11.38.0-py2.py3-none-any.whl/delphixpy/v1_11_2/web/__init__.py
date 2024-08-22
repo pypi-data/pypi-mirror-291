ae77250ea50e2a06d0f59572069c110c2234211d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:46 +0000

    Add package delphixpy/1.11.38.0/delphixpy-1.11.38.0-py2.py3-none-any.whl

---
+++ packages/delphixpy/delphixpy-1.11.38.0-py2.py3-none-any.whl/delphixpy/v1_11_2/web/__init__.py
@@ -0,0 +1,62 @@
+#
+# Copyright 2024 by Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Package "web"
+"""
+from . import storage
+from . import network
+from . import about
+from . import theme
+from . import source
+from . import job
+from . import passwordPolicy
+from . import authorization
+from . import group
+from . import user
+from . import permission
+from . import capacity
+from . import maskingjob
+from . import objects
+from . import service
+from . import connectivity
+from . import database
+from . import fault
+from . import alert
+from . import data
+from . import session
+from . import delphix
+from . import timeflow
+from . import host
+from . import transformation
+from . import timezone
+from . import analytics
+from . import domain
+from . import environment
+from . import toolkit
+from . import sourceconfig
+from . import policy
+from . import usage
+from . import system
+from . import registration
+from . import selfservice
+from . import notification
+from . import snapshot
+from . import namespace
+from . import role
+from . import repository
+from . import action
+from . import replication
