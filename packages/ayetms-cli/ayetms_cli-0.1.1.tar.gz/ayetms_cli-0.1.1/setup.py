7fa46f0d71346fb07db2836dcfb02492ed3f3196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 16:07:46 +0000

    Add package ayetms-cli/0.1.1/ayetms_cli-0.1.1.tar.gz

---
+++ packages/ayetms-cli/ayetms_cli-0.1.1.tar.gz/ayetms_cli-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ayetms-cli",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ayetms=ayetms_cli:main",
+        ],
+    },
+    author="Saurabh Satapathy",
+    author_email="saurabhsatapathy0@gmail.com",
+    description="A CLI for AYETMS (As You Earn Time Management System)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/ayetms-cli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
