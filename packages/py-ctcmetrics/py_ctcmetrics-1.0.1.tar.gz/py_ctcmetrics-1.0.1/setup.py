ec0ea7f033e420851dd852258433b15df7daef1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 15:05:57 +0000

    Add package py-ctcmetrics/1.0.1/py_ctcmetrics-1.0.1.tar.gz

---
+++ packages/py-ctcmetrics/py_ctcmetrics-1.0.1.tar.gz/py_ctcmetrics-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-ctcmetrics",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "scikit-learn",
+        "scipy",
+        "tifffile",
+        "imagecodecs",
+        "pandas"
+    ],
+    author="Timo Kaiser",
+    author_email="kaiser@tnt.uni-hannover.de",
+    description="Metrics for Cell Tracking Challenges",
+    long_description="Metrics for Cell Tracking Challenges",
+    entry_points={
+        'console_scripts': [
+            'ctc_evaluate = ctc_metrics.scripts.evaluate:main',
+            'ctc_validate = ctc_metrics.scripts.validate:main',
+            'ctc_noise = ctc_metrics.scripts.noise:main',
+            'ctc_visualize = ctc_metrics.scripts.visualize:main',
+            # Add more scripts here if needed
+        ],
+    },
+)
