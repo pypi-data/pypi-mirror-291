4b4a3d056c38d1ebf7902343e41e6b85ccbfac1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 06:05:16 +0000

    Add package openmock/3.0.0/openmock-3.0.0-py3-none-any.whl

---
+++ packages/openmock/openmock-3.0.0-py3-none-any.whl/openmock/__init__.py
@@ -0,0 +1,45 @@
+"""
+Module initialization
+"""
+
+from functools import wraps
+from unittest.mock import patch
+
+from openmock.fake_asyncopensearch import AsyncFakeOpenSearch
+from openmock.fake_cluster import FakeClusterClient
+from openmock.fake_indices import FakeIndicesClient
+from openmock.fake_opensearch import FakeOpenSearch
+from openmock.normalize_hosts import _normalize_hosts
+
+__all__ = [
+    "openmock",
+    "FakeOpenSearch",
+    "AsyncFakeOpenSearch",
+    "FakeClusterClient",
+    "FakeIndicesClient",
+]
+
+OPEN_INSTANCES = {}
+
+
+def _get_openmock(*args, hosts=None, **kwargs):
+    host = _normalize_hosts(hosts)[0]
+    open_key = f'{host.get("host", "localhost")}:{host.get("port", 9200)}'
+
+    if open_key in OPEN_INSTANCES:
+        connection = OPEN_INSTANCES.get(open_key)
+    else:
+        connection = FakeOpenSearch()
+        OPEN_INSTANCES[open_key] = connection
+    return connection
+
+
+def openmock(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        OPEN_INSTANCES.clear()
+        with patch("opensearchpy.OpenSearch", _get_openmock):
+            result = f(*args, **kwargs)
+        return result
+
+    return decorated
---
+++ packages/openmock/openmock-3.0.0-py3-none-any.whl/openmock/behaviour/server_failure.py
@@ -0,0 +1,39 @@
+"""
+Simulate server failure
+"""
+
+from functools import wraps
+
+__ENABLED = False
+
+
+def enable():
+    """
+    Enable server failure
+    """
+    global __ENABLED
+    __ENABLED = True
+
+
+def disable():
+    """
+    Disable server failure
+    """
+    global __ENABLED
+    __ENABLED = False
+
+
+def server_failure(f):
+    """
+    Decorator to simulate server failure
+    """
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if __ENABLED:
+            response = {"status_code": 500, "error": "Internal Server Error"}
+        else:
+            response = f(*args, **kwargs)
+        return response
+
+    return decorated
