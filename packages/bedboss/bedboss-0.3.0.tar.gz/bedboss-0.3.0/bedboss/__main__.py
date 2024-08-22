6f80c17f94b016d6210e1bfbbac8153d95a4b543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:05:21 +0000

    Add package bedboss/0.3.0/bedboss-0.3.0.tar.gz

---
+++ packages/bedboss/bedboss-0.3.0.tar.gz/bedboss-0.3.0/bedboss/__main__.py
@@ -0,0 +1,19 @@
+import sys
+import logmuse
+
+from bedboss.cli import app
+
+_LOGGER = logmuse.init_logger("bedboss")
+
+
+def main():
+    app(prog_name="bedboss")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        print("Pipeline aborted.")
+        sys.exit(1)
