a1994cc7c92f1a00e62df4b5ac320ffaf79eec0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-22 05:04:57 +0000

    Add package pyProbabilityDistributionFit/0.0.6/pyprobabilitydistributionfit-0.0.6.tar.gz

---
+++ packages/pyProbabilityDistributionFit/pyprobabilitydistributionfit-0.0.6.tar.gz/pyprobabilitydistributionfit-0.0.6/pdffit/version.py
@@ -0,0 +1,17 @@
+"""
+Created on 2022-05-18
+
+@author: wf
+"""
+
+
+class Version(object):
+    """
+    Version handling for pyProbabilityDistributionFit
+    """
+
+    version = "0.0.6"
+    date = "2022-05-18"
+    updated = "2024-08-21"
+    name = "pyProbabilityDistributionFit"
+    description = "Find the best Probability Distribution Function for the given data"
